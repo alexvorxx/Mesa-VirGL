0e8582d6da8ba91ef08fe8b7aca6f2f4d1d4b5ba
Jason Macnak <natsu@google.com>
2023-09-19 13:21:41 -0700

    Move SyncHelper to platform
    ... upcoming changes will add promote the emulated virtio gpu
    stack as a proper platform backend which will include the
    SyncHelper.
    
    Reviewed-by: Aaron Ruby <aruby@blackberry.com>
    Acked-by: Yonggang Luo <luoyonggang@gmail.com>
    Acked-by: Adam Jackson <ajax@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27246>

---
+++ src/gfxstream/guest/platform/linux/LinuxSync.cpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "LinuxSync.h"
+
+#if defined(__ANDROID__)
+#include <sync/sync.h>
+#endif
+#include <unistd.h>
+
+namespace gfxstream {
+
+LinuxSyncHelper::LinuxSyncHelper() {}
+
+int LinuxSyncHelper::wait(int syncFd, int timeoutMilliseconds) {
+#if defined(__ANDROID__)
+    return sync_wait(syncFd, timeoutMilliseconds);
+#else
+    (void)syncFd;
+    (void)timeoutMilliseconds;
+    return -1;
+#endif
+}
+
+int LinuxSyncHelper::dup(int syncFd) {
+    return ::dup(syncFd);
+}
+
+int LinuxSyncHelper::close(int syncFd) {
+    return ::close(syncFd);
+}
+
+SyncHelper* createPlatformSyncHelper() {
+    return new LinuxSyncHelper();
+}
+
+}  // namespace gfxstream
