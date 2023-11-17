cecd8abbdda102c56ac82f2d8e33bf1ffe9f499e
C Stout <cstout@google.com>
2023-11-17 10:25:46 -0800

    Exclude Android entry points on Fuchsia
    Reviewed-by: Aaron Ruby <aruby@blackberry.com>
    Acked-by: Yonggang Luo <luoyonggang@gmail.com>
    Acked-by: Adam Jackson <ajax@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27246>

--- src/gfxstream/codegen/scripts/cerealgenerator.py
+++ src/gfxstream/codegen/scripts/cerealgenerator.py
@@ -342,7 +342,7 @@ class IOStream;
 // required extensions, but the approach will be to
 // implement them completely on the guest side.
 #undef VK_KHR_android_surface
-#if defined(LINUX_GUEST_BUILD)
+#if defined(LINUX_GUEST_BUILD) || defined(__Fuchsia__)
 #undef VK_ANDROID_native_buffer
 #endif
 """
