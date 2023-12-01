1a72fc013ced60c87cc5c802c083a8d1e0dafc56
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2023-12-01 20:19:08 +0200

    isl: Tile W memcpy support
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Reviewed-by: Nanley Chery <nanley.g.chery@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31579>

--- src/intel/isl/tests/isl_tilememcpy_tiled_unittest.cpp
+++ src/intel/isl/tests/isl_tilememcpy_tiled_unittest.cpp
@@ -60,6 +60,11 @@ typedef uint8_t *(*swizzle_func_t)(const uint8_t *base_addr, uint32_t pitch, uin
    std::make_tuple(  0,  16,  0, 32),    \
    std::make_tuple(  0,  16,  0, 64)
 
+#define FULL_TILEW_COORDINATES \
+   std::make_tuple(  0,  64,  0, 64),    \
+   std::make_tuple(  0, 128,  0, 64),    \
+   std::make_tuple(  0, 128,  0,128)
+
 struct tile_swizzle_ops {
    enum isl_tiling tiling;
    swizzle_func_t linear_to_tile_swizzle;
@@ -138,10 +143,42 @@ uint8_t *linear_to_tileX_swizzle(const uint8_t * base_addr, uint32_t pitch, uint
    return (uint8_t *) (base_addr + tiled_off);
 }
 
+uint8_t *linear_to_tileW_swizzle(const uint8_t *base_addr, uint32_t pitch, uint32_t x_B, uint32_t y_px)
+{
+   /* TileW is a special case with doubled physical tile width due to HW
+    * programming requirements (see isl_tiling_get_info() in
+    * src/intel/isl/isl.c)
+    */
+   pitch /= 2;
+
+   const uint32_t cu = 6, cv = 6;
+   const uint32_t tile_id = (y_px >> cv) * (pitch >> cu) + (x_B >> cu);
+
+   /* The table below represents the mapping from coordinate (x_B, y_px) to
+    * byte offset in a 64x64px 1Bpp image:
+    *
+    *    Bit ind : 11 10  9  8  7  6  5  4  3  2  1  0
+    *     Tile-W : u5 u4 u3 v5 v4 v3 v2 u2 v1 u1 v0 u0
+    */
+   uint32_t tiled_off;
+
+   tiled_off = tile_id * 4096 |
+               swizzle_bitops(x_B,  1, 0, 0) |
+               swizzle_bitops(y_px, 1, 0, 1) |
+               swizzle_bitops(x_B,  1, 1, 2) |
+               swizzle_bitops(y_px, 1, 1, 3) |
+               swizzle_bitops(x_B,  1, 2, 4) |
+               swizzle_bitops(y_px, 4, 2, 5) |
+               swizzle_bitops(x_B,  3, 3, 9);
+
+   return (uint8_t *) (base_addr + tiled_off);
+}
+
 struct tile_swizzle_ops swizzle_opers[] = {
    {ISL_TILING_Y0, linear_to_tileY_swizzle},
    {ISL_TILING_4, linear_to_tile4_swizzle},
    {ISL_TILING_X, linear_to_tileX_swizzle},
+   {ISL_TILING_W, linear_to_tileW_swizzle},
 };
 
 class tileTFixture: public ::testing::Test {
@@ -190,6 +227,11 @@ class tile4Fixture : public tileTFixture,
 {};
 
 class tileXFixture : public tileTFixture,
+                     public ::testing::WithParamInterface<std::tuple<int, int,
+                                                                     int, int>>
+{};
+
+class tileWFixture : public tileTFixture,
                      public ::testing::WithParamInterface<std::tuple<int, int,
                                                                      int, int>>
 {};
@@ -401,6 +443,24 @@ TEST_P(tileXFixture, tiletolin)
     run_test(x1, x2, y1, y2);
 }
 
+TEST_P(tileWFixture, lintotile)
+{
+    auto [x1, x2, y1, y2] = GetParam();
+    test_setup(LIN_TO_TILE, ISL_TILING_W, TILEW_IMAGE_FORMAT, x2, y2);
+    if (print_results)
+       printf("Coordinates: x1=%d x2=%d y1=%d y2=%d \n", x1, x2, y1, y2);
+    run_test(x1, x2, y1, y2);
+}
+
+TEST_P(tileWFixture, tiletolin)
+{
+    auto [x1, x2, y1, y2] = GetParam();
+    test_setup(TILE_TO_LIN, ISL_TILING_W, TILEW_IMAGE_FORMAT, x2, y2);
+    if (print_results)
+       printf("Coordinates: x1=%d x2=%d y1=%d y2=%d \n", x1, x2, y1, y2);
+    run_test(x1, x2, y1, y2);
+}
+
 
 INSTANTIATE_TEST_SUITE_P(tileY, tileYFixture, testing::Values(TILE_COORDINATES,
                                                               FULL_TILEY_COORDINATES));
@@ -408,3 +468,5 @@ INSTANTIATE_TEST_SUITE_P(tile4, tile4Fixture, testing::Values(TILE_COORDINATES,
                                                               FULL_TILEY_COORDINATES));
 INSTANTIATE_TEST_SUITE_P(tileX, tileXFixture, testing::Values(TILE_COORDINATES,
                                                               FULL_TILEX_COORDINATES));
+INSTANTIATE_TEST_SUITE_P(tileW, tileWFixture, testing::Values(TILE_COORDINATES,
+                                                              FULL_TILEW_COORDINATES));
