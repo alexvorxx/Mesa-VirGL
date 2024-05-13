7d5cb04125ddf2d86db361d4942086d1c53a78e9
Erico Nunes <nunes.erico@gmail.com>
2024-05-14 01:20:06 +0200

    lima/ppir: add folding lowering for src modifiers
    Lowering to fold src modifiers to prepare for this being disabled
    in nir.
    This will allow ppir to drop nir_legacy usage.
    
    Signed-off-by: Erico Nunes <nunes.erico@gmail.com>
    Reviewed-by: Vasily Khoruzhick <anarsoul@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30332>

--- src/gallium/drivers/lima/ir/pp/lower.c
+++ src/gallium/drivers/lima/ir/pp/lower.c
@@ -283,9 +283,87 @@ static bool ppir_lower_trunc(ppir_block *block, ppir_node *node)
    return true;
 }
 
+static bool ppir_lower_fold_src_mod(ppir_block *block, ppir_node *node)
+{
+   assert(node->op == ppir_op_neg || node->op == ppir_op_abs);
+
+   if (ppir_node_is_root(node))
+      return false;
+
+   if (node->succ_different_block)
+      return false;
+
+   ppir_dest *dest = ppir_node_get_dest(node);
+   if (dest->type != ppir_target_ssa)
+      return false;
+
+   ppir_src *mod_src = ppir_node_get_src(node, 0);
+
+   if (mod_src->type == ppir_target_pipeline &&
+       !ppir_node_has_single_succ(node))
+      return false;
+
+   ppir_node_foreach_succ_safe(node, dep) {
+      ppir_node *succ = dep->succ;
+      assert(succ);
+
+      if (succ->type != ppir_node_type_alu)
+         return false;
+   }
+
+   ppir_node_foreach_succ_safe(node, dep) {
+      ppir_node *succ = dep->succ;
+      assert(succ && succ->type == ppir_node_type_alu);
+
+      for (int i = 0; i < ppir_node_get_src_num(succ); i++) {
+         ppir_src *src = ppir_node_get_src(succ, i);
+         assert(src);
+
+         if (src->node != node)
+            continue;
+
+         uint8_t swizzle[4];
+         for (int j = 0; j < 4; j++)
+            swizzle[j] = mod_src->swizzle[src->swizzle[j]];
+
+         /* Both src or mod_src may already carry folded modifiers.
+          * Account for those by saving src modifiers and applying
+          * them again afterwards. */
+         bool neg = src->negate;
+         bool abs = src->absolute;
+
+         *src = *mod_src;
+         if (node->op == ppir_op_neg)
+            src->negate = !src->negate;
+         else /* ppir_op_abs */
+            src->absolute = true;
+
+         if (neg)
+            src->negate = !src->negate;
+         if (abs)
+            src->absolute = true;
+
+         memcpy(src->swizzle, swizzle, sizeof(swizzle));
+      }
+
+      /* insert the succ alu node as successor of the mod src node */
+      ppir_node_foreach_pred_safe(node, dep) {
+         ppir_node *pred = dep->pred;
+         ppir_node_add_dep(succ, pred, ppir_dep_src);
+      }
+   }
+
+   ppir_node_delete(node);
+   return true;
+}
+
 static bool ppir_lower_abs(ppir_block *block, ppir_node *node)
 {
-   /* Turn it into a mov and set the absolute modifier */
+   /* Check if we can fold it as a src modifier */
+   if (ppir_lower_fold_src_mod(block, node))
+      return true;
+
+   /* Fall back to a mov and set the absolute modifier */
    ppir_alu_node *alu = ppir_node_to_alu(node);
 
    assert(alu->num_src == 1);
@@ -299,7 +377,11 @@ static bool ppir_lower_abs(ppir_block *block, ppir_node *node)
 
 static bool ppir_lower_neg(ppir_block *block, ppir_node *node)
 {
-   /* Turn it into a mov and set the negate modifier */
+   /* Check if we can fold it as a src modifier */
+   if (ppir_lower_fold_src_mod(block, node))
+      return true;
+
+   /* Fall back to a mov and set the negate modifier */
    ppir_alu_node *alu = ppir_node_to_alu(node);
 
    assert(alu->num_src == 1);
