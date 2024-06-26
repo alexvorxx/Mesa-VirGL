ade4512803330f47fff5319912fb52664c41d4f6
Thomas H.P. Andersen <phomes@gmail.com>
2024-06-27 00:58:33 +0200

    nvk: handle driconf for zeroing vram
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29892>

--- src/nouveau/vulkan/nvk_instance.c
+++ src/nouveau/vulkan/nvk_instance.c
@@ -107,6 +107,7 @@ static const driOptionDescription nvk_dri_options[] = {
       DRI_CONF_FORCE_VK_VENDOR()
       DRI_CONF_VK_WSI_FORCE_SWAPCHAIN_TO_CURRENT_EXTENT(false)
       DRI_CONF_VK_X11_IGNORE_SUBOPTIMAL(false)
+      DRI_CONF_VK_ZERO_VRAM(false)
    DRI_CONF_SECTION_END
 };
 
@@ -120,6 +121,9 @@ nvk_init_dri_options(struct nvk_instance *instance)
 
    instance->force_vk_vendor =
       driQueryOptioni(&instance->dri_options, "force_vk_vendor");
+
+   if (driQueryOptionb(&instance->dri_options, "vk_zero_vram"))
+      instance->debug_flags |= NVK_DEBUG_ZERO_MEMORY;
 }
 
 VKAPI_ATTR VkResult VKAPI_CALL
