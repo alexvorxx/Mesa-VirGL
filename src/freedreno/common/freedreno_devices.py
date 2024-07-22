10eaf06e47d64e6ff44266185347433f1ada0cda
Rob Clark <robdclark@chromium.org>
2024-07-22 10:18:45 -0700

    freedreno/a7xx: Fix GRAS_UNKNOWN_80F4 writes
    If this is a 64b reg, we should write both halves.
    
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30304>

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1055,6 +1055,7 @@ add_gpus([
             [A6XXRegs.REG_A7XX_HLSQ_UNKNOWN_A9AD, 0x00000000],
             [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_8008, 0x00000000],
             [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_80F4, 0x00000000],
+            [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_80F5, 0x00000000],
         ],
     ))
 
@@ -1139,6 +1140,7 @@ add_gpus([
             [A6XXRegs.REG_A6XX_RB_UNKNOWN_88F4,   0x00000000],
             [A6XXRegs.REG_A7XX_HLSQ_UNKNOWN_A9AD, 0x00000000],
             [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_80F4, 0x00000000],
+            [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_80F5, 0x00000000],
         ],
     ))
 
