c2dda8c8e7536b9e5bc973adbed8c6003cb49c7b
Ian Romanick <ian.d.romanick@intel.com>
2024-07-22 19:27:41 -0700

    intel/elk: Fix undefined shift by 64 of uint64_t in elk_compute_first_urb_slot_required
    Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>
    Reviewed-by: Matt Turner <mattst88@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30333>

--- src/intel/compiler/elk/elk_compiler.h
+++ src/intel/compiler/elk/elk_compiler.h
@@ -1750,7 +1750,8 @@ elk_compute_first_urb_slot_required(uint64_t inputs_read,
    if ((inputs_read & (VARYING_BIT_LAYER | VARYING_BIT_VIEWPORT | VARYING_BIT_PRIMITIVE_SHADING_RATE)) == 0) {
       for (int i = 0; i < prev_stage_vue_map->num_slots; i++) {
          int varying = prev_stage_vue_map->slot_to_varying[i];
-         if (varying > 0 && (inputs_read & BITFIELD64_BIT(varying)) != 0)
+         if (varying != ELK_VARYING_SLOT_PAD && varying > 0 &&
+             varying > 0 && (inputs_read & BITFIELD64_BIT(varying)) != 0)
             return ROUND_DOWN_TO(i, 2);
       }
    }
