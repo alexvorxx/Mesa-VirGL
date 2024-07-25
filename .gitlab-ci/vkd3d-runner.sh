1b8533a7f42b7da6ec9025990af1279e848bfacb
Eric Engestrom <eric@igalia.com>
2024-07-25 17:09:39 +0200

    ci/vkd3d: move expectation file creation logic out of the test results handling
    The next commit will add checks before we start running the tests.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30413>

--- .gitlab-ci/vkd3d-runner.sh
+++ .gitlab-ci/vkd3d-runner.sh
@@ -37,6 +37,15 @@ if ! vulkaninfo | grep driverInfo | tee /tmp/version.txt | grep -F "Mesa $MESA_V
     exit 1
 fi
 
+# Gather the list expected failures
+EXPECTATIONFILE="$RESULTS/$GPU_VERSION-vkd3d.txt"
+if [ -f "$INSTALL/$GPU_VERSION-vkd3d.txt" ]; then
+    cp "$INSTALL/$GPU_VERSION-vkd3d.txt" "$EXPECTATIONFILE"
+else
+    printf "%s\n" "$GPU_VERSION-vkd3d.txt not found, assuming a \"no failures\" baseline."
+    touch "$EXPECTATIONFILE"
+fi
+
 printf "%s\n" "Running vkd3d-proton testsuite..."
 
 if ! /vkd3d-proton-tests/x64/bin/d3d12 &> "$RESULTS/vkd3d-proton-log.txt"; then
@@ -51,15 +60,6 @@ if ! /vkd3d-proton-tests/x64/bin/d3d12 &> "$RESULTS/vkd3d-proton-log.txt"; then
     if ! grep "Test failed" "$RESULTS"/vkd3d-proton-log.txt > "$RESULTSFILE"; then
       error "Failed to get the list of failing tests, see ${ARTIFACTS_BASE_URL}/results/vkd3d-proton-log.txt"
       exit 1
-    fi
-
-    # Gather the list expected failures
-    EXPECTATIONFILE="$RESULTS/$GPU_VERSION-vkd3d.txt"
-    if [ -f "$INSTALL/$GPU_VERSION-vkd3d.txt" ]; then
-        cp "$INSTALL/$GPU_VERSION-vkd3d.txt" "$EXPECTATIONFILE"
-    else
-        printf "%s\n" "$GPU_VERSION-vkd3d.txt not found, assuming a \"no failures\" baseline."
-        touch "$EXPECTATIONFILE"
     fi
 
     # Make sure that the failures found in this run match the current expectation
