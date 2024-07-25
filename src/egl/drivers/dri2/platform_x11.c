b7c17a4f3b65022b6299201f47e3f1d8e2021a75
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2024-07-25 11:54:13 -0400

    loader/dri3: delete DRI2_FLUSH remnants
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Reviewed-by: Adam Jackson <ajax@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30450>

--- src/egl/drivers/dri2/platform_x11.c
+++ src/egl/drivers/dri2/platform_x11.c
@@ -1928,7 +1928,6 @@ dri2_initialize_x11_dri3(_EGLDisplay *disp)
                                 dri2_dpy->screen);
 
    dri2_dpy->loader_dri3_ext.image_driver = dri2_dpy->image_driver;
-   dri2_dpy->loader_dri3_ext.flush = dri2_dpy->flush;
    dri2_dpy->loader_dri3_ext.image = dri2_dpy->image;
    dri2_dpy->loader_dri3_ext.config = dri2_dpy->config;
 
