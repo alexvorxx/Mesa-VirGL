alexvorxx/zink-xlib-termux

commit f1f8c465d5e14f8f1301478b4662ce6663a05f3c

vc4,v3d,v3dv: avoid compiling in unused `sim_file` field in the {vc4,v3d}_screen/v3dv_physical_device struct
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30287>

src/gallium/drivers/vc4/vc4_screen.h
@@ -105,7 +105,9 @@ struct vc4_screen {
         bool has_perfmon_ioctl;
         bool has_syncobj;
 
+#if USE_V3D_SIMULATOR
         struct vc4_simulator_file *sim_file;
+#endif
 };
 
 static inline struct vc4_screen *

src/broadcom/vulkan/v3dv_private.h
@@ -162,7 +162,9 @@ struct v3dv_physical_device {
 
    struct v3d_device_info devinfo;
 
+#if USE_V3D_SIMULATOR
    struct v3d_simulator_file *sim_file;
+#endif
 
    const struct v3d_compiler *compiler;
    uint32_t next_program_id;

src/gallium/drivers/v3d/v3d_screen.c
@@ -84,8 +84,9 @@ v3d_screen_destroy(struct pipe_screen *pscreen)
         if (screen->ro)
                 screen->ro->destroy(screen->ro);
 
-        if (USE_V3D_SIMULATOR)
-                v3d_simulator_destroy(screen->sim_file);
+#if USE_V3D_SIMULATOR
+        v3d_simulator_destroy(screen->sim_file);
+#endif
 
         v3d_compiler_free(screen->compiler);
 

src/gallium/drivers/v3d/v3d_screen.h
@@ -87,7 +87,9 @@ struct v3d_screen {
         bool has_perfmon;
         bool nonmsaa_texture_size_limit;
 
+#if USE_V3D_SIMULATOR
         struct v3d_simulator_file *sim_file;
+#endif
 
 #ifdef ENABLE_SHADER_CACHE
         struct disk_cache *disk_cache;

alexvorxx/Mesa-VirGL

commit 8f8b520c7881859d12eba7461bb985993a3fb670

vc4: fix typo in simulator conditional declaration
Fixes: f1f8c465d5e ("vc4,v3d,v3dv: avoid compiling in unused `sim_file` field in the {vc4,v3d}_screen/v3dv_physical_device struct")
Reviewed-by: Eric Engestrom <eric@igalia.com>
Signed-off-by: Juan A. Suarez Romero <jasuarez@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30357>

src/gallium/drivers/vc4/vc4_screen.h
@@ -105,7 +105,7 @@ struct vc4_screen {
         bool has_perfmon_ioctl;
         bool has_syncobj;
 
-#if USE_V3D_SIMULATOR
+#ifdef USE_VC4_SIMULATOR
         struct vc4_simulator_file *sim_file;
 #endif
 };