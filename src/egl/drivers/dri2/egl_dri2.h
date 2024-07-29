455219979e6602b18e76b2a0f0589b7e657f08ab
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2024-07-29 10:20:40 -0400

    egl: replace display_name checks with swrast checks
    the only time when display_name isn't set is with true swrast
    
    Reviewed-by: Adam Jackson <ajax@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30472>

--- src/egl/drivers/dri2/egl_dri2.h
+++ src/egl/drivers/dri2/egl_dri2.h
@@ -630,7 +630,7 @@ dri2_set_WL_bind_wayland_display(_EGLDisplay *disp)
 #ifdef HAVE_WAYLAND_PLATFORM
    struct dri2_egl_display *dri2_dpy = dri2_egl_display(disp);
 
-   if (dri2_dpy->device_name && dri2_dpy->image) {
+   if (!dri2_dpy->swrast && dri2_dpy->image) {
       if (dri2_dpy->image->base.version >= 10 &&
           dri2_dpy->image->getCapabilities != NULL) {
          int capabilities;
