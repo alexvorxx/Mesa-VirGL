alexvorxx/zink-xlib-termux

commit 821f4c8d99a3068758db834a5c219082a9609b3c

nouveau: import libdrm_nouveau
Most of the code is copied as is, but there are a couple of changes:
- drop pre nvif code
- nouveau_list -> list_head
- pthread_mutex_t -> simple_mtx_t
- nvif_unwrap removed
- no assignments in if statements
- nouveau_object_mthd -> nouveau_device_info
- reworked object allocation UAPI calls

Most of the reworked code was copied from NVK or a previous version of it

Signed-off-by: Karol Herbst <kherbst@redhat.com>
Acked-by: Faith Ekstrand <faith.ekstrand@collabora.com>
Reviewed-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27853>

src/gallium/drivers/nouveau/nouveau_winsys.h
@@ -8,8 +8,7 @@
 #include "util/os_misc.h"
 
 #include "drm-uapi/drm.h"
-#include <nouveau.h>
-
+#include "nouveau.h"
 #include "nouveau_screen.h"
 
 #ifndef NV04_PFIFO_MAX_PACKET_LEN

android/Android.mk
@@ -79,11 +79,6 @@ LOCAL_SHARED_LIBRARIES += libdrm_radeon
 MESON_GEN_PKGCONFIGS += libdrm_radeon:$(LIBDRM_VERSION)
 endif
 
-ifneq ($(filter nouveau,$(BOARD_MESA3D_GALLIUM_DRIVERS)),)
-LOCAL_SHARED_LIBRARIES += libdrm_nouveau
-MESON_GEN_PKGCONFIGS += libdrm_nouveau:$(LIBDRM_VERSION)
-endif
-
 ifneq ($(filter d3d12,$(BOARD_MESA3D_GALLIUM_DRIVERS)),)
 LOCAL_HEADER_LIBRARIES += DirectX-Headers
 LOCAL_STATIC_LIBRARIES += DirectX-Guids

meson.build
@@ -1633,12 +1633,10 @@ endif
 # bar are both on use 2.4.3 for both of them
 dep_libdrm_amdgpu = null_dep
 dep_libdrm_radeon = null_dep
-dep_libdrm_nouveau = null_dep
 dep_libdrm_intel = null_dep
 
 _drm_amdgpu_ver = '2.4.119'
 _drm_radeon_ver = '2.4.71'
-_drm_nouveau_ver = '2.4.102'
 _drm_intel_ver = '2.4.75'
 _drm_ver = '2.4.109'
 
@@ -1646,7 +1644,6 @@ _libdrm_checks = [
   ['intel', with_gallium_i915],
   ['amdgpu', (with_amd_vk and not with_platform_windows) or with_gallium_radeonsi],
   ['radeon', (with_gallium_radeonsi or with_gallium_r300 or with_gallium_r600)],
-  ['nouveau', with_any_nouveau],
 ]
 
 # Loop over the enables versions and get the highest libdrm requirement for all

src/gallium/drivers/nouveau/ci/gitlab-ci-inc.yml
@@ -7,6 +7,7 @@
     - changes: &nouveau_file_list
       - src/nouveau/*
       - src/nouveau/codegen/**/*
+      - src/nouveau/drm/**/*
       - src/nouveau/drm-shim/**/*
       - src/nouveau/headers/**/*
       - src/nouveau/winsys/**/*

src/gallium/drivers/nouveau/meson.build
@@ -185,11 +185,12 @@ libnouveau = static_library(
     inc_src, inc_include, inc_gallium, inc_gallium_aux, inc_mapi, inc_mesa,
   ],
   gnu_symbol_visibility : 'hidden',
-  dependencies : [dep_libdrm, idep_nouveau_codegen, dep_libdrm_nouveau, idep_mesautil, idep_nir_headers],
+  link_with : [libnouveauwinsys],
+  dependencies : [dep_libdrm, idep_libnouveauwinsys, idep_nouveau_codegen, idep_mesautil, idep_nir_headers],
 )
 
 driver_nouveau = declare_dependency(
   compile_args : '-DGALLIUM_NOUVEAU',
   dependencies : [idep_nir, idep_nouveau_codegen],
-  link_with : [libnouveauwinsys, libnouveau_codegen, libnouveau],
+  link_with : [libnouveau_codegen, libnouveau],
 )

src/gallium/drivers/nouveau/nouveau_context.h
@@ -3,7 +3,7 @@
 
 #include "pipe/p_context.h"
 #include "pipe/p_state.h"
-#include <nouveau.h>
+#include "nouveau_winsys.h"
 
 #define NOUVEAU_MAX_SCRATCH_BUFS 4
 

src/gallium/drivers/nouveau/nouveau_screen.c
@@ -16,10 +16,10 @@
 #include <errno.h>
 #include <stdlib.h>
 
-#include <nouveau_drm.h>
+#include "drm-uapi/nouveau_drm.h"
 #include <xf86drm.h>
-#include <nvif/class.h>
-#include <nvif/cl0080.h>
+#include "nvif/class.h"
+#include "nvif/cl0080.h"
 
 #include "nouveau_winsys.h"
 #include "nouveau_screen.h"
@@ -261,13 +261,13 @@ nouveau_pushbuf_destroy(struct nouveau_pushbuf **push)
 }
 
 static bool
-nouveau_check_for_uma(int chipset, struct nouveau_object *obj)
+nouveau_check_for_uma(int chipset, struct nouveau_device *dev)
 {
    struct nv_device_info_v0 info = {
       .version = 0,
    };
 
-   nouveau_object_mthd(obj, NV_DEVICE_V0_INFO, &info, sizeof(info));
+   nouveau_device_info(dev, &info);
 
    return (info.platform == NV_DEVICE_INFO_V0_IGP) || (info.platform == NV_DEVICE_INFO_V0_SOC);
 }
@@ -286,6 +286,7 @@ nouveau_screen_init(struct nouveau_screen *screen, struct nouveau_device *dev)
    struct pipe_screen *pscreen = &screen->base;
    struct nv04_fifo nv04_data = { .vram = 0xbeef0201, .gart = 0xbeef0202 };
    struct nvc0_fifo nvc0_data = { };
+   struct nve0_fifo nve0_data = { .engine = NOUVEAU_FIFO_ENGINE_GR };
    uint64_t time;
    int size, ret;
    void *data;
@@ -313,9 +314,12 @@ nouveau_screen_init(struct nouveau_screen *screen, struct nouveau_device *dev)
    if (dev->chipset < 0xc0) {
       data = &nv04_data;
       size = sizeof(nv04_data);
-   } else {
+   } else if (dev->chipset < 0xe0) {
       data = &nvc0_data;
       size = sizeof(nvc0_data);
+   } else {
+      data = &nve0_data;
+      size = sizeof(nve0_data);
    }
 
    bool enable_svm = debug_get_bool_option("NOUVEAU_SVM", false);
@@ -432,7 +436,7 @@ nouveau_screen_init(struct nouveau_screen *screen, struct nouveau_device *dev)
       PIPE_BIND_SAMPLER_VIEW | PIPE_BIND_STREAM_OUTPUT |
       PIPE_BIND_COMMAND_ARGS_BUFFER;
 
-   screen->is_uma = nouveau_check_for_uma(dev->chipset, &dev->object);
+   screen->is_uma = nouveau_check_for_uma(dev->chipset, dev);
 
    memset(&mm_config, 0, sizeof(mm_config));
    nouveau_fence_list_init(&screen->fence);

alexvorxx/Mesa-VirGL

commit 801078cbf8e69f0b285c908c4017257798d0940a

nouveau: handle realloc failure inside cli_kref_set
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/11477
Fixes: 821f4c8d99a ("nouveau: import libdrm_nouveau")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30374>

src/gallium/drivers/nouveau/nouveau_winsys.h
@@ -94,7 +94,8 @@ PUSH_KICK(struct nouveau_pushbuf *push)
 {
    struct nouveau_pushbuf_priv *ppush = push->user_priv;
    simple_mtx_lock(&ppush->screen->fence.lock);
-   nouveau_pushbuf_kick(push);
+   int ASSERTED ret = nouveau_pushbuf_kick(push);
+   assert(!ret);
    simple_mtx_unlock(&ppush->screen->fence.lock);
 }
 

src/gallium/winsys/nouveau/drm/nouveau.c
@@ -848,13 +848,18 @@ cli_kref_get(struct nouveau_client *client, struct nouveau_bo *bo)
    return kref;
 }
 
-static inline void
+static inline int
 cli_kref_set(struct nouveau_client *client, struct nouveau_bo *bo,
              struct drm_nouveau_gem_pushbuf_bo *kref, struct nouveau_pushbuf *push)
 {
    struct nouveau_client_priv *pcli = nouveau_client(client);
    if (pcli->kref_nr <= bo->handle) {
-      pcli->kref = realloc(pcli->kref, sizeof(*pcli->kref) * bo->handle * 2);
+      void *new_ptr = realloc(pcli->kref, sizeof(*pcli->kref) * bo->handle * 2);
+      if (!new_ptr) {
+         err("Failed to realloc memory, expect faulty rendering.\n");
+         return -ENOMEM;
+      }
+      pcli->kref = new_ptr;
       while (pcli->kref_nr < bo->handle * 2) {
          pcli->kref[pcli->kref_nr].kref = NULL;
          pcli->kref[pcli->kref_nr].push = NULL;
@@ -863,6 +868,7 @@ cli_kref_set(struct nouveau_client *client, struct nouveau_bo *bo,
    }
    pcli->kref[bo->handle].kref = kref;
    pcli->kref[bo->handle].push = push;
+   return 0;
 }
 
 int
@@ -1203,7 +1209,9 @@ pushbuf_flush(struct nouveau_pushbuf *push)
    kref = krec->buffer;
    for (i = 0; i < krec->nr_buffer; i++, kref++) {
       struct nouveau_bo *bo = (void *)(unsigned long)kref->user_priv;
-      cli_kref_set(push->client, bo, NULL, NULL);
+      ret = cli_kref_set(push->client, bo, NULL, NULL);
+      if (ret)
+         return ret;
       nouveau_bo_ref(NULL, &bo);
    }
 
@@ -1299,6 +1307,7 @@ pushbuf_kref(struct nouveau_pushbuf *push, struct nouveau_bo *bo, uint32_t flags
    struct nouveau_pushbuf *fpush;
    struct drm_nouveau_gem_pushbuf_bo *kref;
    uint32_t domains, domains_wr, domains_rd;
+   int ret;
 
    domains = 0;
    if (flags & NOUVEAU_BO_VRAM)
@@ -1354,7 +1363,9 @@ pushbuf_kref(struct nouveau_pushbuf *push, struct nouveau_bo *bo, uint32_t flags
       else
          kref->presumed.domain = NOUVEAU_GEM_DOMAIN_GART;
 
-      cli_kref_set(push->client, bo, kref, push);
+      ret = cli_kref_set(push->client, bo, kref, push);
+      if (ret)
+         return NULL;
       p_atomic_inc(&nouveau_bo(bo)->refcnt);
    }
 
@@ -1401,22 +1412,26 @@ pushbuf_krel(struct nouveau_pushbuf *push, struct nouveau_bo *bo,
    return reloc;
 }
 
-static void
+static int
 pushbuf_refn_fail(struct nouveau_pushbuf *push, int sref, int srel)
 {
    struct nouveau_pushbuf_priv *nvpb = nouveau_pushbuf(push);
    struct nouveau_pushbuf_krec *krec = nvpb->krec;
    struct drm_nouveau_gem_pushbuf_bo *kref;
+   int ret;
 
    kref = krec->buffer + sref;
    while (krec->nr_buffer-- > sref) {
       struct nouveau_bo *bo = (void *)(unsigned long)kref->user_priv;
-      cli_kref_set(push->client, bo, NULL, NULL);
+      ret = cli_kref_set(push->client, bo, NULL, NULL);
+      if (ret)
+         return ret;
       nouveau_bo_ref(NULL, &bo);
       kref++;
    }
    krec->nr_buffer = sref;
    krec->nr_reloc = srel;
+   return 0;
 }
 
 static int
@@ -1438,7 +1453,9 @@ pushbuf_refn(struct nouveau_pushbuf *push, bool retry,
    }
 
    if (ret) {
-      pushbuf_refn_fail(push, sref, krec->nr_reloc);
+      ret = pushbuf_refn_fail(push, sref, krec->nr_reloc);
+      if (ret)
+         return ret;
       if (retry) {
          pushbuf_flush(push);
          nouveau_pushbuf_space(push, 0, 0, 0);
@@ -1489,7 +1506,9 @@ pushbuf_validate(struct nouveau_pushbuf *push, bool retry)
    list_inithead(&bctx->pending);
 
    if (ret) {
-      pushbuf_refn_fail(push, sref, srel);
+      ret = pushbuf_refn_fail(push, sref, srel);
+      if (ret)
+         return ret;
       if (retry) {
          pushbuf_flush(push);
          return pushbuf_validate(push, false);