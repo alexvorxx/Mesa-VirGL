7c579f448fe0663a1d6a045b832ecc65e27dbb26
Kenneth Graunke <kenneth@whitecape.org>
2024-08-02 16:29:22 -0700

    intel/brw: Mark all UBO access with a direct buffer index as speculative
    UBO loads with a non-indirect buffer index should be safe to perform
    speculatively.  With a direct offset, we may sometimes turn them into
    push constants, at which point it's just reading a register with no
    cost at all.  Otherwise, we access them via messages that use surface
    state, and automatically perform bounds checking.  So we shouldn't have
    any issues with reading out of bounds and page faulting, for example.
    
    This allows nir_opt_peephole_sel() to operate on load_ubo intrinsics,
    so we can turn simple if's with loads on both sides to bcsels.  In some
    cases this can collapse a surprising amount of control flow, allowing
    other optimizations to work better.
    
    The i965 OpenGL driver used load_uniform intrinsics, which are allowed
    in NIR's peephole select pass.  But iris uses the Gallium NIR pass that
    translates uniforms to loads from UBO 0, so we haven't been able to take
    advantage of NIR's peephole select pass there.  The backend pass was
    still able to handle this to some extent, however.
    
    fossil-db results on Alchemist:
    
       Totals:
       Instrs: 150656329 -> 150645307 (-0.01%); split: -0.01%, +0.00%
       Cycles: 12635230179 -> 12633696811 (-0.01%); split: -0.02%, +0.00%
       Send messages: 7416330 -> 7416261 (-0.00%)
       Spill count: 52471 -> 52473 (+0.00%)
       Fill count: 100818 -> 100803 (-0.01%); split: -0.02%, +0.00%
       Scratch Memory Size: 3197952 -> 3198976 (+0.03%)
    
       Totals from 1848 (0.29% of 630003) affected shaders:
       Instrs: 1412300 -> 1401278 (-0.78%); split: -0.80%, +0.02%
       Cycles: 1809789567 -> 1808256199 (-0.08%); split: -0.11%, +0.03%
       Send messages: 59829 -> 59760 (-0.12%)
       Spill count: 3870 -> 3872 (+0.05%)
       Fill count: 9693 -> 9678 (-0.15%); split: -0.18%, +0.02%
       Scratch Memory Size: 174080 -> 175104 (+0.59%)
    
    Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30498>

--- src/intel/compiler/brw_nir.c
+++ src/intel/compiler/brw_nir.c
@@ -677,6 +677,28 @@ brw_nir_lower_fs_outputs(nir_shader *nir)
    nir_lower_io(nir, nir_var_shader_out, type_size_dvec4, 0);
 }
 
+static bool
+tag_speculative_access(nir_builder *b,
+                       nir_intrinsic_instr *intrin,
+                       void *unused)
+{
+   if (intrin->intrinsic == nir_intrinsic_load_ubo &&
+       brw_nir_ubo_surface_index_is_pushable(intrin->src[0])) {
+      nir_intrinsic_set_access(intrin, ACCESS_CAN_SPECULATE |
+                               nir_intrinsic_access(intrin));
+      return true;
+   }
+
+   return false;
+}
+
+static bool
+brw_nir_tag_speculative_access(nir_shader *nir)
+{
+   return nir_shader_intrinsics_pass(nir, tag_speculative_access,
+                                     nir_metadata_all, NULL);
+}
+
 #define OPT(pass, ...) ({                                  \
    bool this_progress = false;                             \
    NIR_PASS(this_progress, nir, pass, ##__VA_ARGS__);      \
@@ -1666,6 +1688,8 @@ brw_postprocess_nir(nir_shader *nir, const struct brw_compiler *compiler,
 
    if (gl_shader_stage_can_set_fragment_shading_rate(nir->info.stage))
       NIR_PASS(_, nir, intel_nir_lower_shading_rate_output);
+
+   OPT(brw_nir_tag_speculative_access);
 
    brw_nir_optimize(nir, devinfo);
 
