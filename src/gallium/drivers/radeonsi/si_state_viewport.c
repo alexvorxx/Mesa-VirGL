alexvorxx/zink-xlib-termux

commit f703dfd1bb8c22b6791dd95c7de270e176452b4b

radeonsi: add gfx12
Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29007>

src/gallium/drivers/radeonsi/si_state_viewport.c
@@ -8,7 +8,8 @@
 #include "util/u_upload_mgr.h"
 #include "util/u_viewport.h"
 
-#define SI_MAX_SCISSOR 16384
+#define GFX6_MAX_VIEWPORT_SIZE   16384
+#define GFX12_MAX_VIEWPORT_SIZE  32768 /* TODO: this should be 64K, but maxx/maxy doesn't have enough bits */
 
 static void si_get_small_prim_cull_info(struct si_context *sctx, struct si_small_prim_cull_info *out)
 {
@@ -97,7 +98,11 @@ static void si_emit_cull_state(struct si_context *sctx, unsigned index)
    radeon_add_to_buffer_list(sctx, &sctx->gfx_cs, sctx->small_prim_cull_info_buf,
                              RADEON_USAGE_READ | RADEON_PRIO_CONST_BUFFER);
 
-   if (sctx->screen->info.has_set_sh_pairs_packed) {
+   if (sctx->gfx_level >= GFX12) {
+      gfx12_push_gfx_sh_reg(R_00B230_SPI_SHADER_USER_DATA_GS_0 +
+                            GFX9_SGPR_SMALL_PRIM_CULL_INFO * 4,
+                            sctx->small_prim_cull_info_address);
+   } else if (sctx->screen->info.has_set_sh_pairs_packed) {
       gfx11_push_gfx_sh_reg(R_00B230_SPI_SHADER_USER_DATA_GS_0 +
                             GFX9_SGPR_SMALL_PRIM_CULL_INFO * 4,
                             sctx->small_prim_cull_info_address);
@@ -195,10 +200,12 @@ static void si_get_scissor_from_viewport(struct si_context *ctx,
 static void si_clamp_scissor(struct si_context *ctx, struct pipe_scissor_state *out,
                              struct si_signed_scissor *scissor)
 {
-   out->minx = CLAMP(scissor->minx, 0, SI_MAX_SCISSOR);
-   out->miny = CLAMP(scissor->miny, 0, SI_MAX_SCISSOR);
-   out->maxx = CLAMP(scissor->maxx, 0, SI_MAX_SCISSOR);
-   out->maxy = CLAMP(scissor->maxy, 0, SI_MAX_SCISSOR);
+   unsigned max_scissor = ctx->gfx_level >= GFX12 ? GFX12_MAX_VIEWPORT_SIZE : GFX6_MAX_VIEWPORT_SIZE;
+
+   out->minx = CLAMP(scissor->minx, 0, max_scissor);
+   out->miny = CLAMP(scissor->miny, 0, max_scissor);
+   out->maxx = CLAMP(scissor->maxx, 0, max_scissor);
+   out->maxy = CLAMP(scissor->maxy, 0, max_scissor);
 }
 
 static void si_clip_scissor(struct pipe_scissor_state *out, struct pipe_scissor_state *clip)
@@ -226,7 +233,7 @@ static void si_emit_one_scissor(struct si_context *ctx, struct radeon_cmdbuf *cs
 
    if (ctx->vs_disables_clipping_viewport) {
       final.minx = final.miny = 0;
-      final.maxx = final.maxy = SI_MAX_SCISSOR;
+      final.maxx = final.maxy = ctx->gfx_level >= GFX12 ? GFX12_MAX_VIEWPORT_SIZE : GFX6_MAX_VIEWPORT_SIZE;
    } else {
       si_clamp_scissor(ctx, &final, vp_scissor);
    }
@@ -235,16 +242,27 @@ static void si_emit_one_scissor(struct si_context *ctx, struct radeon_cmdbuf *cs
       si_clip_scissor(&final, scissor);
 
    radeon_begin(cs);
-   /* Workaround for a hw bug on GFX6 that occurs when PA_SU_HARDWARE_SCREEN_OFFSET != 0 and
-    * any_scissor.BR_X/Y <= 0.
-    */
-   if (ctx->gfx_level == GFX6 && (final.maxx == 0 || final.maxy == 0)) {
-      radeon_emit(S_028250_TL_X(1) | S_028250_TL_Y_GFX6(1) | S_028250_WINDOW_OFFSET_DISABLE(1));
-      radeon_emit(S_028254_BR_X(1) | S_028254_BR_Y(1));
+   if (ctx->gfx_level >= GFX12) {
+      if (final.maxx == 0 || final.maxy == 0) {
+         /* An empty scissor must be done like this because the bottom-right bounds are inclusive. */
+         radeon_emit(S_028250_TL_X(1) | S_028250_TL_Y_GFX12(1));
+         radeon_emit(S_028254_BR_X(0) | S_028254_BR_Y(0));
+      } else {
+         radeon_emit(S_028250_TL_X(final.minx) | S_028250_TL_Y_GFX12(final.miny));
+         radeon_emit(S_028254_BR_X(final.maxx - 1) | S_028254_BR_Y(final.maxy - 1));
+      }
    } else {
-      radeon_emit(S_028250_TL_X(final.minx) | S_028250_TL_Y_GFX6(final.miny) |
-                  S_028250_WINDOW_OFFSET_DISABLE(1));
-      radeon_emit(S_028254_BR_X(final.maxx) | S_028254_BR_Y(final.maxy));
+      /* Workaround for a hw bug on GFX6 that occurs when PA_SU_HARDWARE_SCREEN_OFFSET != 0 and
+       * any_scissor.BR_X/Y <= 0.
+       */
+      if (ctx->gfx_level == GFX6 && (final.maxx == 0 || final.maxy == 0)) {
+         radeon_emit(S_028250_TL_X(1) | S_028250_TL_Y_GFX6(1) | S_028250_WINDOW_OFFSET_DISABLE(1));
+         radeon_emit(S_028254_BR_X(1) | S_028254_BR_Y(1));
+      } else {
+         radeon_emit(S_028250_TL_X(final.minx) | S_028250_TL_Y_GFX6(final.miny) |
+                     S_028250_WINDOW_OFFSET_DISABLE(1));
+         radeon_emit(S_028254_BR_X(final.maxx) | S_028254_BR_Y(final.maxy));
+      }
    }
    radeon_end();
 }
@@ -284,7 +302,7 @@ static void si_emit_guardband(struct si_context *sctx, unsigned index)
    const unsigned hw_screen_offset_alignment =
       sctx->gfx_level >= GFX11 ? 32 :
       sctx->gfx_level >= GFX8 ? 16 : MAX2(sctx->screen->se_tile_repeat, 16);
-   const unsigned max_hw_screen_offset = 8176;
+   const unsigned max_hw_screen_offset = sctx->gfx_level >= GFX12 ? 32752 : 8176;
 
    /* Indexed by quantization modes */
    static int max_viewport_size[] = {65536, 16384, 4096};
@@ -367,7 +385,21 @@ static void si_emit_guardband(struct si_context *sctx, unsigned index)
     * R_028BE8_PA_CL_GB_VERT_CLIP_ADJ, R_028BEC_PA_CL_GB_VERT_DISC_ADJ
     * R_028BF0_PA_CL_GB_HORZ_CLIP_ADJ, R_028BF4_PA_CL_GB_HORZ_DISC_ADJ
     */
-   if (sctx->screen->info.has_set_context_pairs_packed) {
+   if (sctx->gfx_level >= GFX12) {
+      radeon_begin(&sctx->gfx_cs);
+      gfx12_begin_context_regs();
+      gfx12_opt_set_context_reg(R_028BE4_PA_SU_VTX_CNTL, SI_TRACKED_PA_SU_VTX_CNTL,
+                                pa_su_vtx_cntl);
+      gfx12_opt_set_context_reg4(R_02842C_PA_CL_GB_VERT_CLIP_ADJ,
+                                 SI_TRACKED_PA_CL_GB_VERT_CLIP_ADJ,
+                                 fui(guardband_y), fui(discard_y),
+                                 fui(guardband_x), fui(discard_x));
+      gfx12_opt_set_context_reg(R_028234_PA_SU_HARDWARE_SCREEN_OFFSET,
+                                SI_TRACKED_PA_SU_HARDWARE_SCREEN_OFFSET,
+                                pa_su_hardware_screen_offset);
+      gfx12_end_context_regs();
+      radeon_end(); /* don't track context rolls on GFX12 */
+   } else if (sctx->screen->info.has_set_context_pairs_packed) {
       radeon_begin(&sctx->gfx_cs);
       gfx11_begin_packed_context_regs();
       gfx11_opt_set_context_reg(R_028BE4_PA_SU_VTX_CNTL, SI_TRACKED_PA_SU_VTX_CNTL,
@@ -489,7 +521,7 @@ static void si_set_viewport_states(struct pipe_context *pctx, unsigned start_slo
    si_mark_atom_dirty(ctx, &ctx->atoms.s.scissors);
 }
 
-static void si_emit_one_viewport(struct si_context *ctx, struct pipe_viewport_state *state)
+static void gfx6_emit_one_viewport(struct si_context *ctx, struct pipe_viewport_state *state)
 {
    struct radeon_cmdbuf *cs = &ctx->gfx_cs;
 
@@ -503,7 +535,7 @@ static void si_emit_one_viewport(struct si_context *ctx, struct pipe_viewport_st
    radeon_end();
 }
 
-static void si_emit_viewports(struct si_context *ctx)
+static void gfx6_emit_viewports(struct si_context *ctx)
 {
    struct radeon_cmdbuf *cs = &ctx->gfx_cs;
    struct pipe_viewport_state *states = ctx->viewports.states;
@@ -514,7 +546,7 @@ static void si_emit_viewports(struct si_context *ctx)
       radeon_set_context_reg_seq(R_02843C_PA_CL_VPORT_XSCALE, 6);
       radeon_end();
 
-      si_emit_one_viewport(ctx, &states[0]);
+      gfx6_emit_one_viewport(ctx, &states[0]);
       return;
    }
 
@@ -526,7 +558,7 @@ static void si_emit_viewports(struct si_context *ctx)
    radeon_end();
 
    for (unsigned i = 0; i < SI_MAX_VIEWPORTS; i++)
-      si_emit_one_viewport(ctx, &states[i]);
+      gfx6_emit_one_viewport(ctx, &states[i]);
 }
 
 static inline void si_viewport_zmin_zmax(const struct pipe_viewport_state *vp, bool halfz,
@@ -540,7 +572,7 @@ static inline void si_viewport_zmin_zmax(const struct pipe_viewport_state *vp, b
    util_viewport_zmin_zmax(vp, halfz, zmin, zmax);
 }
 
-static void si_emit_depth_ranges(struct si_context *ctx)
+static void gfx6_emit_depth_ranges(struct si_context *ctx)
 {
    struct radeon_cmdbuf *cs = &ctx->gfx_cs;
    struct pipe_viewport_state *states = ctx->viewports.states;
@@ -573,10 +605,57 @@ static void si_emit_depth_ranges(struct si_context *ctx)
    radeon_end();
 }
 
-static void si_emit_viewport_states(struct si_context *ctx, unsigned index)
+static void gfx6_emit_viewport_states(struct si_context *ctx, unsigned index)
 {
-   si_emit_viewports(ctx);
-   si_emit_depth_ranges(ctx);
+   gfx6_emit_viewports(ctx);
+   gfx6_emit_depth_ranges(ctx);
+}
+
+static void gfx12_emit_viewport_states(struct si_context *ctx, unsigned index)
+{
+   struct radeon_cmdbuf *cs = &ctx->gfx_cs;
+   struct pipe_viewport_state *states = ctx->viewports.states;
+   bool clip_halfz = ctx->queued.named.rasterizer->clip_halfz;
+   bool window_space = ctx->vs_disables_clipping_viewport;
+   float zmin, zmax;
+
+   /* The simple case: Only 1 viewport is active. */
+   if (!ctx->vs_writes_viewport_index) {
+      si_viewport_zmin_zmax(&states[0], clip_halfz, window_space, &zmin, &zmax);
+
+      radeon_begin(cs);
+      radeon_set_context_reg_seq(R_02843C_PA_CL_VPORT_XSCALE, 8);
+      radeon_emit(fui(states[0].scale[0]));
+      radeon_emit(fui(states[0].translate[0]));
+      radeon_emit(fui(states[0].scale[1]));
+      radeon_emit(fui(states[0].translate[1]));
+      radeon_emit(fui(states[0].scale[2]));
+      radeon_emit(fui(states[0].translate[2]));
+      radeon_emit(fui(zmin));
+      radeon_emit(fui(zmax));
+      radeon_end();
+      return;
+   }
+
+   /* All registers in the array need to be updated if any of them is changed.
+    * This is (or was) a hardware requirement.
+    */
+   radeon_begin(cs);
+   radeon_set_context_reg_seq(R_02843C_PA_CL_VPORT_XSCALE, SI_MAX_VIEWPORTS * 8);
+
+   for (unsigned i = 0; i < SI_MAX_VIEWPORTS; i++) {
+      si_viewport_zmin_zmax(&states[i], clip_halfz, window_space, &zmin, &zmax);
+
+      radeon_emit(fui(states[i].scale[0]));
+      radeon_emit(fui(states[i].translate[0]));
+      radeon_emit(fui(states[i].scale[1]));
+      radeon_emit(fui(states[i].translate[1]));
+      radeon_emit(fui(states[i].scale[2]));
+      radeon_emit(fui(states[i].translate[2]));
+      radeon_emit(fui(zmin));
+      radeon_emit(fui(zmax));
+   }
+   radeon_end();
 }
 
 /**
@@ -661,20 +740,43 @@ static void si_emit_window_rectangles(struct si_context *sctx, unsigned index)
    else
       rule = outside[num_rectangles - 1];
 
-   radeon_begin(cs);
-   radeon_opt_set_context_reg(sctx, R_02820C_PA_SC_CLIPRECT_RULE, SI_TRACKED_PA_SC_CLIPRECT_RULE,
-                              rule);
-   if (num_rectangles == 0) {
-      radeon_end();
-      return;
-   }
+   if (sctx->gfx_level >= GFX12) {
+      radeon_begin(cs);
+      gfx12_begin_context_regs();
+      gfx12_opt_set_context_reg(R_02820C_PA_SC_CLIPRECT_RULE, SI_TRACKED_PA_SC_CLIPRECT_RULE, rule);
 
-   radeon_set_context_reg_seq(R_028210_PA_SC_CLIPRECT_0_TL, num_rectangles * 2);
-   for (unsigned i = 0; i < num_rectangles; i++) {
-      radeon_emit(S_028210_TL_X(rects[i].minx) | S_028210_TL_Y(rects[i].miny));
-      radeon_emit(S_028214_BR_X(rects[i].maxx) | S_028214_BR_Y(rects[i].maxy));
+      if (num_rectangles) {
+         radeon_set_context_reg_seq(R_028210_PA_SC_CLIPRECT_0_TL, num_rectangles * 2);
+         for (unsigned i = 0; i < num_rectangles; i++) {
+            gfx12_set_context_reg(R_028210_PA_SC_CLIPRECT_0_TL + i * 8,
+                                  S_028210_TL_X(rects[i].minx) | S_028210_TL_Y(rects[i].miny));
+            gfx12_set_context_reg(R_028214_PA_SC_CLIPRECT_0_BR + i * 8,
+                                  S_028214_BR_X(rects[i].maxx) | S_028214_BR_Y(rects[i].maxy));
+         }
+
+         for (unsigned i = 0; i < num_rectangles; i++) {
+            gfx12_set_context_reg(R_028374_PA_SC_CLIPRECT_0_EXT + i * 4,
+                                  S_028374_TL_X_EXT(rects[i].minx >> 15) |
+                                  S_028374_TL_Y_EXT(rects[i].miny >> 15) |
+                                  S_028374_BR_X_EXT(rects[i].maxx >> 15) |
+                                  S_028374_BR_Y_EXT(rects[i].maxy >> 15));
+         }
+      }
+      gfx12_end_context_regs();
+      radeon_end();
+   } else {
+      radeon_begin(cs);
+      radeon_opt_set_context_reg(sctx, R_02820C_PA_SC_CLIPRECT_RULE, SI_TRACKED_PA_SC_CLIPRECT_RULE,
+                                 rule);
+      if (num_rectangles) {
+         radeon_set_context_reg_seq(R_028210_PA_SC_CLIPRECT_0_TL, num_rectangles * 2);
+         for (unsigned i = 0; i < num_rectangles; i++) {
+            radeon_emit(S_028210_TL_X(rects[i].minx) | S_028210_TL_Y(rects[i].miny));
+            radeon_emit(S_028214_BR_X(rects[i].maxx) | S_028214_BR_Y(rects[i].maxy));
+         }
+      }
+      radeon_end();
    }
-   radeon_end();
 }
 
 static void si_set_window_rectangles(struct pipe_context *ctx, bool include,
@@ -696,7 +798,10 @@ void si_init_viewport_functions(struct si_context *ctx)
 {
    ctx->atoms.s.guardband.emit = si_emit_guardband;
    ctx->atoms.s.scissors.emit = si_emit_scissors;
-   ctx->atoms.s.viewports.emit = si_emit_viewport_states;
+   if (ctx->gfx_level >= GFX12)
+      ctx->atoms.s.viewports.emit = gfx12_emit_viewport_states;
+   else
+      ctx->atoms.s.viewports.emit = gfx6_emit_viewport_states;
    ctx->atoms.s.window_rectangles.emit = si_emit_window_rectangles;
    ctx->atoms.s.ngg_cull_state.emit = si_emit_cull_state;
 

src/gallium/drivers/radeonsi/gfx11_query.c
@@ -345,6 +345,10 @@ static void gfx11_sh_query_get_result_resource(struct si_context *sctx, struct s
    grid.grid[1] = 1;
    grid.grid[2] = 1;
 
+   /* TODO: Range-invalidate GL2 */
+   if (sctx->screen->info.cp_sdma_ge_use_system_memory_scope)
+      sctx->flags |= SI_CONTEXT_INV_L2;
+
    struct gfx11_sh_query_buffer *qbuf = query->first;
    for (;;) {
       unsigned begin = qbuf == query->first ? query->first_begin : 0;

src/gallium/drivers/radeonsi/meson.build
@@ -144,7 +144,7 @@ if with_llvm
 endif
 
 radeonsi_gfx_libs = []
-foreach ver : ['6', '7', '8', '9', '10', '103', '11', '115']
+foreach ver : ['6', '7', '8', '9', '10', '103', '11', '115', '12']
   radeonsi_gfx_libs += static_library(
     'radeonsi_gfx@0@'.format(ver),
     ['si_state_draw.cpp'],

src/gallium/drivers/radeonsi/si_blit.c
@@ -193,6 +193,8 @@ static void si_blit_decompress_zs_planes_in_place(struct si_context *sctx,
    unsigned layer, max_layer, checked_last_layer;
    unsigned fully_decompressed_mask = 0;
 
+   assert(sctx->gfx_level < GFX12);
+
    if (!level_mask)
       return;
 
@@ -286,6 +288,8 @@ static void si_decompress_depth(struct si_context *sctx, struct si_texture *tex,
    unsigned level_mask = u_bit_consecutive(first_level, last_level - first_level + 1);
    unsigned levels_z = 0;
    unsigned levels_s = 0;
+
+   assert(sctx->gfx_level < GFX12);
 
    if (required_planes & PIPE_MASK_Z) {
       levels_z = level_mask & tex->dirty_level_mask;
@@ -401,6 +405,8 @@ static bool si_decompress_sampler_depth_textures(struct si_context *sctx,
    unsigned mask = textures->needs_depth_decompress_mask;
    bool need_flush = false;
 
+   assert(sctx->gfx_level < GFX12);
+
    while (mask) {
       struct pipe_sampler_view *view;
       struct si_sampler_view *sview;
@@ -436,6 +442,9 @@ static void si_blit_decompress_color(struct si_context *sctx, struct si_texture 
    void *custom_blend;
    unsigned layer, checked_last_layer, max_layer;
    unsigned level_mask = u_bit_consecutive(first_level, last_level - first_level + 1);
+
+   /* No decompression is ever needed on Gfx12. */
+   assert(sctx->gfx_level < GFX12);
 
    if (!need_dcc_decompress)
       level_mask &= tex->dirty_level_mask;
@@ -624,6 +633,8 @@ static void si_check_render_feedback_texture(struct si_context *sctx, struct si_
 {
    bool render_feedback = false;
 
+   assert(sctx->gfx_level < GFX12);
+
    if (!vi_dcc_enabled(tex, first_level))
       return;
 
@@ -652,6 +663,8 @@ static void si_check_render_feedback_textures(struct si_context *sctx, struct si
 {
    uint32_t mask = textures->enabled_mask & in_use_mask;
 
+   assert(sctx->gfx_level < GFX12);
+
    while (mask) {
       const struct pipe_sampler_view *view;
       struct si_texture *tex;
@@ -674,6 +687,8 @@ static void si_check_render_feedback_images(struct si_context *sctx, struct si_i
 {
    uint32_t mask = images->enabled_mask & in_use_mask;
 
+   assert(sctx->gfx_level < GFX12);
+
    while (mask) {
       const struct pipe_image_view *view;
       struct si_texture *tex;
@@ -693,6 +708,8 @@ static void si_check_render_feedback_images(struct si_context *sctx, struct si_i
 
 static void si_check_render_feedback_resident_textures(struct si_context *sctx)
 {
+   assert(sctx->gfx_level < GFX12);
+
    util_dynarray_foreach (&sctx->resident_tex_handles, struct si_texture_handle *, tex_handle) {
       struct pipe_sampler_view *view;
       struct si_texture *tex;
@@ -710,6 +727,8 @@ static void si_check_render_feedback_resident_textures(struct si_context *sctx)
 
 static void si_check_render_feedback_resident_images(struct si_context *sctx)
 {
+   assert(sctx->gfx_level < GFX12);
+
    util_dynarray_foreach (&sctx->resident_img_handles, struct si_image_handle *, img_handle) {
       struct pipe_image_view *view;
       struct si_texture *tex;
@@ -727,6 +746,8 @@ static void si_check_render_feedback_resident_images(struct si_context *sctx)
 
 static void si_check_render_feedback(struct si_context *sctx)
 {
+   assert(sctx->gfx_level < GFX12);
+
    if (!sctx->need_check_render_feedback)
       return;
 
@@ -895,6 +916,9 @@ void si_decompress_subresource(struct pipe_context *ctx, struct pipe_resource *t
 {
    struct si_context *sctx = (struct si_context *)ctx;
    struct si_texture *stex = (struct si_texture *)tex;
+
+   if (sctx->gfx_level >= GFX12)
+      return;
 
    if (stex->db_compatible) {
       planes &= PIPE_MASK_Z | PIPE_MASK_S;
@@ -1324,7 +1348,7 @@ static void si_flush_resource(struct pipe_context *ctx, struct pipe_resource *re
    struct si_context *sctx = (struct si_context *)ctx;
    struct si_texture *tex = (struct si_texture *)res;
 
-   if (res->target == PIPE_BUFFER)
+   if (sctx->gfx_level >= GFX12 || res->target == PIPE_BUFFER)
       return;
 
    if (!tex->is_depth && (tex->cmask_buffer || vi_dcc_enabled(tex, 0))) {
@@ -1340,6 +1364,8 @@ static void si_flush_resource(struct pipe_context *ctx, struct pipe_resource *re
 
 void si_flush_implicit_resources(struct si_context *sctx)
 {
+   assert(sctx->gfx_level < GFX12);
+
    hash_table_foreach(sctx->dirty_implicit_resources, entry) {
       si_flush_resource(&sctx->b, entry->data);
       pipe_resource_reference((struct pipe_resource **)&entry->data, NULL);
@@ -1349,6 +1375,7 @@ void si_flush_implicit_resources(struct si_context *sctx)
 
 void si_decompress_dcc(struct si_context *sctx, struct si_texture *tex)
 {
+   assert(sctx->gfx_level < GFX12);
    assert(!tex->is_depth);
 
    /* If graphics is disabled, we can't decompress DCC, but it shouldn't

src/gallium/drivers/radeonsi/si_build_pm4.h
@@ -397,8 +397,106 @@
    } \
 } while (0)
 
+/* GFX12 generic packet building helpers for PAIRS packets. Don't use these directly. */
+#define gfx12_begin_regs(header) unsigned header = __cs_num++
+
+#define gfx12_set_reg(reg, value, base_offset) do { \
+   radeon_emit(((reg) - (base_offset)) >> 2); \
+   radeon_emit(value); \
+} while (0)
+
+#define gfx12_opt_set_reg(reg, reg_enum, value, base_offset) do { \
+   unsigned __value = value; \
+   if (!BITSET_TEST(sctx->tracked_regs.reg_saved_mask, (reg_enum)) || \
+       sctx->tracked_regs.reg_value[reg_enum] != __value) { \
+      gfx12_set_reg(reg, __value, base_offset); \
+      BITSET_SET(sctx->tracked_regs.reg_saved_mask, (reg_enum)); \
+      sctx->tracked_regs.reg_value[reg_enum] = __value; \
+   } \
+} while (0)
+
+#define gfx12_opt_set_reg4(reg, reg_enum, v1, v2, v3, v4, base_offset) do { \
+   unsigned __v1 = (v1), __v2 = (v2), __v3 = (v3), __v4 = (v4); \
+   if (!BITSET_TEST_RANGE_INSIDE_WORD(sctx->tracked_regs.reg_saved_mask, \
+                                      (reg_enum), (reg_enum) + 3, 0xf) || \
+       sctx->tracked_regs.reg_value[(reg_enum)] != __v1 || \
+       sctx->tracked_regs.reg_value[(reg_enum) + 1] != __v2 || \
+       sctx->tracked_regs.reg_value[(reg_enum) + 2] != __v3 || \
+       sctx->tracked_regs.reg_value[(reg_enum) + 3] != __v4) { \
+      gfx12_set_reg((reg), __v1, (base_offset)); \
+      gfx12_set_reg((reg) + 4, __v2, (base_offset)); \
+      gfx12_set_reg((reg) + 8, __v3, (base_offset)); \
+      gfx12_set_reg((reg) + 12, __v4, (base_offset)); \
+      BITSET_SET_RANGE_INSIDE_WORD(sctx->tracked_regs.reg_saved_mask, \
+                                   (reg_enum), (reg_enum) + 3); \
+      sctx->tracked_regs.reg_value[(reg_enum)] = __v1; \
+      sctx->tracked_regs.reg_value[(reg_enum) + 1] = __v2; \
+      sctx->tracked_regs.reg_value[(reg_enum) + 2] = __v3; \
+      sctx->tracked_regs.reg_value[(reg_enum) + 3] = __v4; \
+   } \
+} while (0)
+
+#define gfx12_end_regs(header, packet) do { \
+   if ((header) + 1 == __cs_num) { \
+      __cs_num--; /* no registers have been set, back off */ \
+   } else { \
+      unsigned __dw_count = __cs_num - (header) - 2; \
+      __cs_buf[(header)] = PKT3((packet), __dw_count, 0) | PKT3_RESET_FILTER_CAM_S(1); \
+   } \
+} while (0)
+
+/* GFX12 generic packet building helpers for buffered registers. Don't use these directly. */
+#define gfx12_push_reg(reg, value, base_offset, type) do { \
+   unsigned __i = sctx->num_buffered_##type##_regs++; \
+   assert(__i < ARRAY_SIZE(sctx->gfx12.buffered_##type##_regs)); \
+   sctx->gfx12.buffered_##type##_regs[__i].reg_offset = ((reg) - (base_offset)) >> 2; \
+   sctx->gfx12.buffered_##type##_regs[__i].reg_value = value; \
+} while (0)
+
+#define gfx12_opt_push_reg(reg, reg_enum, value, type) do { \
+   unsigned __value = value; \
+   unsigned __reg_enum = reg_enum; \
+   if (!BITSET_TEST(sctx->tracked_regs.reg_saved_mask, (reg_enum)) || \
+       sctx->tracked_regs.reg_value[__reg_enum] != __value) { \
+      gfx12_push_##type##_reg(reg, __value); \
+      BITSET_SET(sctx->tracked_regs.reg_saved_mask, (reg_enum)); \
+      sctx->tracked_regs.reg_value[__reg_enum] = __value; \
+   } \
+} while (0)
+
+/* GFX12 packet building helpers for PAIRS packets. */
+#define gfx12_begin_context_regs() \
+   gfx12_begin_regs(__cs_context_reg_header)
+
+#define gfx12_set_context_reg(reg, value) \
+   gfx12_set_reg(reg, value, SI_CONTEXT_REG_OFFSET)
+
+#define gfx12_opt_set_context_reg(reg, reg_enum, value) \
+   gfx12_opt_set_reg(reg, reg_enum, value, SI_CONTEXT_REG_OFFSET)
+
+#define gfx12_opt_set_context_reg4(reg, reg_enum, v1, v2, v3, v4) \
+   gfx12_opt_set_reg4(reg, reg_enum, v1, v2, v3, v4, SI_CONTEXT_REG_OFFSET)
+
+#define gfx12_end_context_regs() \
+   gfx12_end_regs(__cs_context_reg_header, PKT3_SET_CONTEXT_REG_PAIRS)
+
+/* GFX12 packet building helpers for buffered registers. */
+#define gfx12_push_gfx_sh_reg(reg, value) \
+   gfx12_push_reg(reg, value, SI_SH_REG_OFFSET, gfx_sh)
+
+#define gfx12_push_compute_sh_reg(reg, value) \
+   gfx12_push_reg(reg, value, SI_SH_REG_OFFSET, compute_sh)
+
+#define gfx12_opt_push_gfx_sh_reg(reg, reg_enum, value) \
+   gfx12_opt_push_reg(reg, reg_enum, value, gfx_sh)
+
+#define gfx12_opt_push_compute_sh_reg(reg, reg_enum, value) \
+   gfx12_opt_push_reg(reg, reg_enum, value, compute_sh)
+
 #define radeon_set_or_push_gfx_sh_reg(reg, value) do { \
-   if (GFX_VERSION >= GFX11 && HAS_SH_PAIRS_PACKED) { \
+   if (GFX_VERSION >= GFX12) { \
+      gfx12_push_gfx_sh_reg(reg, value); \
+   } else if (GFX_VERSION >= GFX11 && HAS_SH_PAIRS_PACKED) { \
       gfx11_push_gfx_sh_reg(reg, value); \
    } else { \
       radeon_set_sh_reg_seq(reg, 1); \

src/gallium/drivers/radeonsi/si_clear.c
@@ -707,6 +707,8 @@ static void si_fast_clear(struct si_context *sctx, unsigned *buffers,
    unsigned clear_types = 0;
    unsigned num_pixels = fb->width * fb->height;
 
+   assert(sctx->gfx_level < GFX12);
+
    /* This function is broken in BE, so just disable this path for now */
 #if UTIL_ARCH_BIG_ENDIAN
    return;
@@ -1149,9 +1151,9 @@ static void si_fast_clear(struct si_context *sctx, unsigned *buffers,
    si_execute_clears(sctx, info, num_clears, clear_types, sctx->render_cond_enabled);
 }
 
-static void si_clear(struct pipe_context *ctx, unsigned buffers,
-                     const struct pipe_scissor_state *scissor_state,
-                     const union pipe_color_union *color, double depth, unsigned stencil)
+static void gfx6_clear(struct pipe_context *ctx, unsigned buffers,
+                       const struct pipe_scissor_state *scissor_state,
+                       const union pipe_color_union *color, double depth, unsigned stencil)
 {
    struct si_context *sctx = (struct si_context *)ctx;
    struct pipe_framebuffer_state *fb = &sctx->framebuffer.state;
@@ -1268,6 +1270,44 @@ static void si_clear(struct pipe_context *ctx, unsigned buffers,
       sctx->db_stencil_disable_expclear = false;
       zstex->stencil_cleared_level_mask_once |= BITFIELD_BIT(zsbuf->u.tex.level);
       si_mark_atom_dirty(sctx, &sctx->atoms.s.db_render_state);
+   }
+}
+
+static void gfx12_clear(struct pipe_context *ctx, unsigned buffers,
+                        const struct pipe_scissor_state *scissor_state,
+                        const union pipe_color_union *color, double depth, unsigned stencil)
+{
+   struct si_context *sctx = (struct si_context *)ctx;
+   struct pipe_framebuffer_state *fb = &sctx->framebuffer.state;
+   struct pipe_surface *zsbuf = fb->zsbuf;
+   struct si_texture *zstex = zsbuf ? (struct si_texture *)zsbuf->texture : NULL;
+
+   /* Unset clear flags for non-existent buffers. */
+   for (unsigned i = 0; i < 8; i++) {
+      if (i >= fb->nr_cbufs || !fb->cbufs[i])
+         buffers &= ~(PIPE_CLEAR_COLOR0 << i);
+   }
+   if (!zsbuf)
+      buffers &= ~PIPE_CLEAR_DEPTHSTENCIL;
+   else if (!util_format_has_stencil(util_format_description(zsbuf->format)))
+      buffers &= ~PIPE_CLEAR_STENCIL;
+
+   if (unlikely(sctx->sqtt_enabled)) {
+      if (buffers & PIPE_CLEAR_COLOR)
+         sctx->sqtt_next_event = EventCmdClearColorImage;
+      else if (buffers & PIPE_CLEAR_DEPTHSTENCIL)
+         sctx->sqtt_next_event = EventCmdClearDepthStencilImage;
+   }
+
+   si_blitter_begin(sctx, SI_CLEAR);
+   util_blitter_clear(sctx->blitter, fb->width, fb->height, util_framebuffer_get_num_layers(fb),
+                      buffers, color, depth, stencil, sctx->framebuffer.nr_samples > 1);
+   si_blitter_end(sctx);
+
+   /* This is only used by the driver, not the hw. */
+   if (buffers & PIPE_CLEAR_DEPTH) {
+      zstex->depth_cleared_level_mask |= BITFIELD_BIT(zsbuf->u.tex.level);
+      zstex->depth_clear_value[zsbuf->u.tex.level] = depth;
    }
 }
 
@@ -1459,7 +1499,11 @@ void si_init_clear_functions(struct si_context *sctx)
    sctx->b.clear_texture = u_default_clear_texture;
 
    if (sctx->has_graphics) {
-      sctx->b.clear = si_clear;
+      if (sctx->gfx_level >= GFX12)
+         sctx->b.clear = gfx12_clear;
+      else
+         sctx->b.clear = gfx6_clear;
+
       sctx->b.clear_depth_stencil = si_clear_depth_stencil;
    }
 }

alexvorxx/Mesa-VirGL

commit e4b3848fdebf84b5d7ffaf113f5be8462d045eb6

radeonsi/gfx12: fix a GPU hang due to an invalid packet with window rectangles
I guess incorrect packet interrupts have been enabled, so this started hanging.

radeon_set_context_reg_seq shouldn't be used with gfx12_set_context_reg.

Fixes: f703dfd1bb8 - radeonsi: add gfx12

Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30503>

src/gallium/drivers/radeonsi/si_state_viewport.c
@@ -746,7 +746,6 @@ static void si_emit_window_rectangles(struct si_context *sctx, unsigned index)
       gfx12_opt_set_context_reg(R_02820C_PA_SC_CLIPRECT_RULE, SI_TRACKED_PA_SC_CLIPRECT_RULE, rule);
 
       if (num_rectangles) {
-         radeon_set_context_reg_seq(R_028210_PA_SC_CLIPRECT_0_TL, num_rectangles * 2);
          for (unsigned i = 0; i < num_rectangles; i++) {
             gfx12_set_context_reg(R_028210_PA_SC_CLIPRECT_0_TL + i * 8,
                                   S_028210_TL_X(rects[i].minx) | S_028210_TL_Y(rects[i].miny));