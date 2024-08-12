alexvorxx/Mesa-VirGL

commit 5ab29555cf009e4a53eefa5304efae45c011d755

egl/x11: pass render fd to screen create for zink
Fixes: f43883cf673 ("egl/gbm: rip out DRI_SWRAST")

Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30563>

src/egl/drivers/dri2/egl_dri2.c
@@ -836,7 +836,7 @@ dri2_create_screen(_EGLDisplay *disp)
       }
    }
 
-   int screen_fd = dri2_dpy->swrast || dri2_dpy->kopper ? -1 : dri2_dpy->fd_render_gpu;
+   int screen_fd = dri2_dpy->swrast ? -1 : dri2_dpy->fd_render_gpu;
    dri2_dpy->dri_screen_render_gpu = driCreateNewScreen3(
       0, screen_fd, dri2_dpy->loader_extensions, dri2_dpy->driver_extensions,
       &dri2_dpy->driver_configs, false, disp);

commit c8c46bf90066748d550f492abf6fc03136cde348

egl: fix fd passing on init with zink
this should be passed with gbm but not for any other display type

Fixes: 5ab29555cf0 ("egl/x11: pass render fd to screen create for zink")

Acked-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30588>

src/egl/drivers/dri2/egl_dri2.c
@@ -807,7 +807,13 @@ dri2_create_screen(_EGLDisplay *disp)
       }
    }
 
-   int screen_fd = dri2_dpy->swrast ? -1 : dri2_dpy->fd_render_gpu;
+   int screen_fd = -1;
+   if (!dri2_dpy->swrast) {
+#ifdef HAVE_DRM_PLATFORM
+      if (!dri2_dpy->kopper || dri2_dpy->gbm_dri)
+#endif
+          screen_fd = dri2_dpy->fd_render_gpu;
+   }
    dri2_dpy->dri_screen_render_gpu = driCreateNewScreen3(
       0, screen_fd, dri2_dpy->loader_extensions, type,
       &dri2_dpy->driver_configs, false, disp);