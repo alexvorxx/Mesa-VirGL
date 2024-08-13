alexvorxx/zink-xlib-termux

commit 3d38c9597ff3b687026fa4c664f8b4aa20b97f53

zink: hook up KHR_partial_update
this just requires clamping the renderArea to the passed damage rect,
which is composed by merging all the passed rects into a single large
region

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27867>

src/gallium/drivers/zink/zink_context.c
@@ -2923,6 +2923,8 @@ begin_rendering(struct zink_context *ctx)
       assert(ctx->dynamic_fb.info.renderArea.extent.height <= res->base.b.height0);
       assert(ctx->fb_state.width <= res->base.b.width0);
       assert(ctx->fb_state.height <= res->base.b.height0);
+      if (res->use_damage)
+         ctx->dynamic_fb.info.renderArea = res->damage;
    }
    if (ctx->fb_state.zsbuf && zsbuf_used) {
       struct zink_surface *surf = zink_csurface(ctx->fb_state.zsbuf);

src/gallium/drivers/zink/zink_screen.c
@@ -1534,6 +1534,22 @@ zink_is_format_supported(struct pipe_screen *pscreen,
 }
 
 static void
+zink_set_damage_region(struct pipe_screen *pscreen, struct pipe_resource *pres, unsigned int nrects, const struct pipe_box *rects)
+{
+   struct zink_resource *res = zink_resource(pres);
+
+   for (unsigned i = 0; i < nrects; i++) {
+      int y = pres->height0 - rects[i].y - rects[i].height;
+      res->damage.extent.width = MAX2(res->damage.extent.width, rects[i].x + rects[i].width);
+      res->damage.extent.height = MAX2(res->damage.extent.height, y + rects[i].height);
+      res->damage.offset.x = MIN2(res->damage.offset.x, rects[i].x);
+      res->damage.offset.y = MIN2(res->damage.offset.y, y);
+   }
+
+   res->use_damage = nrects > 0;
+}
+
+static void
 zink_destroy_screen(struct pipe_screen *pscreen)
 {
    struct zink_screen *screen = zink_screen(pscreen);
@@ -1825,6 +1841,7 @@ zink_flush_frontbuffer(struct pipe_screen *pscreen,
          util_queue_fence_wait(&bs->flush_completed);
       }
    }
+   res->use_damage = false;
 
    /* always verify that this was acquired */
    assert(zink_kopper_acquired(res->obj->dt, res->obj->dt_idx));
@@ -3457,6 +3474,7 @@ zink_internal_create_screen(const struct pipe_screen_config *config, int64_t dev
    screen->base.get_sparse_texture_virtual_page_size = zink_get_sparse_texture_virtual_page_size;
    screen->base.get_driver_query_group_info = zink_get_driver_query_group_info;
    screen->base.get_driver_query_info = zink_get_driver_query_info;
+   screen->base.set_damage_region = zink_set_damage_region;
 
    if (screen->info.have_EXT_sample_locations) {
       VkMultisamplePropertiesEXT prop;

src/gallium/drivers/zink/zink_render_pass.c
@@ -662,6 +662,14 @@ begin_render_pass(struct zink_context *ctx)
    rpbi.renderArea.extent.width = fb_state->width;
    rpbi.renderArea.extent.height = fb_state->height;
 
+   if (ctx->fb_state.cbufs[0]) {
+      struct zink_resource *res = zink_resource(ctx->fb_state.cbufs[0]->texture);
+      if (zink_is_swapchain(res)) {
+         if (res->use_damage)
+            rpbi.renderArea = res->damage;
+      }
+   }
+
    VkClearValue clears[PIPE_MAX_COLOR_BUFS + 1] = {0};
    unsigned clear_buffers = 0;
    uint32_t clear_validate = 0;

src/gallium/drivers/zink/zink_types.h
@@ -1360,6 +1360,9 @@ struct zink_resource {
       };
    };
 
+   VkRect2D damage;
+   bool use_damage;
+
    bool copies_warned;
    bool swapchain;
    bool dmabuf;

alexvorxx/Mesa-VirGL

commit a7f64c62037eb33e97e0d4a0f9a27345baa8bcc3

zink: fix partial update handling
* the damage region was not being used correctly (this is a normal rect)
* use_damage was never unset at frame boundary
* original renderArea was never re-set

Fixes: 3d38c9597ff ("zink: hook up KHR_partial_update")

Acked-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30625>

src/gallium/drivers/zink/zink_context.c
@@ -2924,8 +2924,14 @@ begin_rendering(struct zink_context *ctx, bool check_msaa_expand)
    if (has_swapchain) {
       ASSERTED struct zink_resource *res = zink_resource(ctx->fb_state.cbufs[0]->texture);
       zink_render_fixup_swapchain(ctx);
-      if (res->use_damage)
+      if (res->use_damage) {
          ctx->dynamic_fb.info.renderArea = res->damage;
+      } else {
+         ctx->dynamic_fb.info.renderArea.offset.x = 0;
+         ctx->dynamic_fb.info.renderArea.offset.y = 0;
+         ctx->dynamic_fb.info.renderArea.extent.width = ctx->fb_state.width;
+         ctx->dynamic_fb.info.renderArea.extent.height = ctx->fb_state.height;
+      }
       /* clamp for late swapchain resize */
       if (res->base.b.width0 < ctx->dynamic_fb.info.renderArea.extent.width)
          ctx->dynamic_fb.info.renderArea.extent.width = res->base.b.width0;

src/gallium/drivers/zink/zink_kopper.c
@@ -887,6 +887,8 @@ zink_kopper_present_queue(struct zink_screen *screen, struct zink_resource *res,
       kopper_present(cpi, screen, -1);
    }
    res->obj->indefinite_acquire = false;
+   res->use_damage = false;
+   memset(&res->damage, 0, sizeof(res->damage));
    cdt->swapchain->images[res->obj->dt_idx].acquired = NULL;
    res->obj->dt_idx = UINT32_MAX;
 }

src/gallium/drivers/zink/zink_screen.c
@@ -1545,10 +1545,25 @@ zink_set_damage_region(struct pipe_screen *pscreen, struct pipe_resource *pres, 
 
    for (unsigned i = 0; i < nrects; i++) {
       int y = pres->height0 - rects[i].y - rects[i].height;
-      res->damage.extent.width = MAX2(res->damage.extent.width, rects[i].x + rects[i].width);
-      res->damage.extent.height = MAX2(res->damage.extent.height, y + rects[i].height);
-      res->damage.offset.x = MIN2(res->damage.offset.x, rects[i].x);
-      res->damage.offset.y = MIN2(res->damage.offset.y, y);
+      /* convert back to coord-based rects to use coordinate calcs */
+      struct u_rect currect = {
+         .x0 = res->damage.offset.x,
+         .y0 = res->damage.offset.y,
+         .x1 = res->damage.offset.x + res->damage.extent.width,
+         .y1 = res->damage.offset.y + res->damage.extent.height,
+      };
+      struct u_rect newrect = {
+         .x0 = rects[i].x,
+         .y0 = y,
+         .x1 = rects[i].x + rects[i].width,
+         .y1 = y + rects[i].height,
+      };
+      struct u_rect u;
+      u_rect_union(&u, &currect, &newrect);
+      res->damage.extent.width = u.y1 - u.y0;
+      res->damage.extent.height = u.x1 - u.x0;
+      res->damage.offset.x = u.x0;
+      res->damage.offset.y = u.y0;
    }
 
    res->use_damage = nrects > 0;