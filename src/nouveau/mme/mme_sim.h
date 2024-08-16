50a030b02a67df44aa15c4ca2e31dc7f467a8413
Faith Ekstrand <faith.ekstrand@collabora.com>
2024-08-16 16:05:53 -0500

    nouveau/mme: Add a generic simulator function
    This one takes a devinfo and decodes and simulates the shader with
    whichever sim is appropriate.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30703>

--- src/nouveau/mme/mme_sim.h
+++ src/nouveau/mme/mme_sim.h
@@ -19,6 +19,11 @@ struct mme_sim_state_ops {
    uint32_t *(*map_dram)(void *handler, uint32_t idx);
 };
 
+void mme_sim_core(const struct nv_device_info *devinfo,
+                  size_t macro_size, const void *macro,
+                  const struct mme_sim_state_ops *state_ops,
+                  void *state_handler);
+
 #ifdef __cplusplus
 }
 #endif
