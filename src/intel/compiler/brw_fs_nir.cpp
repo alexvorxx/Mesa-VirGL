aerisarn/mesa-uwp

commit 43169dbbe5f963ca47d51873f6639fbe3fd665b0

intel/compiler: Support 16 bit float ops
Signed-off-by: Rohan Garg <rohan.garg@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17988>

src/intel/compiler/brw_fs_nir.cpp
@@ -5920,6 +5920,18 @@ fs_visitor::nir_emit_intrinsic(const fs_builder &bld, nir_intrinsic_instr *instr
    }
 }
 
+static fs_reg
+expand_to_32bit(const fs_builder &bld, const fs_reg &src)
+{
+   if (type_sz(src.type) == 2) {
+      fs_reg src32 = bld.vgrf(BRW_REGISTER_TYPE_UD);
+      bld.MOV(src32, retype(src, BRW_REGISTER_TYPE_UW));
+      return src32;
+   } else {
+      return src;
+   }
+}
+
 void
 fs_visitor::nir_emit_ssbo_atomic(const fs_builder &bld,
                                  int op, nir_intrinsic_instr *instr)
@@ -5930,7 +5942,8 @@ fs_visitor::nir_emit_ssbo_atomic(const fs_builder &bld,
     * descriptors provided for Qword atomic ops except for A64 messages.
     */
    assert(nir_dest_bit_size(instr->dest) == 32 ||
-          (nir_dest_bit_size(instr->dest) == 64 && devinfo->has_lsc));
+          (nir_dest_bit_size(instr->dest) == 64 && devinfo->has_lsc) ||
+          (nir_dest_bit_size(instr->dest) == 16 && devinfo->has_lsc));
 
    fs_reg dest;
    if (nir_intrinsic_infos[instr->intrinsic].has_dest)
@@ -5945,11 +5958,14 @@ fs_visitor::nir_emit_ssbo_atomic(const fs_builder &bld,
 
    fs_reg data;
    if (op != BRW_AOP_INC && op != BRW_AOP_DEC && op != BRW_AOP_PREDEC)
-      data = get_nir_src(instr->src[2]);
+      data = expand_to_32bit(bld, get_nir_src(instr->src[2]));
 
    if (op == BRW_AOP_CMPWR) {
       fs_reg tmp = bld.vgrf(data.type, 2);
-      fs_reg sources[2] = { data, get_nir_src(instr->src[3]) };
+      fs_reg sources[2] = {
+         data,
+         expand_to_32bit(bld, get_nir_src(instr->src[3]))
+      };
       bld.LOAD_PAYLOAD(tmp, sources, 2, 0);
       data = tmp;
    }
@@ -5957,8 +5973,25 @@ fs_visitor::nir_emit_ssbo_atomic(const fs_builder &bld,
 
    /* Emit the actual atomic operation */
 
-   bld.emit(SHADER_OPCODE_UNTYPED_ATOMIC_LOGICAL,
-            dest, srcs, SURFACE_LOGICAL_NUM_SRCS);
+   switch (nir_dest_bit_size(instr->dest)) {
+      case 16: {
+         fs_reg dest32 = bld.vgrf(BRW_REGISTER_TYPE_UD);
+         bld.emit(SHADER_OPCODE_UNTYPED_ATOMIC_LOGICAL,
+                  retype(dest32, dest.type),
+                  srcs, SURFACE_LOGICAL_NUM_SRCS);
+         bld.MOV(retype(dest, BRW_REGISTER_TYPE_UW),
+                 retype(dest32, BRW_REGISTER_TYPE_UD));
+         break;
+      }
+
+      case 32:
+      case 64:
+         bld.emit(SHADER_OPCODE_UNTYPED_ATOMIC_LOGICAL,
+                  dest, srcs, SURFACE_LOGICAL_NUM_SRCS);
+         break;
+      default:
+         unreachable("Unsupported bit size");
+   }
 }
 
 void
@@ -5976,19 +6009,38 @@ fs_visitor::nir_emit_ssbo_atomic_float(const fs_builder &bld,
    srcs[SURFACE_LOGICAL_SRC_IMM_ARG] = brw_imm_ud(op);
    srcs[SURFACE_LOGICAL_SRC_ALLOW_SAMPLE_MASK] = brw_imm_ud(1);
 
-   fs_reg data = get_nir_src(instr->src[2]);
+   fs_reg data = expand_to_32bit(bld, get_nir_src(instr->src[2]));
    if (op == BRW_AOP_FCMPWR) {
       fs_reg tmp = bld.vgrf(data.type, 2);
-      fs_reg sources[2] = { data, get_nir_src(instr->src[3]) };
+      fs_reg sources[2] = {
+         data,
+         expand_to_32bit(bld, get_nir_src(instr->src[3]))
+      };
       bld.LOAD_PAYLOAD(tmp, sources, 2, 0);
       data = tmp;
    }
    srcs[SURFACE_LOGICAL_SRC_DATA] = data;
 
    /* Emit the actual atomic operation */
+   switch (nir_dest_bit_size(instr->dest)) {
+      case 16: {
+         fs_reg dest32 = bld.vgrf(BRW_REGISTER_TYPE_UD);
+         bld.emit(SHADER_OPCODE_UNTYPED_ATOMIC_FLOAT_LOGICAL,
+                  retype(dest32, dest.type),
+                  srcs, SURFACE_LOGICAL_NUM_SRCS);
+         bld.MOV(retype(dest, BRW_REGISTER_TYPE_UW),
+                 retype(dest32, BRW_REGISTER_TYPE_UD));
+         break;
+      }
 
-   bld.emit(SHADER_OPCODE_UNTYPED_ATOMIC_FLOAT_LOGICAL,
-            dest, srcs, SURFACE_LOGICAL_NUM_SRCS);
+      case 32:
+      case 64:
+         bld.emit(SHADER_OPCODE_UNTYPED_ATOMIC_FLOAT_LOGICAL,
+                  dest, srcs, SURFACE_LOGICAL_NUM_SRCS);
+         break;
+      default:
+         unreachable("Unsupported bit size");
+   }
 }
 
 void
@@ -6007,10 +6059,13 @@ fs_visitor::nir_emit_shared_atomic(const fs_builder &bld,
 
    fs_reg data;
    if (op != BRW_AOP_INC && op != BRW_AOP_DEC && op != BRW_AOP_PREDEC)
-      data = get_nir_src(instr->src[1]);
+      data = expand_to_32bit(bld, get_nir_src(instr->src[1]));
    if (op == BRW_AOP_CMPWR) {
       fs_reg tmp = bld.vgrf(data.type, 2);
-      fs_reg sources[2] = { data, get_nir_src(instr->src[2]) };
+      fs_reg sources[2] = {
+         expand_to_32bit(bld, data),
+         expand_to_32bit(bld, get_nir_src(instr->src[2]))
+      };
       bld.LOAD_PAYLOAD(tmp, sources, 2, 0);
       data = tmp;
    }
@@ -6030,8 +6085,25 @@ fs_visitor::nir_emit_shared_atomic(const fs_builder &bld,
 
    /* Emit the actual atomic operation operation */
 
-   bld.emit(SHADER_OPCODE_UNTYPED_ATOMIC_LOGICAL,
-            dest, srcs, SURFACE_LOGICAL_NUM_SRCS);
+   switch (nir_dest_bit_size(instr->dest)) {
+      case 16: {
+         fs_reg dest32 = bld.vgrf(BRW_REGISTER_TYPE_UD);
+         bld.emit(SHADER_OPCODE_UNTYPED_ATOMIC_LOGICAL,
+                  retype(dest32, dest.type),
+                  srcs, SURFACE_LOGICAL_NUM_SRCS);
+         bld.MOV(retype(dest, BRW_REGISTER_TYPE_UW),
+                 retype(dest32, BRW_REGISTER_TYPE_UD));
+         break;
+      }
+
+      case 32:
+      case 64:
+         bld.emit(SHADER_OPCODE_UNTYPED_ATOMIC_LOGICAL,
+                  dest, srcs, SURFACE_LOGICAL_NUM_SRCS);
+         break;
+      default:
+         unreachable("Unsupported bit size");
+   }
 }
 
 void
@@ -6048,10 +6120,13 @@ fs_visitor::nir_emit_shared_atomic_float(const fs_builder &bld,
    srcs[SURFACE_LOGICAL_SRC_IMM_ARG] = brw_imm_ud(op);
    srcs[SURFACE_LOGICAL_SRC_ALLOW_SAMPLE_MASK] = brw_imm_ud(1);
 
-   fs_reg data = get_nir_src(instr->src[1]);
+   fs_reg data = expand_to_32bit(bld, get_nir_src(instr->src[1]));
    if (op == BRW_AOP_FCMPWR) {
       fs_reg tmp = bld.vgrf(data.type, 2);
-      fs_reg sources[2] = { data, get_nir_src(instr->src[2]) };
+      fs_reg sources[2] = {
+         data,
+         expand_to_32bit(bld, get_nir_src(instr->src[2]))
+      };
       bld.LOAD_PAYLOAD(tmp, sources, 2, 0);
       data = tmp;
    }
@@ -6071,20 +6146,26 @@ fs_visitor::nir_emit_shared_atomic_float(const fs_builder &bld,
 
    /* Emit the actual atomic operation operation */
 
-   bld.emit(SHADER_OPCODE_UNTYPED_ATOMIC_FLOAT_LOGICAL,
-            dest, srcs, SURFACE_LOGICAL_NUM_SRCS);
-}
+   switch (nir_dest_bit_size(instr->dest)) {
+      case 16: {
+         fs_reg dest32 = bld.vgrf(BRW_REGISTER_TYPE_UD);
+         bld.emit(SHADER_OPCODE_UNTYPED_ATOMIC_FLOAT_LOGICAL,
+                  retype(dest32, dest.type),
+                  srcs, SURFACE_LOGICAL_NUM_SRCS);
+         bld.MOV(retype(dest, BRW_REGISTER_TYPE_UW),
+                 retype(dest32, BRW_REGISTER_TYPE_UD));
+         break;
+      }
 
-static fs_reg
-expand_to_32bit(const fs_builder &bld, const fs_reg &src)
-{
-   if (type_sz(src.type) == 2) {
-      fs_reg src32 = bld.vgrf(BRW_REGISTER_TYPE_UD);
-      bld.MOV(src32, retype(src, BRW_REGISTER_TYPE_UW));
-      return src32;
-   } else {
-      return src;
+      case 32:
+      case 64:
+         bld.emit(SHADER_OPCODE_UNTYPED_ATOMIC_FLOAT_LOGICAL,
+                  dest, srcs, SURFACE_LOGICAL_NUM_SRCS);
+         break;
+      default:
+         unreachable("Unsupported bit size");
    }
+
 }
 
 void
@@ -6120,7 +6201,8 @@ fs_visitor::nir_emit_global_atomic(const fs_builder &bld,
    switch (nir_dest_bit_size(instr->dest)) {
    case 16: {
       fs_reg dest32 = bld.vgrf(BRW_REGISTER_TYPE_UD);
-      bld.emit(SHADER_OPCODE_A64_UNTYPED_ATOMIC_INT16_LOGICAL, dest32,
+      bld.emit(SHADER_OPCODE_A64_UNTYPED_ATOMIC_INT16_LOGICAL,
+               retype(dest32, dest.type),
                srcs, A64_LOGICAL_NUM_SRCS);
       bld.MOV(retype(dest, BRW_REGISTER_TYPE_UW), dest32);
       break;
@@ -6169,7 +6251,8 @@ fs_visitor::nir_emit_global_atomic_float(const fs_builder &bld,
    switch (nir_dest_bit_size(instr->dest)) {
    case 16: {
       fs_reg dest32 = bld.vgrf(BRW_REGISTER_TYPE_UD);
-      bld.emit(SHADER_OPCODE_A64_UNTYPED_ATOMIC_FLOAT16_LOGICAL, dest32,
+      bld.emit(SHADER_OPCODE_A64_UNTYPED_ATOMIC_FLOAT16_LOGICAL,
+               retype(dest32, dest.type),
                srcs, A64_LOGICAL_NUM_SRCS);
       bld.MOV(retype(dest, BRW_REGISTER_TYPE_UW), dest32);
       break;

src/intel/compiler/brw_lower_logical_sends.cpp
@@ -1695,6 +1695,7 @@ lower_lsc_surface_logical_send(const fs_builder &bld, fs_inst *inst)
    const unsigned addr_sz = inst->components_read(SURFACE_LOGICAL_SRC_ADDRESS);
    const unsigned src_comps = inst->components_read(SURFACE_LOGICAL_SRC_DATA);
    const unsigned src_sz = type_sz(src.type);
+   const unsigned dst_sz = type_sz(inst->dst.type);
 
    const bool has_side_effects = inst->has_side_effects();
 
@@ -1758,10 +1759,11 @@ lower_lsc_surface_logical_send(const fs_builder &bld, fs_inst *inst)
          inst->opcode == SHADER_OPCODE_UNTYPED_ATOMIC_FLOAT_LOGICAL ?
          brw_atomic_op_to_lsc_fatomic_op(arg.ud) :
          brw_atomic_op_to_lsc_atomic_op(arg.ud);
+
       inst->desc = lsc_msg_desc(devinfo, opcode, inst->exec_size,
                                 surf_type, LSC_ADDR_SIZE_A32,
                                 1 /* num_coordinates */,
-                                lsc_bits_to_data_size(src_sz * 8),
+                                lsc_bits_to_data_size(dst_sz * 8),
                                 1 /* num_channels */,
                                 false /* transpose */,
                                 LSC_CACHE_STORE_L1UC_L3WB,
@@ -2032,6 +2034,7 @@ lower_lsc_a64_logical_send(const fs_builder &bld, fs_inst *inst)
    const fs_reg &addr = inst->src[A64_LOGICAL_ADDRESS];
    const fs_reg &src = inst->src[A64_LOGICAL_SRC];
    const unsigned src_sz = type_sz(src.type);
+   const unsigned dst_sz = type_sz(inst->dst.type);
 
    const unsigned src_comps = inst->components_read(1);
    assert(inst->src[A64_LOGICAL_ARG].file == IMM);
@@ -2102,7 +2105,7 @@ lower_lsc_a64_logical_send(const fs_builder &bld, fs_inst *inst)
       inst->desc = lsc_msg_desc(devinfo, opcode, inst->exec_size,
                                 LSC_ADDR_SURFTYPE_FLAT, LSC_ADDR_SIZE_A64,
                                 1 /* num_coordinates */,
-                                lsc_bits_to_data_size(src_sz * 8),
+                                lsc_bits_to_data_size(dst_sz * 8),
                                 1 /* num_channels */,
                                 false /* transpose */,
                                 LSC_CACHE_STORE_L1UC_L3WB,

alexvorxx/Mesa-VirGL

commit 71ca8529c5ffa54787f30e6e8b1f9f2971ff649e

intel/brw/gfx12.5+: Fix IR of sub-dword atomic LSC operations.
We were currently emitting logical atomic instructions with a packed
destination region for sub-dword LSC atomics, along the lines of:

> untyped_atomic_logical(32) dst<1>:HF, ...

However, these instructions use an LSC data size D16U32, which means
that the 16b data on the return payload is expanded to 32b by the LSC
shared function, so we were lying to the compiler about the location
of the individual channels on the return payload, its execution
masking, etc.  This is why the hacks that manually set the
'inst->size_written' of the instruction were required.

In some cases this worked, but any non-trivial manipulation of the
instruction destination by lowering or optimization passes could have
led to corruption, as has been reproduced in deqp-vk during
lower_simd_width() for shaders that use 16-bit atomics in SIMD32
dispatch mode.

Note that LSC sub-dword reads aren't affected by this because they use
raw UD destinations and specify the actual bit size of the operation
datatype as the immediate SURFACE_LOGICAL_SRC_IMM_ARG, which doesn't
work for atomic operations since that immediate specifies the atomic
opcode.

Instead, have the logical operation implement the behavior of 16-bit
destinations correctly instead of silently replacing the 16-bit region
with an inconsistent 32-bit region -- This is done by emitting the MOV
instructions used to pack the data from the UD temporary into the
packed destination from the lower_logical_sends() pass instead of from
the NIR translation pass.

Fixes: 43169dbbe5f96 ("intel/compiler: Support 16 bit float ops")
Reviewed-by: Sagar Ghuge <sagar.ghuge@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30683>

src/intel/compiler/brw_fs_nir.cpp
@@ -8165,32 +8165,9 @@ fs_nir_emit_surface_atomic(nir_to_brw_state &ntb, const fs_builder &bld,
    }
    srcs[SURFACE_LOGICAL_SRC_DATA] = data;
 
-   fs_inst *inst;
-   unsigned size_written = 0;
    /* Emit the actual atomic operation */
-   switch (instr->def.bit_size) {
-      case 16: {
-         brw_reg dest32 = bld.vgrf(BRW_TYPE_UD);
-         inst = bld.emit(SHADER_OPCODE_UNTYPED_ATOMIC_LOGICAL,
-                         retype(dest32, dest.type),
-                         srcs, SURFACE_LOGICAL_NUM_SRCS);
-         size_written = dest32.component_size(inst->exec_size);
-         bld.MOV(retype(dest, BRW_TYPE_UW), dest32);
-         break;
-      }
-
-      case 32:
-      case 64:
-         inst = bld.emit(SHADER_OPCODE_UNTYPED_ATOMIC_LOGICAL,
-                         dest, srcs, SURFACE_LOGICAL_NUM_SRCS);
-         size_written = dest.component_size(inst->exec_size);
-         break;
-      default:
-         unreachable("Unsupported bit size");
-   }
-
-   assert(size_written);
-   inst->size_written = size_written * instr->def.num_components;
+   bld.emit(SHADER_OPCODE_UNTYPED_ATOMIC_LOGICAL, dest, srcs,
+            SURFACE_LOGICAL_NUM_SRCS);
 }
 
 static void
@@ -8224,30 +8201,8 @@ fs_nir_emit_global_atomic(nir_to_brw_state &ntb, const fs_builder &bld,
    srcs[A64_LOGICAL_ARG] = brw_imm_ud(op);
    srcs[A64_LOGICAL_ENABLE_HELPERS] = brw_imm_ud(0);
 
-   fs_inst *inst;
-   unsigned size_written = 0;
-   switch (instr->def.bit_size) {
-   case 16: {
-      brw_reg dest32 = bld.vgrf(BRW_TYPE_UD);
-      inst = bld.emit(SHADER_OPCODE_A64_UNTYPED_ATOMIC_LOGICAL,
-                      retype(dest32, dest.type),
-                      srcs, A64_LOGICAL_NUM_SRCS);
-      size_written = dest32.component_size(inst->exec_size);
-      bld.MOV(retype(dest, BRW_TYPE_UW), dest32);
-      break;
-   }
-   case 32:
-   case 64:
-      inst = bld.emit(SHADER_OPCODE_A64_UNTYPED_ATOMIC_LOGICAL, dest,
-                      srcs, A64_LOGICAL_NUM_SRCS);
-      size_written = dest.component_size(inst->exec_size);
-      break;
-   default:
-      unreachable("Unsupported bit size");
-   }
-
-   assert(size_written);
-   inst->size_written = size_written * instr->def.num_components;
+   bld.emit(SHADER_OPCODE_A64_UNTYPED_ATOMIC_LOGICAL, dest,
+            srcs, A64_LOGICAL_NUM_SRCS);
 }
 
 static void

src/intel/compiler/brw_lower_logical_sends.cpp
@@ -1653,7 +1653,8 @@ lsc_bits_to_data_size(unsigned bit_size)
 }
 
 static void
-lower_lsc_surface_logical_send(const fs_builder &bld, fs_inst *inst)
+lower_lsc_surface_logical_send(bblock_t *block, const fs_builder &bld,
+                               fs_inst *inst)
 {
    const brw_compiler *compiler = bld.shader->compiler;
    const intel_device_info *devinfo = bld.shader->devinfo;
@@ -1809,6 +1810,20 @@ lower_lsc_surface_logical_send(const fs_builder &bld, fs_inst *inst)
    inst->send_is_volatile = !has_side_effects;
    inst->send_ex_bso = surf_type == LSC_ADDR_SURFTYPE_BSS &&
                        compiler->extended_bindless_surface_offset;
+
+   /* Messages with destination datatypes narrower than a dword use a
+    * D*32 LSC data size, update the destination to use a temporary of
+    * the raw (UD) return payload datatype.
+    */
+   if (dst_sz < 4) {
+      assert(lsc_data_size_bytes(lsc_bits_to_data_size(dst_sz * 8)) == 4);
+      assert(inst->size_written == inst->dst.component_size(inst->exec_size));
+      const brw_reg dest32 = bld.vgrf(BRW_TYPE_UD);
+      const brw_reg_type t = brw_int_type(dst_sz, false);
+      bld.at(block, inst->next).MOV(retype(inst->dst, t), dest32);
+      inst->dst = dest32;
+      inst->size_written = inst->dst.component_size(inst->exec_size);
+   }
 
    inst->resize_sources(4);
 
@@ -2032,7 +2047,7 @@ emit_fragment_mask(const fs_builder &bld, fs_inst *inst)
 }
 
 static void
-lower_lsc_a64_logical_send(const fs_builder &bld, fs_inst *inst)
+lower_lsc_a64_logical_send(bblock_t *block, const fs_builder &bld, fs_inst *inst)
 {
    const intel_device_info *devinfo = bld.shader->devinfo;
 
@@ -2143,6 +2158,20 @@ lower_lsc_a64_logical_send(const fs_builder &bld, fs_inst *inst)
    inst->header_size = 0;
    inst->send_has_side_effects = has_side_effects;
    inst->send_is_volatile = !has_side_effects;
+
+   /* Messages with destination datatypes narrower than a dword use a
+    * D*32 LSC data size, update the destination to use a temporary of
+    * the raw (UD) return payload datatype.
+    */
+   if (dst_sz < 4) {
+      assert(lsc_data_size_bytes(lsc_bits_to_data_size(dst_sz * 8)) == 4);
+      assert(inst->size_written == inst->dst.component_size(inst->exec_size));
+      const brw_reg dest32 = bld.vgrf(BRW_TYPE_UD);
+      const brw_reg_type t = brw_int_type(dst_sz, false);
+      bld.at(block, inst->next).MOV(retype(inst->dst, t), dest32);
+      inst->dst = dest32;
+      inst->size_written = inst->dst.component_size(inst->exec_size);
+   }
 
    /* Set up SFID and descriptors */
    inst->sfid = GFX12_SFID_UGM;
@@ -2805,7 +2834,7 @@ brw_fs_lower_logical_sends(fs_visitor &s)
       case SHADER_OPCODE_DWORD_SCATTERED_READ_LOGICAL:
       case SHADER_OPCODE_DWORD_SCATTERED_WRITE_LOGICAL:
          if (devinfo->has_lsc)
-            lower_lsc_surface_logical_send(ibld, inst);
+            lower_lsc_surface_logical_send(block, ibld, inst);
          else
             lower_surface_logical_send(ibld, inst);
          break;
@@ -2814,7 +2843,7 @@ brw_fs_lower_logical_sends(fs_visitor &s)
       case SHADER_OPCODE_TYPED_SURFACE_WRITE_LOGICAL:
       case SHADER_OPCODE_TYPED_ATOMIC_LOGICAL:
          devinfo->ver >= 20 && devinfo->has_lsc ?
-            lower_lsc_surface_logical_send(ibld, inst) :
+            lower_lsc_surface_logical_send(block, ibld, inst) :
             lower_surface_logical_send(ibld, inst);
          break;
 
@@ -2836,7 +2865,7 @@ brw_fs_lower_logical_sends(fs_visitor &s)
       case SHADER_OPCODE_A64_UNALIGNED_OWORD_BLOCK_READ_LOGICAL:
       case SHADER_OPCODE_A64_OWORD_BLOCK_WRITE_LOGICAL:
          if (devinfo->has_lsc) {
-            lower_lsc_a64_logical_send(ibld, inst);
+            lower_lsc_a64_logical_send(block, ibld, inst);
             break;
          }
          lower_a64_logical_send(ibld, inst);