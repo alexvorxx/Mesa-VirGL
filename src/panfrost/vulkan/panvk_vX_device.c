aedb00ca083e370bdf22ae04bda34426bffb6bb4
Boris Brezillon <boris.brezillon@collabora.com>
2024-08-23 17:40:45 +0200

    panvk: Set the submit mode to THREADED_ON_DEMAND
    It's needed for wait-before-submit, which drm_syncobjs don't
    support.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
    Reviewed-by: Rebecca Mckeever <rebecca.mckeever@collabora.com>
    Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
    Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
    Reviewed-by: John Anthony <john.anthony@arm.com>
    Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30969>

--- src/panfrost/vulkan/panvk_vX_device.c
+++ src/panfrost/vulkan/panvk_vX_device.c
@@ -238,6 +238,7 @@ panvk_per_arch(create_device)(struct panvk_physical_device *physical_device,
    device->vk.command_dispatch_table = &device->cmd_dispatch;
    device->vk.command_buffer_ops = &panvk_per_arch(cmd_buffer_ops);
    device->vk.shader_ops = &panvk_per_arch(device_shader_ops);
+   device->vk.submit_mode = VK_QUEUE_SUBMIT_MODE_THREADED_ON_DEMAND;
 
    device->kmod.allocator = (struct pan_kmod_allocator){
       .zalloc = panvk_kmod_zalloc,
