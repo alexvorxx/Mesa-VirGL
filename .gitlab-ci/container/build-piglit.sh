e54440d15e922e428454ae64f92d62dd42814728
Collabora's Gfx CI Team <gfx-ci-bot-collabora@contact.daniels.if.required.fdo.invalid>
2024-08-24 00:06:57 +0000

    Uprev Piglit to a3826de3c26a279599d15b018a9a3e75ca46f4f8
    https://gitlab.freedesktop.org/mesa/piglit/-/compare/6533b66878c8cb9361c554b7ff940518ab12c816...a3826de3c26a279599d15b018a9a3e75ca46f4f8
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30825>

--- .gitlab-ci/container/build-piglit.sh
+++ .gitlab-ci/container/build-piglit.sh
@@ -8,7 +8,7 @@ set -ex
 # DEBIAN_TEST_VK_TAG
 # KERNEL_ROOTFS_TAG
 
-REV="6533b66878c8cb9361c554b7ff940518ab12c816"
+REV="a3826de3c26a279599d15b018a9a3e75ca46f4f8"
 
 git clone https://gitlab.freedesktop.org/mesa/piglit.git --single-branch --no-checkout /piglit
 pushd /piglit
