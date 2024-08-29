d14b27b7fee74cfd9ee31dbf40a5464f83bccfb9
Daniel Stone <daniels@collabora.com>
2024-08-29 18:49:26 +0100

    ci/libdrm: Make build-libdrm safe for set -u
    Signed-off-by: Daniel Stone <daniels@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31281>

--- .gitlab-ci/container/build-libdrm.sh
+++ .gitlab-ci/container/build-libdrm.sh
@@ -3,7 +3,7 @@
 # from https://gitlab.freedesktop.org/gfx-ci/ci-deb-repo - see PKG_REPO_REV)
 # shellcheck disable=SC2086 # we want word splitting
 
-set -ex
+set -uex
 
 export LIBDRM_VERSION=libdrm-2.4.122
 
@@ -11,7 +11,7 @@ curl -L -O --retry 4 -f --retry-all-errors --retry-delay 60 \
     https://dri.freedesktop.org/libdrm/"$LIBDRM_VERSION".tar.xz
 tar -xvf "$LIBDRM_VERSION".tar.xz && rm "$LIBDRM_VERSION".tar.xz
 cd "$LIBDRM_VERSION"
-meson setup build -D vc4=disabled -D freedreno=disabled -D etnaviv=disabled $EXTRA_MESON_ARGS
+meson setup build -D vc4=disabled -D freedreno=disabled -D etnaviv=disabled ${EXTRA_MESON_ARGS:-}
 meson install -C build
 cd ..
 rm -rf "$LIBDRM_VERSION"
