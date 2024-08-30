898012ba111939521ab1a1afd4e1363821d14144
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2024-08-30 12:50:40 -0400

    egl: use more precise conditional for passing fd through to dri screen create
    kms_swrast is included in the swrast conditional and needs a fd,
    and there is a flag which indicates pure swrast which can be used to
    ensure this case is detected
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30951>

--- src/egl/drivers/dri2/egl_dri2.c
+++ src/egl/drivers/dri2/egl_dri2.c
@@ -806,7 +806,7 @@ dri2_create_screen(_EGLDisplay *disp)
       }
    }
 
-   int screen_fd = dri2_dpy->swrast ? -1 : dri2_dpy->fd_render_gpu;
+   int screen_fd = dri2_dpy->swrast_not_kms ? -1 : dri2_dpy->fd_render_gpu;
    dri2_dpy->dri_screen_render_gpu = driCreateNewScreen3(
       0, screen_fd, dri2_dpy->loader_extensions, type,
       &dri2_dpy->driver_configs, false, dri2_dpy->multibuffers_available, disp);
