alexvorxx/Mesa-VirGL

commit e268ed0613f048ee9896325d3b9ed5ce10162d8e

d3d12: Video Encode HEVC to use direct DPB from frontend
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30908>

src/gallium/drivers/d3d12/d3d12_video_enc_hevc.cpp
@@ -358,23 +358,6 @@ d3d12_video_encoder_update_current_frame_pic_params_info_hevc(struct d3d12_video
    }
 
    picParams.pHEVCPicData->slice_pic_parameter_set_id = pHEVCBitstreamBuilder->get_active_pps().pps_pic_parameter_set_id;
-   picParams.pHEVCPicData->FrameType = d3d12_video_encoder_convert_frame_type_hevc(hevcPic->picture_type);
-   picParams.pHEVCPicData->PictureOrderCountNumber = hevcPic->pic_order_cnt;
-
-   picParams.pHEVCPicData->List0ReferenceFramesCount = 0;
-   picParams.pHEVCPicData->pList0ReferenceFrames = nullptr;
-   picParams.pHEVCPicData->List1ReferenceFramesCount = 0;
-   picParams.pHEVCPicData->pList1ReferenceFrames = nullptr;
-
-   if (picParams.pHEVCPicData->FrameType == D3D12_VIDEO_ENCODER_FRAME_TYPE_HEVC_P_FRAME) {
-      picParams.pHEVCPicData->List0ReferenceFramesCount = hevcPic->num_ref_idx_l0_active_minus1 + 1;
-      picParams.pHEVCPicData->pList0ReferenceFrames = hevcPic->ref_idx_l0_list;
-   } else if (picParams.pHEVCPicData->FrameType == D3D12_VIDEO_ENCODER_FRAME_TYPE_HEVC_B_FRAME) {
-      picParams.pHEVCPicData->List0ReferenceFramesCount = hevcPic->num_ref_idx_l0_active_minus1 + 1;
-      picParams.pHEVCPicData->pList0ReferenceFrames = hevcPic->ref_idx_l0_list;
-      picParams.pHEVCPicData->List1ReferenceFramesCount = hevcPic->num_ref_idx_l1_active_minus1 + 1;
-      picParams.pHEVCPicData->pList1ReferenceFrames = hevcPic->ref_idx_l1_list;
-   }
 
    if ((pD3D12Enc->m_currentEncodeConfig.m_encoderCodecSpecificConfigDesc.m_HEVCConfig.ConfigurationFlags 
       & D3D12_VIDEO_ENCODER_CODEC_CONFIGURATION_HEVC_FLAG_ALLOW_REQUEST_INTRA_CONSTRAINED_SLICES) != 0)
@@ -393,33 +376,6 @@ d3d12_video_encoder_update_current_frame_pic_params_info_hevc(struct d3d12_video
          pD3D12Enc->m_currentEncodeConfig.m_encoderRateControlDesc.m_pRateControlQPMap8Bit);
       picParams.pHEVCPicData->pRateControlQPMap = pD3D12Enc->m_currentEncodeConfig.m_encoderRateControlDesc.m_pRateControlQPMap8Bit.data();
       picParams.pHEVCPicData->QPMapValuesCount = pD3D12Enc->m_currentEncodeConfig.m_encoderRateControlDesc.m_pRateControlQPMap8Bit.size();
-   }
-}
-
-D3D12_VIDEO_ENCODER_FRAME_TYPE_HEVC
-d3d12_video_encoder_convert_frame_type_hevc(enum pipe_h2645_enc_picture_type picType)
-{
-   switch (picType) {
-      case PIPE_H2645_ENC_PICTURE_TYPE_P:
-      {
-         return D3D12_VIDEO_ENCODER_FRAME_TYPE_HEVC_P_FRAME;
-      } break;
-      case PIPE_H2645_ENC_PICTURE_TYPE_B:
-      {
-         return D3D12_VIDEO_ENCODER_FRAME_TYPE_HEVC_B_FRAME;
-      } break;
-      case PIPE_H2645_ENC_PICTURE_TYPE_I:
-      {
-         return D3D12_VIDEO_ENCODER_FRAME_TYPE_HEVC_I_FRAME;
-      } break;
-      case PIPE_H2645_ENC_PICTURE_TYPE_IDR:
-      {
-         return D3D12_VIDEO_ENCODER_FRAME_TYPE_HEVC_IDR_FRAME;
-      } break;
-      default:
-      {
-         unreachable("Unsupported pipe_h2645_enc_picture_type");
-      } break;
    }
 }
 

src/gallium/drivers/d3d12/d3d12_video_enc.cpp
@@ -352,7 +352,7 @@ d3d12_video_encoder_uses_direct_dpb(enum pipe_video_format codec)
 #if VIDEO_CODEC_H265ENC
       case PIPE_VIDEO_FORMAT_HEVC:
       {
-         return false;
+         return true;
       } break;
 #endif
 #if VIDEO_CODEC_AV1ENC
@@ -606,19 +606,7 @@ d3d12_video_encoder_create_reference_picture_manager(struct d3d12_video_encoder 
 #if VIDEO_CODEC_H265ENC
       case PIPE_VIDEO_FORMAT_HEVC:
       {
-         bool gopHasPFrames =
-            (pD3D12Enc->m_currentEncodeConfig.m_encoderGOPConfigDesc.m_HEVCGroupOfPictures.PPicturePeriod > 0) &&
-            ((pD3D12Enc->m_currentEncodeConfig.m_encoderGOPConfigDesc.m_HEVCGroupOfPictures.GOPLength == 0) ||
-             (pD3D12Enc->m_currentEncodeConfig.m_encoderGOPConfigDesc.m_HEVCGroupOfPictures.PPicturePeriod <
-              pD3D12Enc->m_currentEncodeConfig.m_encoderGOPConfigDesc.m_HEVCGroupOfPictures.GOPLength));
-
-         pD3D12Enc->m_upDPBManager = std::make_unique<d3d12_video_encoder_references_manager_hevc>(
-            gopHasPFrames,
-            *pD3D12Enc->m_upDPBStorageManager,
-            // Max number of frames to be used as a reference, without counting the current recon picture
-            d3d12_video_encoder_get_current_max_dpb_capacity(pD3D12Enc)
-         );
-
+         pD3D12Enc->m_upDPBManager = std::make_unique<d3d12_video_encoder_references_manager_hevc>();
          pD3D12Enc->m_upBitstreamBuilder = std::make_unique<d3d12_video_bitstream_builder_hevc>();
       } break;
 #endif

src/gallium/drivers/d3d12/d3d12_video_enc_hevc.h
@@ -54,8 +54,6 @@ d3d12_video_encoder_update_current_frame_pic_params_info_hevc(struct d3d12_video
                                                               struct pipe_picture_desc *  picture,
                                                               D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA &picParams,
                                                              bool &bUsedAsReference);
-D3D12_VIDEO_ENCODER_FRAME_TYPE_HEVC
-d3d12_video_encoder_convert_frame_type_hevc(enum pipe_h2645_enc_picture_type picType);
 uint32_t
 d3d12_video_encoder_build_codec_headers_hevc(struct d3d12_video_encoder *pD3D12Enc,
                                              std::vector<uint64_t> &pWrittenCodecUnitsSizes);

src/gallium/drivers/d3d12/d3d12_video_encoder_references_manager_hevc.h
@@ -21,67 +21,66 @@
  * IN THE SOFTWARE.
  */
 
-#ifndef D3D12_VIDEO_ENCODE_FIFO_REFERENCES_MANAGER_HEVC_H
-#define D3D12_VIDEO_ENCODE_FIFO_REFERENCES_MANAGER_HEVC_H
+#ifndef D3D12_VIDEO_ENCODE_REFERENCES_MANAGER_HEVC_H
+#define D3D12_VIDEO_ENCODE_REFERENCES_MANAGER_HEVC_H
 
 #include "d3d12_video_types.h"
 #include "d3d12_video_encoder_references_manager.h"
-#include "d3d12_video_dpb_storage_manager.h"
 
 class d3d12_video_encoder_references_manager_hevc : public d3d12_video_encoder_references_manager_interface
 {
  public:
-   void                                      end_frame();
-   void                                      begin_frame(D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA curFrameData, bool bUsedAsReference, struct pipe_picture_desc* picture);
-   D3D12_VIDEO_ENCODER_RECONSTRUCTED_PICTURE get_current_frame_recon_pic_output_allocation();
+   void begin_frame(D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA curFrameData,
+                    bool bUsedAsReference,
+                    struct pipe_picture_desc *picture);
    bool get_current_frame_picture_control_data(D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA &codecAllocation);
-   bool is_current_frame_used_as_reference();
    D3D12_VIDEO_ENCODE_REFERENCE_FRAMES get_current_reference_frames();
 
-   d3d12_video_encoder_references_manager_hevc(bool                                       gopHasInterCodedFrames,
-                                               d3d12_video_dpb_storage_manager_interface &rDpbStorageManager,
-                                               uint32_t                                   MaxDPBCapacity);
+   bool is_current_frame_used_as_reference()
+   {
+      return m_isCurrentFrameUsedAsReference;
+   }
+   D3D12_VIDEO_ENCODER_RECONSTRUCTED_PICTURE get_current_frame_recon_pic_output_allocation()
+   {
+      return m_CurrentFrameReferencesData.ReconstructedPicTexture;
+   }
+   void end_frame()
+   { }
+
+   d3d12_video_encoder_references_manager_hevc()
+   { }
 
    ~d3d12_video_encoder_references_manager_hevc()
    { }
 
  private:
    // Class helpers
-   void prepare_current_frame_recon_pic_allocation();
-   void reset_gop_tracking_and_dpb();
    void update_fifo_dpb_push_front_cur_recon_pic();
    void print_dpb();
    void print_l0_l1_lists();
 
    // Class members
-
-   uint32_t m_MaxDPBCapacity      = 0;
-
-  struct D3D12_VIDEO_ENCODER_REFERENCE_PICTURE_DESCRIPTOR_HEVC_EX {
-    D3D12_VIDEO_ENCODER_REFERENCE_PICTURE_DESCRIPTOR_HEVC base;
-    /* the upper layer uses pipe_h265_enc_picture_desc.frame_num to identify frames
-    in the L0 and L1 reference lists. This frame_num is different than POC
-    so let's save it in this variable to be able to reverse-map the L0/L1 lists from
-    these indices into POCs */
-    unsigned int reference_lists_frame_idx;
-  };
+   struct d3d12_video_dpb
+   {
+      std::vector<ID3D12Resource *> pResources;
+      std::vector<uint32_t> pSubresources;
+   };
 
    struct current_frame_references_data
    {
-      std::vector<D3D12_VIDEO_ENCODER_REFERENCE_PICTURE_DESCRIPTOR_HEVC_EX> pReferenceFramesReconPictureDescriptors;
-      D3D12_VIDEO_ENCODER_RECONSTRUCTED_PICTURE                          ReconstructedPicTexture;
+      std::vector<D3D12_VIDEO_ENCODER_REFERENCE_PICTURE_DESCRIPTOR_HEVC> pReferenceFramesReconPictureDescriptors;
+      D3D12_VIDEO_ENCODER_RECONSTRUCTED_PICTURE ReconstructedPicTexture;
+      d3d12_video_dpb ReferenceTextures;
+      std::vector<UINT> pList0ReferenceFrames;
+      std::vector<UINT> pList1ReferenceFrames;
+      std::vector<UINT> pList0RefPicModifications;
+      std::vector<UINT> pList1RefPicModifications;
    };
-
-   d3d12_video_dpb_storage_manager_interface &m_rDPBStorageManager;
 
    current_frame_references_data m_CurrentFrameReferencesData;
 
-   bool m_gopHasInterFrames = false;
-
    bool m_isCurrentFrameUsedAsReference = false;
    D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA_HEVC m_curFrameState = {};
-   std::vector<D3D12_VIDEO_ENCODER_REFERENCE_PICTURE_DESCRIPTOR_HEVC> m_curFrameStateDescriptorStorage;
-   unsigned int m_current_frame_idx = 0;
 };
 
 #endif

commit de4e25da469a4f014f4a79554870a6c0407fcaf1

d3d12: Video Encode HEVC - Store L0/L1 active nums at time to encode them in the headers
Fixes: e268ed0613f ("d3d12: Video Encode HEVC to use direct DPB from frontend")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30997>

src/gallium/drivers/d3d12/d3d12_video_enc_hevc.cpp
@@ -364,6 +364,17 @@ d3d12_video_encoder_update_current_frame_pic_params_info_hevc(struct d3d12_video
 
    picParams.pHEVCPicData->slice_pic_parameter_set_id = pHEVCBitstreamBuilder->get_active_pps().pps_pic_parameter_set_id;
 
+   //
+   // These need to be set here so they're available for SPS/PPS header building (reference manager updates after that, for slice header params)
+   //
+   picParams.pHEVCPicData->List0ReferenceFramesCount = 0;
+   picParams.pHEVCPicData->List1ReferenceFramesCount = 0;
+   if ((hevcPic->picture_type == PIPE_H2645_ENC_PICTURE_TYPE_P) ||
+       (hevcPic->picture_type == PIPE_H2645_ENC_PICTURE_TYPE_B))
+      picParams.pHEVCPicData->List0ReferenceFramesCount = hevcPic->num_ref_idx_l0_active_minus1 + 1;
+   if (hevcPic->picture_type == PIPE_H2645_ENC_PICTURE_TYPE_B)
+      picParams.pHEVCPicData->List1ReferenceFramesCount = hevcPic->num_ref_idx_l1_active_minus1 + 1;
+
    if ((pD3D12Enc->m_currentEncodeConfig.m_encoderCodecSpecificConfigDesc.m_HEVCConfig.ConfigurationFlags 
       & D3D12_VIDEO_ENCODER_CODEC_CONFIGURATION_HEVC_FLAG_ALLOW_REQUEST_INTRA_CONSTRAINED_SLICES) != 0)
       picParams.pHEVCPicData->Flags |= D3D12_VIDEO_ENCODER_PICTURE_CONTROL_CODEC_DATA_HEVC_FLAG_REQUEST_INTRA_CONSTRAINED_SLICES;