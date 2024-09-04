685432e7ab2bb3bb5b35838522c6505cae0ddf51
Yahan Zhou <yahan@google.com>
2024-09-04 14:42:00 -0700

    Signal the fence after color buffer copying
    We move the fence signaling to after color buffer copying. This is done
    by reusing the existing CPU CV object and wait on it before triggering
    the GPU vkWaitForFences.
    
    Also rewrite fenceInfo.status with std::future to simplify the code.
    
    Design: go/emu-async-cb-copy
    Reviewed-by: Aaron Ruby <aruby@blackberry.com>
    Acked-by: Yonggang Luo <luoyonggang@gmail.com>
    Acked-by: Adam Jackson <ajax@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27246>

--- src/gfxstream/codegen/scripts/cereal/decoder.py
+++ src/gfxstream/codegen/scripts/cereal/decoder.py
@@ -734,6 +734,8 @@ custom_decodes = {
     "vkDestroySemaphore" : emit_global_state_wrapped_decoding,
 
     "vkCreateFence" : emit_global_state_wrapped_decoding,
+    "vkGetFenceStatus" : emit_global_state_wrapped_decoding,
+    "vkWaitForFences" : emit_global_state_wrapped_decoding,
     "vkResetFences" : emit_global_state_wrapped_decoding,
     "vkDestroyFence" : emit_global_state_wrapped_decoding,
 
