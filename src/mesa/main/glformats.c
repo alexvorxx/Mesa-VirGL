d5916cb3ca7af90f81639d91922efecf9e713fda
Tapani Pälli <tapani.palli@intel.com>
2024-09-04 09:26:00 +0300

    mesa: check for ARB_ES3_compatibility in format checks
    Fixes regressions seen with following tests:
    
     dEQP-GL45-ES3.functional.negative_api.buffer.read_pixels
     dEQP-GL45-ES3.functional.negative_api.buffer.read_pixels_format_mismatch
     dEQP-GL45-ES31.functional.debug.negative_coverage.get_error.buffer.read_pixels
     dEQP-GL45-ES31.functional.debug.negative_coverage.get_error.buffer.readn_pixels
     dEQP-GL45-ES31.functional.debug.negative_coverage.get_error.buffer.read_pixels_format_mismatch
    
    Fixes: e8226bda60d ("mesa/main: validate desktop gl format/types")
    Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
    Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31008>

--- src/mesa/main/glformats.c
+++ src/mesa/main/glformats.c
@@ -1788,7 +1788,9 @@ valid_texture_format_enum(const struct gl_context *ctx, GLenum format)
    case GL_LUMINANCE_ALPHA:
    case GL_LUMINANCE:
    case GL_ALPHA:
-      return _mesa_is_desktop_gl_compat(ctx) || _mesa_is_gles(ctx);
+      return _mesa_is_desktop_gl_compat(ctx) ||
+             _mesa_has_ARB_ES3_compatibility(ctx) ||
+             _mesa_is_gles(ctx);
 
    case GL_ABGR_EXT:
       return _mesa_has_EXT_abgr(ctx);
