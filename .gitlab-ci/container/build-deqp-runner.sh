alexvorxx/Mesa-VirGL

commit 83d9cfa58d34edff4182cd2ce558ecf87122ea49

ci/deqp-runner: build from git checkout even on linux
This allows things like patching deqp-runner, and unifies linux and
android.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30866>

.gitlab-ci/container/build-deqp-runner.sh
@@ -14,24 +14,18 @@ DEQP_RUNNER_VERSION=0.20.0
 
 DEQP_RUNNER_GIT_URL="${DEQP_RUNNER_GIT_URL:-https://gitlab.freedesktop.org/mesa/deqp-runner.git}"
 
-if [ -n "${DEQP_RUNNER_GIT_TAG}${DEQP_RUNNER_GIT_REV}" ]; then
-    # Build and install from source
-    DEQP_RUNNER_CARGO_ARGS="--git $DEQP_RUNNER_GIT_URL"
-
-    if [ -n "${DEQP_RUNNER_GIT_TAG}" ]; then
-        DEQP_RUNNER_CARGO_ARGS="--tag ${DEQP_RUNNER_GIT_TAG} ${DEQP_RUNNER_CARGO_ARGS}"
-        DEQP_RUNNER_GIT_CHECKOUT="$DEQP_RUNNER_GIT_TAG"
-    else
-        DEQP_RUNNER_CARGO_ARGS="--rev ${DEQP_RUNNER_GIT_REV} ${DEQP_RUNNER_CARGO_ARGS}"
-        DEQP_RUNNER_GIT_CHECKOUT="$DEQP_RUNNER_GIT_REV"
-    fi
-
-    DEQP_RUNNER_CARGO_ARGS="${DEQP_RUNNER_CARGO_ARGS} ${EXTRA_CARGO_ARGS}"
+if [ -n "$DEQP_RUNNER_GIT_TAG" ]; then
+    DEQP_RUNNER_GIT_CHECKOUT="$DEQP_RUNNER_GIT_TAG"
+elif [ -n "$DEQP_RUNNER_GIT_REV" ]; then
+    DEQP_RUNNER_GIT_CHECKOUT="$DEQP_RUNNER_GIT_REV"
 else
-    # Install from package registry
-    DEQP_RUNNER_CARGO_ARGS="--version ${DEQP_RUNNER_VERSION} ${EXTRA_CARGO_ARGS} -- deqp-runner"
     DEQP_RUNNER_GIT_CHECKOUT="v$DEQP_RUNNER_VERSION"
 fi
+
+mkdir -p /deqp-runner
+pushd /deqp-runner
+git clone --branch "$DEQP_RUNNER_GIT_CHECKOUT" --depth 1 "$DEQP_RUNNER_GIT_URL" deqp-runner-git
+pushd deqp-runner-git
 
 if [[ "$RUST_TARGET" != *-android ]]; then
     # When CC (/usr/lib/ccache/gcc) variable is set, the rust compiler uses
@@ -44,14 +38,9 @@ if [[ "$RUST_TARGET" != *-android ]]; then
     cargo install --locked  \
         -j ${FDO_CI_CONCURRENT:-4} \
         --root /usr/local \
-        ${DEQP_RUNNER_CARGO_ARGS}
+        --path .
     CC=$SAVEDCC
 else
-    mkdir -p /deqp-runner
-    pushd /deqp-runner
-    git clone --branch "$DEQP_RUNNER_GIT_CHECKOUT" --depth 1 "$DEQP_RUNNER_GIT_URL" deqp-runner-git
-    pushd deqp-runner-git
-
     cargo install --locked  \
         -j ${FDO_CI_CONCURRENT:-4} \
         --root /usr/local --version 2.10.0 \
@@ -65,11 +54,11 @@ else
     cargo uninstall --locked  \
         --root /usr/local \
         cargo-ndk
-
-    popd
-    rm -rf deqp-runner-git
-    popd
 fi
+
+popd
+rm -rf deqp-runner-git
+popd
 
 # remove unused test runners to shrink images for the Mesa CI build (not kernel,
 # which chooses its own deqp branch)

commit c6a9a9c3bce90923f7700219354e0b6e5a3c9ba6

ci/deqp-runner: pass EXTRA_CARGO_ARGS
Without EXTRA_CARGO_ARGS='--target armv7-unknown-linux-gnueabihf',
deqp-runner binary was built for aarch64 architecture for arm32
builds and causing below failures on arm32 target,
/usr/bin/deqp-runner: cannot execute binary file: Exec format error

So pass EXTRA_CARGO_ARGS to cargo to fix this.

Fixes: 83d9cfa5 ("ci/deqp-runner: build from git checkout even on linux")
Signed-off-by: Vignesh Raman <vignesh.raman@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31071>

.gitlab-ci/container/build-deqp-runner.sh
@@ -59,6 +59,7 @@ if [[ "$RUST_TARGET" != *-android ]]; then
     cargo install --locked  \
         -j ${FDO_CI_CONCURRENT:-4} \
         --root /usr/local \
+        ${EXTRA_CARGO_ARGS} \
         --path .
     CC=$SAVEDCC
 else

.gitlab-ci/image-tags.yml
@@ -27,10 +27,10 @@ variables:
    DEBIAN_ARM64_TEST_IMAGE_VK_PATH: "debian/arm64_test-vk"
    DEBIAN_X86_64_TEST_ANDROID_IMAGE_PATH: "debian/x86_64_test-android"
 
-   DEBIAN_TEST_ANDROID_TAG: "20240826-runner"
-   DEBIAN_TEST_GL_TAG: "20240830-piglit-93"
-   DEBIAN_TEST_VK_TAG: "20240830-piglit-93"
-   KERNEL_ROOTFS_TAG: "20240902-mtlfw"
+   DEBIAN_TEST_ANDROID_TAG: "20240906-deqp"
+   DEBIAN_TEST_GL_TAG: "20240906-deqp"
+   DEBIAN_TEST_VK_TAG: "20240906-deqp"
+   KERNEL_ROOTFS_TAG: "20240906-deqp"
 
    ALPINE_X86_64_BUILD_TAG: "20240814-var"
    ALPINE_X86_64_LAVA_SSH_TAG: "20240401-wlproto"