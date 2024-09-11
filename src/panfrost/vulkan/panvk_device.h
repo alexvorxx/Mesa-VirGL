alexvorxx/Mesa-VirGL

commit 5067921349a9830eeac3414e47f11119cfae9291

panvk: Switch to vk_meta
Replace our custom panvk_meta stuff by something based on vk_meta.
That's less gen-specific code to maintain which will help with v10
support.

Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29451>

src/panfrost/vulkan/panvk_device.h
@@ -9,6 +9,7 @@
 #include <stdint.h>
 
 #include "vk_device.h"
+#include "vk_meta.h"
 
 #include "panvk_blend.h"
 #include "panvk_instance.h"
@@ -36,13 +37,31 @@ struct panvk_device {
    struct panvk_priv_bo *tiler_heap;
    struct panvk_priv_bo *sample_positions;
 
-   struct panvk_blend_shader_cache blend_shader_cache;
-   struct panvk_meta meta;
-
+   /* Access to the blitter pools are protected by the blitter
+    * shader/rsd locks. They can't be merged with other binary/desc
+    * pools unless we patch pan_blitter.c to support external pool locks.
+    *
+    * FIXME: The blitter infrastructure is only needed for FB preload.
+    * We should probably consider getting rid of the dependency we have
+    * on pan_desc.c and implement preload ourselves so we don't have
+    * to duplicate caches.
+    */
    struct {
-      struct panvk_priv_bo *shader_bo;
-      struct panvk_priv_bo *rsd_bo;
+      struct panvk_pool bin_pool;
+      struct panvk_pool desc_pool;
+      struct pan_blitter_cache cache;
+      struct pan_blend_shader_cache blend_shader_cache;
+   } blitter;
+
+   struct panvk_blend_shader_cache blend_shader_cache;
+   struct vk_meta_device meta;
+
+#if PAN_ARCH <= 7
+   struct {
+      struct panvk_priv_mem shader;
+      struct panvk_priv_mem rsd;
    } desc_copy;
+#endif
 
    struct {
       struct panvk_pool rw;

src/panfrost/vulkan/bifrost/panvk_vX_meta_desc_copy.c
@@ -235,7 +235,7 @@ single_desc_copy(nir_builder *b, nir_def *desc_copy_idx)
    nir_pop_if(b, NULL);
 }
 
-static mali_ptr
+static struct panvk_priv_mem
 panvk_meta_desc_copy_shader(struct panvk_device *dev,
                             struct pan_shader_info *shader_info)
 {
@@ -270,8 +270,8 @@ panvk_meta_desc_copy_shader(struct panvk_device *dev,
    shader_info->push.count =
       DIV_ROUND_UP(sizeof(struct pan_nir_desc_copy_info), 4);
 
-   mali_ptr shader = pan_pool_upload_aligned(&dev->meta.bin_pool.base,
-                                             binary.data, binary.size, 128);
+   struct panvk_priv_mem shader = panvk_pool_upload_aligned(
+      &dev->mempools.exec, binary.data, binary.size, 128);
 
    util_dynarray_fini(&binary);
    return shader;
@@ -282,15 +282,24 @@ panvk_per_arch(meta_desc_copy_init)(struct panvk_device *dev)
 {
    struct pan_shader_info shader_info;
 
-   mali_ptr shader = panvk_meta_desc_copy_shader(dev, &shader_info);
-   struct panfrost_ptr rsd =
-      pan_pool_alloc_desc(&dev->meta.desc_pool.base, RENDERER_STATE);
+   dev->desc_copy.shader = panvk_meta_desc_copy_shader(dev, &shader_info);
 
-   pan_pack(rsd.cpu, RENDERER_STATE, cfg) {
+   mali_ptr shader = panvk_priv_mem_dev_addr(dev->desc_copy.shader);
+   struct panvk_priv_mem rsd =
+      panvk_pool_alloc_desc(&dev->mempools.rw, RENDERER_STATE);
+
+   pan_pack(panvk_priv_mem_host_addr(rsd), RENDERER_STATE, cfg) {
       pan_shader_prepare_rsd(&shader_info, shader, &cfg);
    }
 
-   dev->meta.desc_copy.rsd = rsd.gpu;
+   dev->desc_copy.rsd = rsd;
+}
+
+void
+panvk_per_arch(meta_desc_copy_cleanup)(struct panvk_device *dev)
+{
+   panvk_pool_free_mem(&dev->mempools.rw, dev->desc_copy.rsd);
+   panvk_pool_free_mem(&dev->mempools.exec, dev->desc_copy.shader);
 }
 
 struct panfrost_ptr
@@ -368,7 +377,7 @@ panvk_per_arch(meta_get_copy_desc_job)(
    GENX(pan_emit_tls)(&tlsinfo, tls.cpu);
 
    pan_section_pack(job.cpu, COMPUTE_JOB, DRAW, cfg) {
-      cfg.state = dev->meta.desc_copy.rsd;
+      cfg.state = panvk_priv_mem_dev_addr(dev->desc_copy.rsd);
       cfg.push_uniforms = push_uniforms;
       cfg.thread_storage = tls.gpu;
    }

src/panfrost/vulkan/jm/panvk_vX_cmd_buffer.c
@@ -154,7 +154,7 @@ panvk_per_arch(cmd_close_batch)(struct panvk_cmd_buffer *cmdbuf)
             cmdbuf->state.gfx.render.fb.info.bifrost.pre_post.dcds.gpu = 0;
 
             ASSERTED unsigned num_preload_jobs = GENX(pan_preload_fb)(
-               &dev->meta.blitter.cache, &cmdbuf->desc_pool.base,
+               &dev->blitter.cache, &cmdbuf->desc_pool.base,
                &cmdbuf->state.gfx.render.fb.info, i, batch->tls.gpu, NULL);
 
             /* Bifrost GPUs use pre frame DCDs to preload the FB content. We

src/panfrost/vulkan/jm/panvk_vX_cmd_dispatch.c
@@ -15,6 +15,7 @@
 #include "panvk_cmd_desc_state.h"
 #include "panvk_device.h"
 #include "panvk_entrypoints.h"
+#include "panvk_meta.h"
 #include "panvk_physical_device.h"
 
 #include "pan_desc.h"

src/panfrost/vulkan/jm/panvk_vX_cmd_draw.c
@@ -32,6 +32,7 @@
 #include "pan_shader.h"
 
 #include "vk_format.h"
+#include "vk_meta.h"
 #include "vk_pipeline_layout.h"
 
 struct panvk_draw_info {
@@ -857,6 +858,12 @@ panvk_draw_prepare_vertex_job(struct panvk_cmd_buffer *cmdbuf,
 static enum mali_draw_mode
 translate_prim_topology(VkPrimitiveTopology in)
 {
+   /* Test VK_PRIMITIVE_TOPOLOGY_META_RECT_LIST_MESA separately, as it's not
+    * part of the VkPrimitiveTopology enum.
+    */
+   if (in == VK_PRIMITIVE_TOPOLOGY_META_RECT_LIST_MESA)
+      return MALI_DRAW_MODE_TRIANGLES;
+
    switch (in) {
    case VK_PRIMITIVE_TOPOLOGY_POINT_LIST:
       return MALI_DRAW_MODE_POINTS;

src/panfrost/vulkan/jm/panvk_vX_meta.c
@@ -1,84 +1,0 @@
-/*
- * Copyright © 2021 Collabora Ltd.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice (including the next
- * paragraph) shall be included in all copies or substantial portions of the
- * Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- */
-
-#include "genxml/gen_macros.h"
-
-#include "nir/nir_builder.h"
-#include "pan_encoder.h"
-#include "pan_shader.h"
-
-#include "panvk_device.h"
-
-#include "vk_format.h"
-
-mali_ptr
-panvk_per_arch(meta_emit_viewport)(struct pan_pool *pool, uint16_t minx,
-                                   uint16_t miny, uint16_t maxx, uint16_t maxy)
-{
-   struct panfrost_ptr vp = pan_pool_alloc_desc(pool, VIEWPORT);
-
-   pan_pack(vp.cpu, VIEWPORT, cfg) {
-      cfg.scissor_minimum_x = minx;
-      cfg.scissor_minimum_y = miny;
-      cfg.scissor_maximum_x = maxx;
-      cfg.scissor_maximum_y = maxy;
-   }
-
-   return vp.gpu;
-}
-
-void
-panvk_per_arch(meta_init)(struct panvk_device *dev)
-{
-   struct panvk_pool_properties bin_pool_props = {
-      .create_flags = PAN_KMOD_BO_FLAG_EXECUTABLE,
-      .slab_size = 16 * 1024,
-      .label = "panvk_meta binary pool",
-      .prealloc = false,
-      .owns_bos = true,
-      .needs_locking = false,
-   };
-   struct panvk_pool_properties desc_pool_props = {
-      .create_flags = 0,
-      .slab_size = 16 * 1024,
-      .label = "panvk_meta descriptor pool",
-      .prealloc = false,
-      .owns_bos = true,
-      .needs_locking = false,
-   };
-
-   panvk_pool_init(&dev->meta.bin_pool, dev, NULL, &bin_pool_props);
-   panvk_pool_init(&dev->meta.desc_pool, dev, NULL, &desc_pool_props);
-   panvk_per_arch(meta_blit_init)(dev);
-   panvk_per_arch(meta_copy_init)(dev);
-   panvk_per_arch(meta_clear_init)(dev);
-   panvk_per_arch(meta_desc_copy_init)(dev);
-}
-
-void
-panvk_per_arch(meta_cleanup)(struct panvk_device *dev)
-{
-   panvk_per_arch(meta_blit_cleanup)(dev);
-   panvk_pool_cleanup(&dev->meta.desc_pool);
-   panvk_pool_cleanup(&dev->meta.bin_pool);
-}

src/panfrost/vulkan/jm/panvk_vX_meta_blit.c
@@ -1,272 +1,0 @@
-/*
- * Copyright © 2021 Collabora Ltd.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice (including the next
- * paragraph) shall be included in all copies or substantial portions of the
- * Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- */
-
-#include "gen_macros.h"
-
-#include "pan_blitter.h"
-#include "pan_props.h"
-
-#include "panvk_cmd_buffer.h"
-#include "panvk_device.h"
-#include "panvk_entrypoints.h"
-#include "panvk_image.h"
-#include "panvk_physical_device.h"
-
-static void
-panvk_meta_blit(struct panvk_cmd_buffer *cmdbuf,
-                const struct pan_blit_info *blitinfo,
-                const struct panvk_image *src_img,
-                const struct panvk_image *dst_img)
-{
-   struct panvk_device *dev = to_panvk_device(cmdbuf->vk.base.device);
-   struct panvk_physical_device *phys_dev =
-      to_panvk_physical_device(dev->vk.physical);
-   struct pan_fb_info *fbinfo = &cmdbuf->state.gfx.render.fb.info;
-   struct pan_blit_context ctx;
-   struct pan_image_view views[2] = {
-      {
-         .format = blitinfo->dst.planes[0].format,
-         .dim = MALI_TEXTURE_DIMENSION_2D,
-         .planes =
-            {
-               blitinfo->dst.planes[0].image,
-               blitinfo->dst.planes[1].image,
-               blitinfo->dst.planes[2].image,
-            },
-         .nr_samples = blitinfo->dst.planes[0].image->layout.nr_samples,
-         .first_level = blitinfo->dst.level,
-         .last_level = blitinfo->dst.level,
-         .swizzle = {PIPE_SWIZZLE_X, PIPE_SWIZZLE_Y, PIPE_SWIZZLE_Z,
-                     PIPE_SWIZZLE_W},
-      },
-   };
-
-   *fbinfo = (struct pan_fb_info){
-      .tile_buf_budget = panfrost_query_optimal_tib_size(phys_dev->model),
-      .width = u_minify(blitinfo->dst.planes[0].image->layout.width,
-                        blitinfo->dst.level),
-      .height = u_minify(blitinfo->dst.planes[0].image->layout.height,
-                         blitinfo->dst.level),
-      .extent =
-         {
-            .minx = MAX2(MIN2(blitinfo->dst.start.x, blitinfo->dst.end.x), 0),
-            .miny = MAX2(MIN2(blitinfo->dst.start.y, blitinfo->dst.end.y), 0),
-            .maxx = MAX2(blitinfo->dst.start.x, blitinfo->dst.end.x),
-            .maxy = MAX2(blitinfo->dst.start.y, blitinfo->dst.end.y),
-         },
-      .nr_samples = blitinfo->dst.planes[0].image->layout.nr_samples,
-   };
-
-   fbinfo->extent.maxx = MIN2(fbinfo->extent.maxx, fbinfo->width - 1);
-   fbinfo->extent.maxy = MIN2(fbinfo->extent.maxy, fbinfo->height - 1);
-
-   /* TODO: don't force preloads of dst resources if unneeded */
-
-   const struct util_format_description *fdesc =
-      util_format_description(blitinfo->dst.planes[0].image->layout.format);
-
-   if (util_format_has_depth(fdesc)) {
-      /* We want the image format here, otherwise we might lose one of the
-       * component.
-       */
-      views[0].format = blitinfo->dst.planes[0].image->layout.format;
-      fbinfo->zs.view.zs = &views[0];
-      fbinfo->zs.preload.z = true;
-      fbinfo->zs.preload.s = util_format_has_stencil(fdesc);
-   } else if (util_format_has_stencil(fdesc)) {
-      fbinfo->zs.view.s = &views[0];
-      fbinfo->zs.preload.s = true;
-   } else {
-      fbinfo->rt_count = 1;
-      fbinfo->rts[0].view = &views[0];
-      fbinfo->rts[0].preload = true;
-      cmdbuf->state.gfx.render.fb.crc_valid[0] = false;
-      fbinfo->rts[0].crc_valid = &cmdbuf->state.gfx.render.fb.crc_valid[0];
-   }
-
-   if (blitinfo->dst.planes[1].format != PIPE_FORMAT_NONE) {
-      /* TODO: don't force preloads of dst resources if unneeded */
-      views[1].format = blitinfo->dst.planes[1].format;
-      views[1].dim = MALI_TEXTURE_DIMENSION_2D;
-      views[1].planes[0] = blitinfo->dst.planes[1].image;
-      views[1].nr_samples = blitinfo->dst.planes[1].image->layout.nr_samples;
-      views[1].first_level = blitinfo->dst.level;
-      views[1].last_level = blitinfo->dst.level;
-      views[1].swizzle[0] = PIPE_SWIZZLE_X;
-      views[1].swizzle[1] = PIPE_SWIZZLE_Y;
-      views[1].swizzle[2] = PIPE_SWIZZLE_Z;
-      views[1].swizzle[3] = PIPE_SWIZZLE_W;
-      fbinfo->zs.view.s = &views[1];
-   }
-
-   panvk_per_arch(cmd_close_batch)(cmdbuf);
-   cmdbuf->state.gfx.render.layer_count = 1;
-
-   GENX(pan_blit_ctx_init)
-   (&dev->meta.blitter.cache, blitinfo, &cmdbuf->desc_pool.base, &ctx);
-   do {
-      if (ctx.dst.cur_layer < 0)
-         continue;
-
-      struct panvk_batch *batch = panvk_per_arch(cmd_open_batch)(cmdbuf);
-      mali_ptr tsd, tiler;
-
-      views[0].first_layer = views[0].last_layer = ctx.dst.cur_layer;
-      views[1].first_layer = views[1].last_layer = views[0].first_layer;
-      batch->blit.src = src_img->bo;
-      batch->blit.dst = dst_img->bo;
-      panvk_per_arch(cmd_alloc_tls_desc)(cmdbuf, true);
-      panvk_per_arch(cmd_alloc_fb_desc)(cmdbuf);
-      panvk_per_arch(cmd_prepare_tiler_context)(cmdbuf, 0);
-
-      tsd = batch->tls.gpu;
-      tiler = batch->tiler.ctx_descs.gpu;
-
-      struct panfrost_ptr job = GENX(pan_blit)(&ctx, &cmdbuf->desc_pool.base,
-                                               &batch->vtc_jc, tsd, tiler);
-      util_dynarray_append(&batch->jobs, void *, job.cpu);
-      panvk_per_arch(cmd_close_batch)(cmdbuf);
-   } while (pan_blit_next_surface(&ctx));
-}
-
-VKAPI_ATTR void VKAPI_CALL
-panvk_per_arch(CmdBlitImage2)(VkCommandBuffer commandBuffer,
-                              const VkBlitImageInfo2 *pBlitImageInfo)
-{
-   VK_FROM_HANDLE(panvk_cmd_buffer, cmdbuf, commandBuffer);
-   VK_FROM_HANDLE(panvk_image, src, pBlitImageInfo->srcImage);
-   VK_FROM_HANDLE(panvk_image, dst, pBlitImageInfo->dstImage);
-
-   for (unsigned i = 0; i < pBlitImageInfo->regionCount; i++) {
-      const VkImageBlit2 *region = &pBlitImageInfo->pRegions[i];
-      struct pan_blit_info info = {
-         .src =
-            {
-               .planes[0].image = &src->pimage,
-               .planes[0].format = src->pimage.layout.format,
-               .level = region->srcSubresource.mipLevel,
-               .start =
-                  {
-                     region->srcOffsets[0].x,
-                     region->srcOffsets[0].y,
-                     region->srcOffsets[0].z,
-                     region->srcSubresource.baseArrayLayer,
-                  },
-               .end =
-                  {
-                     region->srcOffsets[1].x,
-                     region->srcOffsets[1].y,
-                     region->srcOffsets[1].z,
-                     region->srcSubresource.baseArrayLayer +
-                        region->srcSubresource.layerCount - 1,
-                  },
-            },
-         .dst =
-            {
-               .planes[0].image = &dst->pimage,
-               .planes[0].format = dst->pimage.layout.format,
-               .level = region->dstSubresource.mipLevel,
-               .start =
-                  {
-                     region->dstOffsets[0].x,
-                     region->dstOffsets[0].y,
-                     region->dstOffsets[0].z,
-                     region->dstSubresource.baseArrayLayer,
-                  },
-               .end =
-                  {
-                     region->dstOffsets[1].x,
-                     region->dstOffsets[1].y,
-                     region->dstOffsets[1].z,
-                     region->dstSubresource.baseArrayLayer +
-                        region->dstSubresource.layerCount - 1,
-                  },
-            },
-         .nearest = pBlitImageInfo->filter == VK_FILTER_NEAREST,
-      };
-
-      if (region->srcSubresource.aspectMask == VK_IMAGE_ASPECT_STENCIL_BIT)
-         info.src.planes[0].format =
-            util_format_stencil_only(info.src.planes[0].format);
-      else if (region->srcSubresource.aspectMask == VK_IMAGE_ASPECT_DEPTH_BIT)
-         info.src.planes[0].format =
-            util_format_get_depth_only(info.src.planes[0].format);
-
-      if (region->dstSubresource.aspectMask == VK_IMAGE_ASPECT_STENCIL_BIT)
-         info.dst.planes[0].format =
-            util_format_stencil_only(info.dst.planes[0].format);
-      else if (region->dstSubresource.aspectMask == VK_IMAGE_ASPECT_DEPTH_BIT)
-         info.dst.planes[0].format =
-            util_format_get_depth_only(info.dst.planes[0].format);
-
-      panvk_meta_blit(cmdbuf, &info, src, dst);
-   }
-}
-
-VKAPI_ATTR void VKAPI_CALL
-panvk_per_arch(CmdResolveImage2)(VkCommandBuffer commandBuffer,
-                                 const VkResolveImageInfo2 *pResolveImageInfo)
-{
-   panvk_stub();
-}
-
-void
-panvk_per_arch(meta_blit_init)(struct panvk_device *dev)
-{
-   struct panvk_physical_device *phys_dev =
-      to_panvk_physical_device(dev->vk.physical);
-   struct panvk_pool_properties bin_pool_props = {
-      .create_flags = PAN_KMOD_BO_FLAG_EXECUTABLE,
-      .slab_size = 16 * 1024,
-      .label = "panvk_meta blitter binary pool",
-      .prealloc = false,
-      .owns_bos = true,
-      .needs_locking = false,
-   };
-   struct panvk_pool_properties desc_pool_props = {
-      .create_flags = 0,
-      .slab_size = 16 * 1024,
-      .label = "panvk_meta blitter descriptor pool",
-      .prealloc = false,
-      .owns_bos = true,
-      .needs_locking = false,
-   };
-
-   panvk_pool_init(&dev->meta.blitter.bin_pool, dev, NULL, &bin_pool_props);
-   panvk_pool_init(&dev->meta.blitter.desc_pool, dev, NULL, &desc_pool_props);
-   pan_blend_shader_cache_init(&dev->meta.blend_shader_cache,
-                               phys_dev->kmod.props.gpu_prod_id);
-   GENX(pan_blitter_cache_init)
-   (&dev->meta.blitter.cache, phys_dev->kmod.props.gpu_prod_id,
-    &dev->meta.blend_shader_cache, &dev->meta.blitter.bin_pool.base,
-    &dev->meta.blitter.desc_pool.base);
-}
-
-void
-panvk_per_arch(meta_blit_cleanup)(struct panvk_device *dev)
-{
-   GENX(pan_blitter_cache_cleanup)(&dev->meta.blitter.cache);
-   pan_blend_shader_cache_cleanup(&dev->meta.blend_shader_cache);
-   panvk_pool_cleanup(&dev->meta.blitter.desc_pool);
-   panvk_pool_cleanup(&dev->meta.blitter.bin_pool);
-}

commit 7f7e772bdf78a0123c17bb60df2f53e7ae7d7382

panvk: Don't make panvk_device size gen dependent
The panvk_device is used in gen-agnostic path, and which means we can't
make its size dependent on PAN_ARCH.

Fixes: 5067921349a9 ("panvk: Switch to vk_meta")
Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Rebecca Mckeever <rebecca.mckeever@collabora.com>
Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Reviewed-by: John Anthony <john.anthony@arm.com>
Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30969>

src/panfrost/vulkan/panvk_device.h
@@ -56,12 +56,10 @@ struct panvk_device {
    struct panvk_blend_shader_cache blend_shader_cache;
    struct vk_meta_device meta;
 
-#if PAN_ARCH <= 7
    struct {
       struct panvk_priv_mem shader;
       struct panvk_priv_mem rsd;
    } desc_copy;
-#endif
 
    struct {
       struct panvk_pool rw;