alexvorxx/zink-xlib-termux

commit 591b5da49bf5dff5c92d2465de9e0fccab2e9d89

nouveau/headers: Run rustfmt on generated files
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28255>

src/nouveau/headers/lib_rs_gen.py
@@ -88,6 +88,10 @@ def main():
     try:
         with open(args.out_rs, 'w', encoding='utf-8') as f:
             f.write(TEMPLATE_RS.render(root=root))
+            try:
+                subprocess.run(['rustfmt', args.out_rs], check=True)
+            except subprocess.CalledProcessError:
+                pass
 
     except Exception:
         # In the event there's an error, this imports some helpers from mako

alexvorxx/Mesa-VirGL

commit 849a496b3324e161c8ea67a6506d0098d80b1d48

nouveau/headers: Fix build without rustfmt
This is optional elsewhere and seemingly the intention was already to
ignore failures, but it didn't catch FileNotFoundError when rustfmt
isn't even available.

Fixes: 591b5da49bf5 ("nouveau/headers: Run rustfmt on generated files")
Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30970>

src/nouveau/headers/lib_rs_gen.py
@@ -111,7 +111,7 @@ def main():
             f.write(TEMPLATE_RS.render(root=root))
             try:
                 subprocess.run(['rustfmt', args.out_rs], check=True)
-            except subprocess.CalledProcessError:
+            except (subprocess.CalledProcessError, FileNotFoundError):
                 pass
 
     except Exception: