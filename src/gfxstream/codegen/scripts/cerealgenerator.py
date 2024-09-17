a3c74c5a07e581c016965b8a2f71b212c2762483
Serdar Kocdemir <kocdemir@google.com>
2024-09-17 11:25:23 +0100

    Add VK_EXT_external_memory_metal definitions
    Partial update to vulkan headers and auto generated code to
    include the new extension. It will be used to fix issues on
    external memory implementation with MoltenVK later on.
    References:
    Khronos MR 6800
    MoltenVK PR 2314
    
    Reviewed-by: Aaron Ruby <aruby@blackberry.com>
    Acked-by: Yonggang Luo <luoyonggang@gmail.com>
    Acked-by: Adam Jackson <ajax@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27246>

--- src/gfxstream/codegen/scripts/cerealgenerator.py
+++ src/gfxstream/codegen/scripts/cerealgenerator.py
@@ -109,6 +109,7 @@ SUPPORTED_FEATURES = [
     "VK_KHR_win32_surface",
     "VK_EXT_metal_surface",
     "VK_EXT_metal_objects",
+    "VK_EXT_external_memory_metal",
     "VK_KHR_external_semaphore_win32",
     "VK_KHR_external_memory_win32",
     "VK_NV_device_diagnostic_checkpoints",
@@ -152,6 +153,7 @@ SUPPORTED_MODULES = {
     "VK_KHR_win32_surface": ["goldfish_vk_dispatch"],
     "VK_EXT_metal_surface": ["goldfish_vk_dispatch"],
     "VK_EXT_metal_objects": ["goldfish_vk_dispatch"],
+    "VK_EXT_external_memory_metal": ["goldfish_vk_dispatch"],
     "VK_KHR_external_semaphore_win32" : ["goldfish_vk_dispatch"],
     "VK_KHR_external_memory_win32" : ["goldfish_vk_dispatch"],
     # Host dispatch for Linux hosts + and entrypoint for guests
