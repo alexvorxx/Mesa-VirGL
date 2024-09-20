alexvorxx/Mesa-VirGL

commit 6f0fff46340177404b123b58678d8aeaba26f0da

gfxstream: guest: fully mesa-ify vulkan_enc
ALOGE(..) to mesa_loge(..), again

Reviewed-by: Aaron Ruby <aruby@blackberry.com>
Acked-by: Yonggang Luo <luoyonggang@gmail.com>
Acked-by: Adam Jackson <ajax@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27246>

src/gfxstream/guest/vulkan_enc/DescriptorSetVirtualization.cpp
@@ -197,7 +197,7 @@ void doEmulatedDescriptorWrite(const VkWriteDescriptorSet* write, ReifiedDescrip
         // TODO
         // Look for pNext inline uniform block or acceleration structure.
         // Append new DescriptorWrite entry that holds the buffer
-        ALOGW("%s: Ignoring emulated write for descriptor type 0x%x\n", __func__, descType);
+        mesa_logw("%s: Ignoring emulated write for descriptor type 0x%x\n", __func__, descType);
     }
 }
 
@@ -339,7 +339,7 @@ static bool isBindingFeasibleForAlloc(
     uint32_t availDescriptorCount = countInfo.descriptorCount - countInfo.used;
 
     if (availDescriptorCount < binding.descriptorCount) {
-        ALOGV(
+        mesa_logi(
             "%s: Ran out of descriptors of type 0x%x. "
             "Wanted %u from layout but "
             "we only have %u free (total in pool: %u)\n",
@@ -356,7 +356,7 @@ static bool isBindingFeasibleForFree(
     const VkDescriptorSetLayoutBinding& binding) {
     if (countInfo.type != binding.descriptorType) return false;
     if (countInfo.used < binding.descriptorCount) {
-        ALOGV(
+        mesa_logi(
             "%s: Was a descriptor set double freed? "
             "Ran out of descriptors of type 0x%x. "
             "Wanted to free %u from layout but "
@@ -386,7 +386,7 @@ static VkResult validateDescriptorSetAllocation(const VkDescriptorSetAllocateInf
     auto setsAvailable = poolInfo->maxSets - poolInfo->usedSets;
 
     if (setsAvailable < pAllocateInfo->descriptorSetCount) {
-        ALOGV(
+        mesa_logi(
             "%s: Error: VkDescriptorSetAllocateInfo wants %u sets "
             "but we only have %u available. "
             "Bailing with VK_ERROR_OUT_OF_POOL_MEMORY.\n",
@@ -401,7 +401,7 @@ static VkResult validateDescriptorSetAllocation(const VkDescriptorSetAllocateInf
 
     for (uint32_t i = 0; i < pAllocateInfo->descriptorSetCount; ++i) {
         if (!pAllocateInfo->pSetLayouts[i]) {
-            ALOGV("%s: Error: Tried to allocate a descriptor set with null set layout.\n",
+            mesa_logi("%s: Error: Tried to allocate a descriptor set with null set layout.\n",
                   __func__);
             return VK_ERROR_INITIALIZATION_FAILED;
         }
@@ -450,7 +450,7 @@ void removeDescriptorSetAllocation(VkDescriptorPool pool,
     auto allocInfo = as_goldfish_VkDescriptorPool(pool)->allocInfo;
 
     if (0 == allocInfo->usedSets) {
-        ALOGV("%s: Warning: a descriptor set was double freed.\n", __func__);
+        mesa_logi("%s: Warning: a descriptor set was double freed.\n", __func__);
         return;
     }
 

src/gfxstream/guest/vulkan_enc/ResourceTracker.cpp
@@ -5688,13 +5688,13 @@ VkResult ResourceTracker::on_vkGetSemaphoreFdKHR(void* context, VkResult, VkDevi
             VkResult result = enc->vkGetSemaphoreGOOGLE(device, pGetFdInfo->semaphore, syncId,
                                                         true /* do lock */);
             if (result != VK_SUCCESS) {
-                ALOGE("unable to get the semaphore");
+                mesa_loge("unable to get the semaphore");
                 return result;
             }
 
             result = acquireSync(syncId, osHandle);
             if (result != VK_SUCCESS) {
-                ALOGE("unable to create host sync object");
+                mesa_loge("unable to create host sync object");
                 return result;
             }
 

commit 6b83d498791b398a23a4709c64f0d73d311990f4

gfxstream: fix log levels in descriptor handling
... that potentially were accidentally promoted to info logs in
aosp/3252215 which affects common hot path.

Fixes: 6f0fff46340 ("gfxstream: guest: fully mesa-ify vulkan_enc")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31288>

src/gfxstream/guest/vulkan_enc/DescriptorSetVirtualization.cpp
@@ -329,7 +329,7 @@ static bool isBindingFeasibleForAlloc(
     uint32_t availDescriptorCount = countInfo.descriptorCount - countInfo.used;
 
     if (availDescriptorCount < binding.descriptorCount) {
-        mesa_logi(
+        mesa_logd(
             "%s: Ran out of descriptors of type 0x%x. "
             "Wanted %u from layout but "
             "we only have %u free (total in pool: %u)\n",
@@ -346,7 +346,7 @@ static bool isBindingFeasibleForFree(
     const VkDescriptorSetLayoutBinding& binding) {
     if (countInfo.type != binding.descriptorType) return false;
     if (countInfo.used < binding.descriptorCount) {
-        mesa_logi(
+        mesa_logd(
             "%s: Was a descriptor set double freed? "
             "Ran out of descriptors of type 0x%x. "
             "Wanted to free %u from layout but "
@@ -376,7 +376,7 @@ static VkResult validateDescriptorSetAllocation(const VkDescriptorSetAllocateInf
     auto setsAvailable = poolInfo->maxSets - poolInfo->usedSets;
 
     if (setsAvailable < pAllocateInfo->descriptorSetCount) {
-        mesa_logi(
+        mesa_logd(
             "%s: Error: VkDescriptorSetAllocateInfo wants %u sets "
             "but we only have %u available. "
             "Bailing with VK_ERROR_OUT_OF_POOL_MEMORY.\n",
@@ -391,7 +391,7 @@ static VkResult validateDescriptorSetAllocation(const VkDescriptorSetAllocateInf
 
     for (uint32_t i = 0; i < pAllocateInfo->descriptorSetCount; ++i) {
         if (!pAllocateInfo->pSetLayouts[i]) {
-            mesa_logi("%s: Error: Tried to allocate a descriptor set with null set layout.\n",
+            mesa_logd("%s: Error: Tried to allocate a descriptor set with null set layout.\n",
                   __func__);
             return VK_ERROR_INITIALIZATION_FAILED;
         }
@@ -440,7 +440,7 @@ void removeDescriptorSetAllocation(VkDescriptorPool pool,
     auto allocInfo = as_goldfish_VkDescriptorPool(pool)->allocInfo;
 
     if (0 == allocInfo->usedSets) {
-        mesa_logi("%s: Warning: a descriptor set was double freed.\n", __func__);
+        mesa_logd("%s: Warning: a descriptor set was double freed.\n", __func__);
         return;
     }
 