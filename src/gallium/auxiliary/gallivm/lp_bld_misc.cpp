a8eed9cca26b1c4b6526be7d042adec6703a30ae
Nikita Popov <npopov@redhat.com>
2024-09-23 15:42:32 +0200

    gallium: Don't pass avx512er and avx512pf features on LLVM 19
    These target features have been remove in LLVM 19, and cause
    warnings like the following to be printed:
    
        '-avx512er' is not a recognized feature for this target (ignoring feature)
        '-avx512pf' is not a recognized feature for this target (ignoring feature)
    
    Do not pass these target features on LLVM 19 and newer.
    
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/11870
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Cc: mesa-stable
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31321>

--- src/gallium/auxiliary/gallivm/lp_bld_misc.cpp
+++ src/gallium/auxiliary/gallivm/lp_bld_misc.cpp
@@ -370,8 +370,10 @@ lp_build_fill_mattrs(std::vector<std::string> &MAttrs)
    /* All avx512 have avx512f */
    MAttrs.push_back(util_get_cpu_caps()->has_avx512f ? "+avx512f"  : "-avx512f");
    MAttrs.push_back(util_get_cpu_caps()->has_avx512cd ? "+avx512cd"  : "-avx512cd");
+#if LLVM_VERSION_MAJOR < 19
    MAttrs.push_back(util_get_cpu_caps()->has_avx512er ? "+avx512er"  : "-avx512er");
    MAttrs.push_back(util_get_cpu_caps()->has_avx512pf ? "+avx512pf"  : "-avx512pf");
+#endif
    MAttrs.push_back(util_get_cpu_caps()->has_avx512bw ? "+avx512bw"  : "-avx512bw");
    MAttrs.push_back(util_get_cpu_caps()->has_avx512dq ? "+avx512dq"  : "-avx512dq");
    MAttrs.push_back(util_get_cpu_caps()->has_avx512vl ? "+avx512vl"  : "-avx512vl");
