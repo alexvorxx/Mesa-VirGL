alexvorxx/zink-xlib-termux

commit 0b5097081a366fde4f9a45eef3b46527df1a59a1

freedreno/devices: Add A740v3 from Quest 3
Two known differences with a740 are:
- RB_DBG_ECO_CNTL being 1 on A740v3
- Concurrent binning is not used

We don't have concurrent binning implemented and it's unknown
how important is RB_DBG_ECO_CNTL diff. So for now A740v3 is aliased
to ordinary A740.

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28380>

src/freedreno/common/freedreno_devices.py
@@ -916,6 +916,8 @@ add_gpus([
         GPUId(740), # Deprecated, used for dev kernels.
         GPUId(chip_id=0x43050a01, name="FD740"), # KGSL, no speedbin data
         GPUId(chip_id=0xffff43050a01, name="FD740"), # Default no-speedbin fallback
+        GPUId(chip_id=0x43050B00, name="FD740"), # Quest 3
+        GPUId(chip_id=0xffff43050B00, name="FD740"),
     ], A6xxGPUInfo(
         CHIP.A7XX,
         [a7xx_base, a7xx_740],

alexvorxx/Mesa-VirGL

commit 7968b356f8cabe862f91d2d82e98195b4b8bb78a

freedreno/devices: Fix A740v3 from Quest 3
Based on a patch by weab chan.

The chip_id wasn't getting picked up with the capital B, so use lowercase hex values
like everywhere else.

Move it as a separate entry and turn on enable_tp_ubwc_flag_hint as the Quest 3 ships
with blob version 7xx, and expose the name as FD740v3 for clarity.

Unify the raw_magic_regs with a740, but as noted by Danylo in the initial enablement, set
RB_DBG_ECO_CNTL to 1 on a740v3 in magic_regs.

Fixes image corruption issues on this device.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/10316
Fixes: 0b5097081a3 ("freedreno/devices: Add A740v3 from Quest 3")
Signed-off-by: Valentine Burley <valentine.burley@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31328>

src/freedreno/common/freedreno_devices.py
@@ -878,6 +878,15 @@ a7xx_740_a32 = A7XXProps(
         enable_tp_ubwc_flag_hint = False,
     )
 
+a7xx_740v3 = A7XXProps(
+        stsc_duplication_quirk = True,
+        has_event_write_sample_count = True,
+        ubwc_unorm_snorm_int_compatible = True,
+        supports_ibo_ubwc = True,
+        fs_must_have_non_zero_constlen_quirk = True,
+        enable_tp_ubwc_flag_hint = True,
+    )
+
 a7xx_750 = A7XXProps(
         has_event_write_sample_count = True,
         load_inline_uniforms_via_preamble_ldgk = True,
@@ -954,6 +963,58 @@ a730_raw_magic_regs = [
         [A6XXRegs.REG_A6XX_RB_UNKNOWN_88F4,   0x00000000],
         [A6XXRegs.REG_A7XX_HLSQ_UNKNOWN_A9AD, 0x00000000],
         [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_80F4, 0x00000000],
+    ]
+
+a740_raw_magic_regs = [
+        [A6XXRegs.REG_A6XX_UCHE_CACHE_WAYS, 0x00040004],
+        [A6XXRegs.REG_A6XX_TPL1_DBG_ECO_CNTL1, 0x00040724],
+
+        [A6XXRegs.REG_A7XX_SP_UNKNOWN_AE08, 0x00000400],
+        [A6XXRegs.REG_A7XX_SP_UNKNOWN_AE09, 0x00430800],
+        [A6XXRegs.REG_A7XX_SP_UNKNOWN_AE0A, 0x00000000],
+        [A6XXRegs.REG_A7XX_UCHE_UNKNOWN_0E10, 0x00000000],
+        [A6XXRegs.REG_A7XX_UCHE_UNKNOWN_0E11, 0x00000000],
+        [A6XXRegs.REG_A7XX_SP_UNKNOWN_AE6C, 0x00000000],
+        [A6XXRegs.REG_A6XX_PC_DBG_ECO_CNTL, 0x00100000],
+        [A6XXRegs.REG_A7XX_PC_UNKNOWN_9E24, 0x21585600],
+        [A6XXRegs.REG_A7XX_VFD_UNKNOWN_A600, 0x00008000],
+        [A6XXRegs.REG_A7XX_SP_UNKNOWN_AE06, 0x00000000],
+        [A6XXRegs.REG_A7XX_SP_UNKNOWN_AE6A, 0x00000000],
+        [A6XXRegs.REG_A7XX_SP_UNKNOWN_AE6B, 0x00000080],
+        [A6XXRegs.REG_A7XX_SP_UNKNOWN_AE73, 0x00000000],
+        [A6XXRegs.REG_A7XX_SP_UNKNOWN_AB02, 0x00000000],
+        [A6XXRegs.REG_A7XX_SP_UNKNOWN_AB01, 0x00000000],
+        [A6XXRegs.REG_A7XX_SP_UNKNOWN_AB22, 0x00000000],
+        [A6XXRegs.REG_A7XX_SP_UNKNOWN_B310, 0x00000000],
+
+        [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_8120, 0x09510840],
+        [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_8121, 0x00000a62],
+
+        [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_8009, 0x00000000],
+        [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_800A, 0x00000000],
+        [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_800B, 0x00000000],
+        [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_800C, 0x00000000],
+
+        [A6XXRegs.REG_A7XX_SP_UNKNOWN_0CE2,   0x00000000],
+        [A6XXRegs.REG_A7XX_SP_UNKNOWN_0CE2+1, 0x00000000],
+        [A6XXRegs.REG_A7XX_SP_UNKNOWN_0CE4,   0x00000000],
+        [A6XXRegs.REG_A7XX_SP_UNKNOWN_0CE4+1, 0x00000000],
+        [A6XXRegs.REG_A7XX_SP_UNKNOWN_0CE6,   0x00000000],
+        [A6XXRegs.REG_A7XX_SP_UNKNOWN_0CE6+1, 0x00000000],
+
+        [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_80A7, 0x00000000],
+
+        [A6XXRegs.REG_A7XX_RB_UNKNOWN_8E79,   0x00000000],
+        [A6XXRegs.REG_A7XX_RB_UNKNOWN_8899,   0x00000000],
+        [A6XXRegs.REG_A7XX_RB_UNKNOWN_88F5,   0x00000000],
+        [A6XXRegs.REG_A7XX_RB_UNKNOWN_8C34,   0x00000000],
+
+        # Shading rate group
+        [A6XXRegs.REG_A6XX_RB_UNKNOWN_88F4,   0x00000000],
+        [A6XXRegs.REG_A7XX_HLSQ_UNKNOWN_A9AD, 0x00000000],
+        [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_8008, 0x00000000],
+        [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_80F4, 0x00000000],
+        [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_80F5, 0x00000000],
     ]
 
 add_gpus([
@@ -1074,8 +1135,6 @@ add_gpus([
         GPUId(740), # Deprecated, used for dev kernels.
         GPUId(chip_id=0x43050a01, name="FD740"), # KGSL, no speedbin data
         GPUId(chip_id=0xffff43050a01, name="FD740"), # Default no-speedbin fallback
-        GPUId(chip_id=0x43050B00, name="FD740"), # Quest 3
-        GPUId(chip_id=0xffff43050B00, name="FD740"),
         GPUId(chip_id=0xffff43050c01, name="Adreno X1-85"),
     ], A6xxGPUInfo(
         CHIP.A7XX,
@@ -1106,57 +1165,7 @@ add_gpus([
 
             RB_UNKNOWN_8E06 = 0x02080000,
         ),
-        raw_magic_regs = [
-            [A6XXRegs.REG_A6XX_UCHE_CACHE_WAYS, 0x00040004],
-            [A6XXRegs.REG_A6XX_TPL1_DBG_ECO_CNTL1, 0x00040724],
-
-            [A6XXRegs.REG_A7XX_SP_UNKNOWN_AE08, 0x00000400],
-            [A6XXRegs.REG_A7XX_SP_UNKNOWN_AE09, 0x00430800],
-            [A6XXRegs.REG_A7XX_SP_UNKNOWN_AE0A, 0x00000000],
-            [A6XXRegs.REG_A7XX_UCHE_UNKNOWN_0E10, 0x00000000],
-            [A6XXRegs.REG_A7XX_UCHE_UNKNOWN_0E11, 0x00000000],
-            [A6XXRegs.REG_A7XX_SP_UNKNOWN_AE6C, 0x00000000],
-            [A6XXRegs.REG_A6XX_PC_DBG_ECO_CNTL, 0x00100000],
-            [A6XXRegs.REG_A7XX_PC_UNKNOWN_9E24, 0x21585600],
-            [A6XXRegs.REG_A7XX_VFD_UNKNOWN_A600, 0x00008000],
-            [A6XXRegs.REG_A7XX_SP_UNKNOWN_AE06, 0x00000000],
-            [A6XXRegs.REG_A7XX_SP_UNKNOWN_AE6A, 0x00000000],
-            [A6XXRegs.REG_A7XX_SP_UNKNOWN_AE6B, 0x00000080],
-            [A6XXRegs.REG_A7XX_SP_UNKNOWN_AE73, 0x00000000],
-            [A6XXRegs.REG_A7XX_SP_UNKNOWN_AB02, 0x00000000],
-            [A6XXRegs.REG_A7XX_SP_UNKNOWN_AB01, 0x00000000],
-            [A6XXRegs.REG_A7XX_SP_UNKNOWN_AB22, 0x00000000],
-            [A6XXRegs.REG_A7XX_SP_UNKNOWN_B310, 0x00000000],
-
-            [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_8120, 0x09510840],
-            [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_8121, 0x00000a62],
-
-            [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_8009, 0x00000000],
-            [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_800A, 0x00000000],
-            [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_800B, 0x00000000],
-            [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_800C, 0x00000000],
-
-            [A6XXRegs.REG_A7XX_SP_UNKNOWN_0CE2,   0x00000000],
-            [A6XXRegs.REG_A7XX_SP_UNKNOWN_0CE2+1, 0x00000000],
-            [A6XXRegs.REG_A7XX_SP_UNKNOWN_0CE4,   0x00000000],
-            [A6XXRegs.REG_A7XX_SP_UNKNOWN_0CE4+1, 0x00000000],
-            [A6XXRegs.REG_A7XX_SP_UNKNOWN_0CE6,   0x00000000],
-            [A6XXRegs.REG_A7XX_SP_UNKNOWN_0CE6+1, 0x00000000],
-
-            [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_80A7, 0x00000000],
-
-            [A6XXRegs.REG_A7XX_RB_UNKNOWN_8E79,   0x00000000],
-            [A6XXRegs.REG_A7XX_RB_UNKNOWN_8899,   0x00000000],
-            [A6XXRegs.REG_A7XX_RB_UNKNOWN_88F5,   0x00000000],
-            [A6XXRegs.REG_A7XX_RB_UNKNOWN_8C34,   0x00000000],
-
-            # Shading rate group
-            [A6XXRegs.REG_A6XX_RB_UNKNOWN_88F4,   0x00000000],
-            [A6XXRegs.REG_A7XX_HLSQ_UNKNOWN_A9AD, 0x00000000],
-            [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_8008, 0x00000000],
-            [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_80F4, 0x00000000],
-            [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_80F5, 0x00000000],
-        ],
+        raw_magic_regs = a740_raw_magic_regs,
     ))
 
 # Values from blob v676.0
@@ -1241,6 +1250,41 @@ add_gpus([
             [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_80F4, 0x00000000],
             [A6XXRegs.REG_A7XX_GRAS_UNKNOWN_80F5, 0x00000000],
         ],
+    ))
+
+add_gpus([
+        GPUId(chip_id=0x43050b00, name="FD740v3"), # Quest 3
+        GPUId(chip_id=0xffff43050b00, name="FD740v3"),
+    ], A6xxGPUInfo(
+        CHIP.A7XX,
+        [a7xx_base, a7xx_740v3],
+        num_ccu = 6,
+        tile_align_w = 96,
+        tile_align_h = 32,
+        num_vsc_pipes = 32,
+        cs_shared_mem_size = 32 * 1024,
+        wave_granularity = 2,
+        fibers_per_sp = 128 * 2 * 16,
+        magic_regs = dict(
+            # PC_POWER_CNTL = 7,
+            TPL1_DBG_ECO_CNTL = 0x11100000,
+            GRAS_DBG_ECO_CNTL = 0x00004800,
+            SP_CHICKEN_BITS = 0x10001400,
+            UCHE_CLIENT_PF = 0x00000084,
+            # Blob uses 0x1f or 0x1f1f, however these values cause vertices
+            # corruption in some tests.
+            PC_MODE_CNTL = 0x0000003f,
+            SP_DBG_ECO_CNTL = 0x10000000,
+            RB_DBG_ECO_CNTL = 0x00000001,
+            RB_DBG_ECO_CNTL_blit = 0x00000000,  # is it even needed?
+            # HLSQ_DBG_ECO_CNTL = 0x0,
+            RB_UNKNOWN_8E01 = 0x0,
+            VPC_DBG_ECO_CNTL = 0x02000000,
+            UCHE_UNKNOWN_0E12 = 0x00000000,
+
+            RB_UNKNOWN_8E06 = 0x02080000,
+        ),
+        raw_magic_regs = a740_raw_magic_regs,
     ))
 
 add_gpus([