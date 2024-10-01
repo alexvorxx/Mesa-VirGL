alexvorxx/Mesa-VirGL

commit badb3f6301d47f922ce52839ae77b0e851b1011e

anv: Only flush render target cache when detecting RT changes
We setup an empty render target when there are no color attachments,
which effectively makes it a different surface state. In most cases
the compiler will insert a null-rt bit in the extended descriptor
which means the RT isn't even accessed. But in some cases like
alpha-to-coverage output + depth/stencil write, we will access the
render target because using the null-rt will prevent alpha-to-coverage
from happening.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 2bd304bc8f ("anv: Skip the RT flush when doing depth-only rendering.")
Reviewed-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31196>

src/intel/vulkan/anv_private.h
@@ -3822,6 +3822,12 @@ struct anv_cmd_graphics_state {
    struct anv_attachment stencil_att;
    struct anv_state null_surface_state;
 
+   /* Bitfield of color attachments disabled by a pipeline (pointing a null
+    * surface state in the last emitted binding table for the fragment
+    * stage)
+    */
+   uint8_t disabled_color_atts;
+
    anv_cmd_dirty_mask_t dirty;
    uint32_t vb_dirty;
 

src/intel/vulkan/genX_cmd_draw.c
@@ -688,6 +688,60 @@ genX(emit_ds)(struct anv_cmd_buffer *cmd_buffer)
 }
 
 ALWAYS_INLINE static void
+cmd_buffer_maybe_flush_rt_writes(struct anv_cmd_buffer *cmd_buffer,
+                                 struct anv_graphics_pipeline *pipeline)
+{
+#if GFX_VER >= 11
+   if (!anv_pipeline_has_stage(pipeline, MESA_SHADER_FRAGMENT))
+      return;
+
+   const struct anv_shader_bin *shader =
+      pipeline->base.shaders[MESA_SHADER_FRAGMENT];
+   const struct anv_pipeline_bind_map *bind_map = &shader->bind_map;
+
+   unsigned s;
+   uint8_t disabled_mask = 0;
+   for (s = 0; s < bind_map->surface_count; s++) {
+      const struct anv_pipeline_binding *binding =
+         &bind_map->surface_to_descriptor[s];
+
+      if (binding->set != ANV_DESCRIPTOR_SET_COLOR_ATTACHMENTS)
+         break;
+
+      if (binding->index >= cmd_buffer->state.gfx.color_att_count)
+         disabled_mask |= BITFIELD_BIT(s);
+   }
+
+   const uint8_t diff_mask = (1u << s) - 1;
+
+   if ((cmd_buffer->state.gfx.disabled_color_atts & diff_mask) != disabled_mask) {
+      cmd_buffer->state.gfx.disabled_color_atts = disabled_mask |
+         (cmd_buffer->state.gfx.disabled_color_atts & ~diff_mask);
+
+      /* The PIPE_CONTROL command description says:
+       *
+       *    "Whenever a Binding Table Index (BTI) used by a Render Target Message
+       *     points to a different RENDER_SURFACE_STATE, SW must issue a Render
+       *     Target Cache Flush by enabling this bit. When render target flush
+       *     is set due to new association of BTI, PS Scoreboard Stall bit must
+       *     be set in this packet."
+       *
+       * Within a renderpass, the render target entries in the binding tables
+       * remain the same as what was setup at CmdBeginRendering() with one
+       * exception where have to setup a null render target because a fragment
+       * writes only depth/stencil yet the renderpass has been setup with at
+       * least one color attachment. This is because our render target messages
+       * in the shader always send the color.
+       */
+      anv_add_pending_pipe_bits(cmd_buffer,
+                                ANV_PIPE_RENDER_TARGET_CACHE_FLUSH_BIT |
+                                ANV_PIPE_STALL_AT_SCOREBOARD_BIT,
+                                "change RT due to shader outputs");
+   }
+#endif
+}
+
+ALWAYS_INLINE static void
 genX(cmd_buffer_flush_gfx_state)(struct anv_cmd_buffer *cmd_buffer)
 {
    struct anv_graphics_pipeline *pipeline =
@@ -708,16 +762,18 @@ genX(cmd_buffer_flush_gfx_state)(struct anv_cmd_buffer *cmd_buffer)
 
    genX(flush_pipeline_select_3d)(cmd_buffer);
 
-   /* Wa_14015814527
-    *
-    * Apply task URB workaround when switching from task to primitive.
-    */
    if (cmd_buffer->state.gfx.dirty & ANV_CMD_DIRTY_PIPELINE) {
+      /* Wa_14015814527
+       *
+       * Apply task URB workaround when switching from task to primitive.
+       */
       if (anv_pipeline_is_primitive(pipeline)) {
          genX(apply_task_urb_workaround)(cmd_buffer);
       } else if (anv_pipeline_has_stage(pipeline, MESA_SHADER_TASK)) {
          cmd_buffer->state.gfx.used_task_shader = true;
       }
+
+      cmd_buffer_maybe_flush_rt_writes(cmd_buffer, pipeline);
    }
 
    /* Apply any pending pipeline flushes we may have.  We want to apply them

src/intel/vulkan/genX_cmd_buffer.c
@@ -5176,15 +5176,24 @@ void genX(CmdBeginRendering)(
    };
 
    const uint32_t color_att_count = pRenderingInfo->colorAttachmentCount;
+
    result = anv_cmd_buffer_init_attachments(cmd_buffer, color_att_count);
    if (result != VK_SUCCESS)
       return;
 
    genX(flush_pipeline_select_3d)(cmd_buffer);
 
+   UNUSED bool render_target_change = false;
    for (uint32_t i = 0; i < gfx->color_att_count; i++) {
-      if (pRenderingInfo->pColorAttachments[i].imageView == VK_NULL_HANDLE)
+      if (pRenderingInfo->pColorAttachments[i].imageView == VK_NULL_HANDLE) {
+         render_target_change |= gfx->color_att[i].iview != NULL;
+
+         gfx->color_att[i].vk_format = VK_FORMAT_UNDEFINED;
+         gfx->color_att[i].iview = NULL;
+         gfx->color_att[i].layout = VK_IMAGE_LAYOUT_UNDEFINED;
+         gfx->color_att[i].aux_usage = ISL_AUX_USAGE_NONE;
          continue;
+      }
 
       const VkRenderingAttachmentInfo *att =
          &pRenderingInfo->pColorAttachments[i];
@@ -5210,6 +5219,13 @@ void genX(CmdBeginRendering)(
                                  VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
                                  att->imageLayout,
                                  cmd_buffer->queue_family->queueFlags);
+
+      render_target_change |= gfx->color_att[i].iview != iview;
+
+      gfx->color_att[i].vk_format = iview->vk.format;
+      gfx->color_att[i].iview = iview;
+      gfx->color_att[i].layout = att->imageLayout;
+      gfx->color_att[i].aux_usage = aux_usage;
 
       union isl_color_value fast_clear_color = { .u32 = { 0, } };
 
@@ -5324,11 +5340,6 @@ void genX(CmdBeginRendering)(
          /* If not LOAD_OP_CLEAR, we shouldn't have a layout transition. */
          assert(att->imageLayout == initial_layout);
       }
-
-      gfx->color_att[i].vk_format = iview->vk.format;
-      gfx->color_att[i].iview = iview;
-      gfx->color_att[i].layout = att->imageLayout;
-      gfx->color_att[i].aux_usage = aux_usage;
 
       struct isl_view isl_view = iview->planes[0].isl;
       if (pRenderingInfo->viewMask) {
@@ -5606,14 +5617,7 @@ void genX(CmdBeginRendering)(
    gfx->dirty |= ANV_CMD_DIRTY_PIPELINE;
 
 #if GFX_VER >= 11
-   bool has_color_att = false;
-   for (uint32_t i = 0; i < gfx->color_att_count; i++) {
-      if (pRenderingInfo->pColorAttachments[i].imageView != VK_NULL_HANDLE) {
-         has_color_att = true;
-         break;
-      }
-   }
-   if (has_color_att) {
+   if (render_target_change) {
       /* The PIPE_CONTROL command description says:
       *
       *    "Whenever a Binding Table Index (BTI) used by a Render Target Message

commit 17c3bd358ef8df18d482f47ba05262472be53763

anv: limit render target cache flushing due to color output remapping
Fixes a performance regression of 1%/2% introduced in badb3f6301
("anv: Only flush render target cache when detecting RT changes")

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31380>

src/intel/vulkan/anv_pipeline.c
@@ -1554,7 +1554,7 @@ anv_pipeline_link_fs(const struct brw_compiler *compiler,
             /* Setup a null render target */
             rt_bindings[rt] = (struct anv_pipeline_binding) {
                .set = ANV_DESCRIPTOR_SET_COLOR_ATTACHMENTS,
-               .index = UINT32_MAX,
+               .index = ANV_COLOR_OUTPUT_UNUSED,
                .binding = UINT32_MAX,
             };
          }
@@ -1567,7 +1567,8 @@ anv_pipeline_link_fs(const struct brw_compiler *compiler,
       /* Setup a null render target */
       rt_bindings[0] = (struct anv_pipeline_binding) {
          .set = ANV_DESCRIPTOR_SET_COLOR_ATTACHMENTS,
-         .index = UINT32_MAX,
+         .index = ANV_COLOR_OUTPUT_DISABLED,
+         .binding = UINT32_MAX,
       };
       num_rt_bindings = 1;
    } else {
@@ -2969,31 +2970,41 @@ anv_graphics_pipeline_emit(struct anv_graphics_pipeline *pipeline,
       pipeline->min_sample_shading = state->ms->min_sample_shading;
    }
 
+   /* Mark all color output as unused by default */
+   memset(pipeline->color_output_mapping,
+          ANV_COLOR_OUTPUT_UNUSED,
+          sizeof(pipeline->color_output_mapping));
+
    if (anv_pipeline_has_stage(pipeline, MESA_SHADER_FRAGMENT)) {
       /* Count the number of color attachments in the binding table */
       const struct anv_pipeline_bind_map *bind_map =
          &pipeline->base.shaders[MESA_SHADER_FRAGMENT]->bind_map;
 
-      memset(pipeline->color_output_mapping,
-             MESA_VK_ATTACHMENT_UNUSED,
-             sizeof(pipeline->color_output_mapping));
-
       if (state->cal != NULL) {
+         /* Build a map of fragment color output to attachment */
+         uint8_t rt_to_att[MAX_RTS];
+         memset(rt_to_att, ANV_COLOR_OUTPUT_DISABLED, MAX_RTS);
          for (uint32_t i = 0; i < MAX_RTS; i++) {
             if (state->cal->color_map[i] != MESA_VK_ATTACHMENT_UNUSED)
-               pipeline->color_output_mapping[state->cal->color_map[i]] = i;
+               rt_to_att[state->cal->color_map[i]] = i;
          }
 
+         /* For each fragment shader output if not unused apply the remapping
+          * to pipeline->color_output_mapping
+          */
          unsigned i;
          for (i = 0; i < MIN2(bind_map->surface_count, MAX_RTS); i++) {
             if (bind_map->surface_to_descriptor[i].set !=
                 ANV_DESCRIPTOR_SET_COLOR_ATTACHMENTS)
                break;
 
-            /* Helping Alpine builders... */
-            assert(i < MAX_RTS);
-            if (bind_map->surface_to_descriptor[i].index >= MAX_RTS)
-               pipeline->color_output_mapping[i] = MESA_VK_ATTACHMENT_UNUSED;
+            uint32_t index = bind_map->surface_to_descriptor[i].index;
+            if (index >= MAX_RTS) {
+               assert(index <= 0xff);
+               pipeline->color_output_mapping[i] = index;
+            } else {
+               pipeline->color_output_mapping[i] = rt_to_att[i];
+            }
          }
          pipeline->num_color_outputs = i;
       }

src/intel/vulkan/anv_private.h
@@ -255,6 +255,9 @@ struct intel_perf_query_result;
 #define ANV_TRTT_L1_NULL_TILE_VAL 0
 #define ANV_TRTT_L1_INVALID_TILE_VAL 1
 
+#define ANV_COLOR_OUTPUT_DISABLED (0xff)
+#define ANV_COLOR_OUTPUT_UNUSED   (0xfe)
+
 static inline uint32_t
 align_down_npot_u32(uint32_t v, uint32_t a)
 {

src/intel/vulkan/genX_cmd_draw.c
@@ -695,12 +695,19 @@ cmd_buffer_maybe_flush_rt_writes(struct anv_cmd_buffer *cmd_buffer,
       return;
 
    UNUSED bool need_rt_flush = false;
-   if (memcmp(cmd_buffer->state.gfx.color_output_mapping,
-              pipeline->color_output_mapping,
-              pipeline->num_color_outputs)) {
-      memcpy(cmd_buffer->state.gfx.color_output_mapping,
-             pipeline->color_output_mapping,
-             pipeline->num_color_outputs);
+   for (uint32_t rt = 0; rt < pipeline->num_color_outputs; rt++) {
+      /* No writes going to this render target so it won't affect the RT cache
+       */
+      if (pipeline->color_output_mapping[rt] == ANV_COLOR_OUTPUT_UNUSED)
+         continue;
+
+      /* No change */
+      if (cmd_buffer->state.gfx.color_output_mapping[rt] ==
+          pipeline->color_output_mapping[rt])
+         continue;
+
+      cmd_buffer->state.gfx.color_output_mapping[rt] =
+         pipeline->color_output_mapping[rt];
       need_rt_flush = true;
       cmd_buffer->state.descriptors_dirty |= VK_SHADER_STAGE_FRAGMENT_BIT;
    }