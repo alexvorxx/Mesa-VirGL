aa9b8316aef35d32575c60ae67aa552f9179144d
Job Noorman <jnoorman@igalia.com>
2024-10-01 07:40:54 +0200

    ir3: lower 64b SSBO accesses
    Signed-off-by: Job Noorman <jnoorman@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31455>

--- src/freedreno/ir3/ir3_nir.c
+++ src/freedreno/ir3/ir3_nir.c
@@ -982,7 +982,7 @@ ir3_nir_lower_variant(struct ir3_shader_variant *so, nir_shader *s)
    nir_lower_mem_access_bit_sizes_options mem_bit_size_options = {
       .modes = nir_var_mem_constant | nir_var_mem_ubo |
                nir_var_mem_global | nir_var_mem_shared |
-               nir_var_function_temp,
+               nir_var_function_temp | nir_var_mem_ssbo,
       .callback = ir3_mem_access_size_align,
    };
 
