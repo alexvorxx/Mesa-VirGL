b43a639fe74bc6aa640a0dbc5181456a8e2a861f
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2024-10-02 11:07:52 -0400

    zink: block all 2d view creation with sparse
    this is illegal (for now)
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31489>

--- src/gallium/drivers/zink/zink_resource.c
+++ src/gallium/drivers/zink/zink_resource.c
@@ -749,11 +749,11 @@ init_ici(struct zink_screen *screen, VkImageCreateInfo *ici, const struct pipe_r
 
    case PIPE_TEXTURE_3D:
       ici->imageType = VK_IMAGE_TYPE_3D;
-      if (!(templ->flags & PIPE_RESOURCE_FLAG_SPARSE))
+      if (!(templ->flags & PIPE_RESOURCE_FLAG_SPARSE)) {
          ici->flags |= VK_IMAGE_CREATE_2D_ARRAY_COMPATIBLE_BIT;
-      if (screen->info.have_EXT_image_2d_view_of_3d &&
-          (screen->driver_workarounds.can_2d_view_sparse || !(templ->flags & PIPE_RESOURCE_FLAG_SPARSE)))
-         ici->flags |= VK_IMAGE_CREATE_2D_VIEW_COMPATIBLE_BIT_EXT;
+         if (screen->info.have_EXT_image_2d_view_of_3d)
+            ici->flags |= VK_IMAGE_CREATE_2D_VIEW_COMPATIBLE_BIT_EXT;
+      }
       break;
 
    case PIPE_BUFFER:
