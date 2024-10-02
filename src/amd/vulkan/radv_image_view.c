alexvorxx/Mesa-VirGL

commit 8ddc527ba4eaf465b87e0670e96f6de147dec1be

vk/image: Fix the view extent of uncompressed views of compressed images
The view extent needs to be divided by the block width/height/depth in
that case.

Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29333>

src/vulkan/runtime/vk_image.c
@@ -535,6 +535,23 @@ vk_image_view_init(struct vk_device *device,
    image_view->extent =
       vk_image_mip_level_extent(image, image_view->base_mip_level);
 
+   if (vk_format_is_compressed(image->format) &&
+       !vk_format_is_compressed(image_view->format)) {
+      const struct util_format_description *fmt =
+         vk_format_description(image_view->format);
+
+      /* Non-compressed view of compressed image only works for single MIP
+       * views.
+       */
+      assert(image_view->level_count == 1);
+      image_view->extent.width =
+         DIV_ROUND_UP(image_view->extent.width, fmt->block.width);
+      image_view->extent.height =
+         DIV_ROUND_UP(image_view->extent.height, fmt->block.height);
+      image_view->extent.depth =
+         DIV_ROUND_UP(image_view->extent.depth, fmt->block.depth);
+   }
+
    /* By default storage uses the same as the image properties, but it can be
     * overriden with VkImageViewSlicedCreateInfoEXT.
     */

commit 790759dfafac752d057cd087b48d4acbe8edc81c

vk/image: Fix the extent adjustment of non-compressed views
When creating a non-compressed view of a compressed image, we need to
divide the extent by the image block size not the view block size.

Fixes: 8ddc527ba4ea ("vk/image: Fix the view extent of uncompressed views of compressed images")
Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31439>

src/amd/vulkan/radv_image_view.c
@@ -506,7 +506,8 @@ radv_image_view_init(struct radv_image_view *iview, struct radv_device *device,
          .depth = image->vk.extent.depth,
       };
    } else {
-      iview->extent = iview->vk.extent;
+      iview->extent =
+         vk_image_mip_level_extent(&image->vk, iview->vk.base_mip_level);
    }
 
    if (iview->vk.format != image->planes[iview->plane_id].format) {

src/vulkan/runtime/vk_image.c
@@ -543,7 +543,7 @@ vk_image_view_init(struct vk_device *device,
    if (vk_format_is_compressed(image->format) &&
        !vk_format_is_compressed(image_view->format)) {
       const struct util_format_description *fmt =
-         vk_format_description(image_view->format);
+         vk_format_description(image->format);
 
       /* Non-compressed view of compressed image only works for single MIP
        * views.