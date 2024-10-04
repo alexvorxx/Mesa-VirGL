alexvorxx/zink-xlib-termux

commit bab52763f45860fba90116e553df5cd8a510282f

intel/hang_replay: fix batch address
Also capture all buffers so that we can compare replay run with the
original error state.

Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27594>

src/intel/tools/intel_hang_replay.c
@@ -600,8 +600,9 @@ main(int argc, char *argv[])
             .relocation_count = 0,
             .relocs_ptr       = 0,
             .flags            = EXEC_OBJECT_SUPPORTS_48B_ADDRESS |
-                                EXEC_OBJECT_PINNED,
-            .offset           = bo->offset,
+                                EXEC_OBJECT_PINNED |
+                                EXEC_OBJECT_CAPTURE,
+            .offset           = intel_canonical_address(bo->offset),
          };
       }
 
@@ -620,8 +621,9 @@ main(int argc, char *argv[])
             .relocs_ptr       = 0,
             .flags            = EXEC_OBJECT_SUPPORTS_48B_ADDRESS |
                                 EXEC_OBJECT_PINNED |
-                                EXEC_OBJECT_WRITE /* to be able to wait on the BO */,
-            .offset           = init_bo->offset,
+                                EXEC_OBJECT_WRITE /* to be able to wait on the BO */ |
+                                EXEC_OBJECT_CAPTURE,
+            .offset           = intel_canonical_address(init_bo->offset),
          };
          ret = execbuffer(drm_fd, ctx_id, &execbuffer_bos, init_bo, init.offset);
          if (ret != 0) {
@@ -641,8 +643,9 @@ main(int argc, char *argv[])
             .relocs_ptr       = 0,
             .flags            = EXEC_OBJECT_SUPPORTS_48B_ADDRESS |
                                 EXEC_OBJECT_PINNED |
-                                EXEC_OBJECT_WRITE /* to be able to wait on the BO */,
-            .offset           = batch_bo->offset,
+                                EXEC_OBJECT_WRITE /* to be able to wait on the BO */ |
+                                EXEC_OBJECT_CAPTURE,
+            .offset           = intel_canonical_address(batch_bo->offset),
          };
          ret = execbuffer(drm_fd, ctx_id, &execbuffer_bos, batch_bo, exec.offset);
          if (ret != 0) {

alexvorxx/Mesa-VirGL

commit 39e3015c3609408d7f0c698fc750b2f040049446

intel/hang_replay: remove EXEC_OBJECT_WRITE
This flag causes the following failure:

total_vma: 0x0000000071c9d000
fail to set context hw img: Invalid argument

Fixes: bab52763f45 ("intel/hang_replay: fix batch address")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/11959
Signed-off-by: Carlos Santa <carlos.santa@intel.corp-partner.google.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31494>

src/intel/tools/intel_hang_replay.c
@@ -625,7 +625,6 @@ main(int argc, char *argv[])
             .relocs_ptr       = 0,
             .flags            = EXEC_OBJECT_SUPPORTS_48B_ADDRESS |
                                 EXEC_OBJECT_PINNED |
-                                EXEC_OBJECT_WRITE /* to be able to wait on the BO */ |
                                 EXEC_OBJECT_CAPTURE,
             .offset           = intel_canonical_address(init_bo->offset),
          };
@@ -647,7 +646,6 @@ main(int argc, char *argv[])
             .relocs_ptr       = 0,
             .flags            = EXEC_OBJECT_SUPPORTS_48B_ADDRESS |
                                 EXEC_OBJECT_PINNED |
-                                EXEC_OBJECT_WRITE /* to be able to wait on the BO */ |
                                 EXEC_OBJECT_CAPTURE,
             .offset           = intel_canonical_address(batch_bo->offset),
          };